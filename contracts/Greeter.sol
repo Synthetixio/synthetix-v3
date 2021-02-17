343f415048e913062d04351bd729b4327d04dd7b
Alejandro <palebluedot@gmail.com>
2021-02-17 10:32:21 -0300

    Use solhint

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,7 +5,7 @@ import "hardhat/console.sol";
 
 
 contract Greeter {
-  string greeting;
+  string public greeting;
 
   constructor(string memory _greeting) {
     console.log("Deploying a Greeter with greeting:", _greeting);
