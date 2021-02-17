df07b686a9e2dc1734234269103c8cc23a185e89
Alejandro <palebluedot@gmail.com>
2021-02-17 10:17:49 -0300

    Use eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  env: {
+    mocha: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
