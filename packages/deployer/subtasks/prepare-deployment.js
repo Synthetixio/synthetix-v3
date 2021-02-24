c7d155bc89e0ab23d2ce0d39630659a7e8335ee8
Alejandro <palebluedot@gmail.com>
2021-02-24 17:12:58 -0300

    Tweak info

--- packages/deployer/subtasks/prepare-deployment.js
+++ packages/deployer/subtasks/prepare-deployment.js
@@ -15,13 +15,14 @@ let _hre;
 subtask(SUBTASK_PREPARE_DEPLOYMENT).setAction(async (taskArguments, hre) => {
   _hre = hre;
 
-  logger.subtitle('Preparing deployment');
+  const package = readPackageJson({ hre: _hre });
+  logger.title(`Deploying ** ${package.name} **`);
+
+  const deploymentFile = readDeploymentFile({ hre });
 
   if (!hre.deployer) {
     hre.deployer = {};
   }
-
-  const deploymentFile = readDeploymentFile({ hre });
 
   hre.deployer.file = deploymentFile.file;
   hre.deployer.data = deploymentFile.data;
@@ -31,26 +32,29 @@ subtask(SUBTASK_PREPARE_DEPLOYMENT).setAction(async (taskArguments, hre) => {
   await prompter.confirmAction('Proceed with deployment');
 });
 
-function _printInfo({ force, debug }) {
-  const package = readPackageJson({ hre: _hre });
-  const network = _hre.network.name;
-  const branch = getBranch();
-  const commit = getCommit();
-
-  logger.title(`Deploying ** ${package.name} **`);
-
+function _printInfo(taskArguments) {
   console.log(chalk.yellow('------------------------------------------------------------'));
-  console.log(chalk.gray(`commit: ${commit}`));
+
+  console.log(chalk.gray(`commit: ${getCommit()}`));
+
+  const branch = getBranch();
   console.log(chalk[branch !== 'master' ? 'red' : 'gray'](`branch: ${branch}`));
+
+  const network = _hre.network.name;
   console.log(chalk[network.includes('mainnet') ? 'red' : 'gray'](`network: ${network}`));
-  console.log(chalk.gray(`debug: ${debug}`));
-  console.log(chalk.gray(`file: ${_hre.deployer.file}`));
-  if (force) {
+
+  console.log(chalk.gray(`debug: ${taskArguments.debug}`));
+
+  console.log(chalk.gray(`deployment file: ${_hre.deployer.file}`));
+
+  if (taskArguments.force) {
     console.log(chalk.red('force: true - This will override all existing deployments!'));
   } else {
     console.log(chalk.gray('force: false'));
   }
+
   logger.debug('Deployer configuration:');
   logger.debug(JSON.stringify(_hre.config.deployer, null, 2));
+
   console.log(chalk.yellow('------------------------------------------------------------'));
 }
