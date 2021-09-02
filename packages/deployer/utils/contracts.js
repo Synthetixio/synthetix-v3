096f4851e52b4a94a8d8e99099c5bd1cf7b870da
Matías <mjlescano@protonmail.com>
2021-09-03 00:08:50 +0200

    remove unnecessary contract util

--- packages/deployer/utils/contracts.js
+++ packages/deployer/utils/contracts.js
@@ -23,27 +23,8 @@ function getContractBytecodeHash(contractPath) {
   return hre.ethers.utils.sha256(data.deployedBytecode);
 }
 
-async function getContractSelectors({ contractName }) {
-  const contract = await hre.ethers.getContractAt(
-    contractName,
-    '0x0000000000000000000000000000000000000001'
-  );
-
-  return contract.interface.fragments.reduce((selectors, fragment) => {
-    if (fragment.type === 'function') {
-      selectors.push({
-        name: fragment.name,
-        selector: contract.interface.getSighash(fragment),
-      });
-    }
-
-    return selectors;
-  }, []);
-}
-
 module.exports = {
   getContractNameFromPath,
   getAddressBytecodeHash,
   getContractBytecodeHash,
-  getContractSelectors,
 };
