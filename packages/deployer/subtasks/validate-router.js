d1d8f30d64792f251c8628e91555a92af7e7660d
Matías <mjlescano@protonmail.com>
2021-09-03 16:04:33 +0200

    fix router validator logic

--- packages/deployer/subtasks/validate-router.js
+++ packages/deployer/subtasks/validate-router.js
@@ -2,43 +2,45 @@ const fs = require('fs');
 const path = require('path');
 const logger = require('../utils/logger');
 const { subtask } = require('hardhat/config');
-const { getContractSelectors } = require('../utils/contracts');
+const { getContractNameFromPath, getContractSelectors } = require('../utils/contracts');
 const { SUBTASK_VALIDATE_ROUTER } = require('../task-names');
 
-/*
- * Runs a series of calidations against a generated router source.
- * */
-subtask(SUBTASK_VALIDATE_ROUTER).setAction(async () => {
+subtask(
+  SUBTASK_VALIDATE_ROUTER,
+  'Runs a series of validations against a generated router source.'
+).setAction(async () => {
   logger.subtitle('Validating router');
 
-  await _selectorsExistInSource();
+  await _selectorsExistInSource({
+    routerPath: path.join(hre.config.paths.sources, `Router_${hre.network.name}.sol`),
+    modules: Object.keys(hre.deployer.data.contracts.modules).map(getContractNameFromPath),
+  });
 
   logger.checked('Router is valid');
 });
 
-async function _selectorsExistInSource() {
-  const routerPath = path.join(hre.config.paths.sources, `Router_${hre.network.name}.sol`);
-  const source = fs.existsSync(routerPath) ? fs.readFileSync(routerPath) : '';
+async function _selectorsExistInSource({ routerPath, modules }) {
+  const source = fs.readFileSync(routerPath).toString();
 
-  for (let i = 0; i < hre.deployer.sources.length; i++) {
-    const module = hre.deployer.sources[i];
-    const moduleSelectors = await getContractSelectors(module);
+  for (const moduleName of modules) {
+    const moduleSelectors = await getContractSelectors(moduleName);
 
-    moduleSelectors.map((moduleSelector) => {
-      const regex = `^.*case ${moduleSelector.selector}.*$`;
+    moduleSelectors.forEach((moduleSelector) => {
+      const regex = `(:?\\s|^)case ${moduleSelector.selector}\\s.+`;
       const matches = source.match(new RegExp(regex, 'gm'));
+
       if (matches.length !== 1) {
         throw new Error(
           `Selector case found ${matches.length} times instead of the expected single time. Regex: ${regex}. Matches: ${matches}`
         );
       }
 
-      const match = matches[0];
-      if (!match.includes(module)) {
-        throw new Error(`Expected to find ${module} in the selector case: ${match}`);
+      const [match] = matches;
+      if (!match.includes(moduleName)) {
+        throw new Error(`Expected to find ${moduleName} in the selector case: ${match}`);
       }
 
-      logger.debug(`${module}.${moduleSelector.name} selector found in the router`);
+      logger.debug(`${moduleName}.${moduleSelector.name} selector found in the router`);
     });
   }
 }
