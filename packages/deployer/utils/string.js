a1188ae369f60f9dbf8f765f8d127dc39bd4d660
Matías <mjlescano@protonmail.com>
2021-09-08 18:26:06 +0200

    fix startsWithWord util

--- packages/deployer/utils/string.js
+++ packages/deployer/utils/string.js
@@ -11,11 +11,16 @@ module.exports = {
   /**
    * Checks if the given string starts with a prefix, followed by an uppercase
    * letter.
+   * E.g.:
+   *   startsWithWord('GenRouter', 'Gen') // true
+   *   startsWithWord('SomeModule', 'Gen') // false
+   *   startsWithWord('GenerationalModule', 'Gen') // false
    * @param {string} str
    * @param {string} prefix
    * @returns {string}
    */
   startsWithWord(str, prefix) {
+    if (str === prefix) return true;
     return new RegExp(`^${prefix}[A-Z]`).test(str);
   },
 };
