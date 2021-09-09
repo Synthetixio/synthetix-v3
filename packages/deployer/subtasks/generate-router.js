c06da634c93fbb695b429d4b92f631aad290c142
Matías <mjlescano@protonmail.com>
2021-09-10 01:47:03 +0200

    fix module constant name with precedding uppercase letters

--- packages/deployer/subtasks/generate-router.js
+++ packages/deployer/subtasks/generate-router.js
@@ -117,9 +117,13 @@ function _renderModules(modules) {
 
 /**
  * Converts the contracts name to private _CONSTANT_CASE format.
+ * E.g.:
+ *   'BearableModule' => '_BEARABLE_MODULE'
+ *   'Proxy' => '_PROXY'
+ *   'ERC20Token' => '_ERC20_TOKEN'
  */
 function _contractConstantName(name) {
-  return name.replace(/[A-Z]/g, '_$&').toUpperCase();
+  return name.replace(/(?<![A-Z])[A-Z]/g, '_$&').toUpperCase();
 }
 
 async function _getAllSelectors(contractNames) {
