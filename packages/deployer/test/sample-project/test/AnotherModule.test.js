7c574a04d963b7b9bc1d1751f13993b2e7f7ead6
Alejandro <palebluedot@gmail.com>
2021-09-13 15:06:05 -0300

    Added AnotherModule tests

---
+++ packages/deployer/test/sample-project/test/AnotherModule.test.js
@@ -0,0 +1,54 @@
+const hre = require('hardhat');
+const assert = require('assert');
+const { config, ethers } = hre;
+const { getProxyAddress } = require('../../../utils/deployments');
+const { assertRevert } = require('@synthetixio/core-js/utils/assertions');
+const { printGasUsed } = require('@synthetixio/core-js/utils/tests');
+const { bootstrap, initializeSystem } = require('./helpers/initializer');
+
+describe('AnotherModule', () => {
+  bootstrap();
+
+  let SomeModule, AnotherModule;
+
+  let owner;
+
+  before('identify signers', async () => {
+    [owner] = await ethers.getSigners();
+  });
+
+  before('initialize the system', async () => {
+    await initializeSystem({ owner });
+  });
+
+  before('identify modules', async () => {
+    const proxyAddress = getProxyAddress();
+
+    SomeModule = await ethers.getContractAt('SomeModule', proxyAddress);
+    AnotherModule = await ethers.getContractAt('AnotherModule', proxyAddress);
+  });
+
+  describe('when setting a value in SomeModule via AnotherModule', () => {
+    before('set value if zero for correct gas measurements', async () => {
+      await (await SomeModule.setSomeValue(1)).wait();
+    });
+
+    it('using casting', async function() {
+      const tx = await AnotherModule.connect(owner).setSomeValueRouter(42);
+      const receipt = await tx.wait();
+
+      printGasUsed({ test: this, gasUsed: receipt.cumulativeGasUsed });
+
+      assert.equal(await SomeModule.getSomeValue(), 42);
+    });
+
+    it('using the router', async function() {
+      const tx = await AnotherModule.connect(owner).setSomeValueCast(1337);
+      const receipt = await tx.wait();
+
+      printGasUsed({ test: this, gasUsed: receipt.cumulativeGasUsed });
+
+      assert.equal(await SomeModule.getSomeValue(), 1337);
+    });
+  });
+});
