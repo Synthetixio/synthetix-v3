a0c4d809e3de8e648659550d3e761e685403ad09
Alejandro <palebluedot@gmail.com>
2021-09-15 12:27:11 -0300

    Remove comms type 1

--- packages/deployer/test/sample-project/contracts/modules/AnotherModule.sol
+++ packages/deployer/test/sample-project/contracts/modules/AnotherModule.sol
@@ -2,21 +2,33 @@
 pragma solidity ^0.8.0;
 
 import "../storage/GlobalNamespace.sol";
-import "../mixins/ModulesMixin.sol";
+import "../mixins/CommsMixin.sol";
 import "./SomeModule.sol";
 
-contract AnotherModule is ModulesMixin {
+contract AnotherModule is CommsMixin {
+    /*
+       NOTE: This module shows an example of how to do intermodule communications.
+
+       In general, it is not recommended to interact with other modules in a second thread (internal call)
+       since its dangerous and expensive.
+
+       The recommendation is to interact with namespace storage via auxiliary mixins within the same thread.
+
+       Using `call` to read or write to a module, will lose the proxy storage context, unless the call
+       goes through the proxy, which is expensive. Even so, in such a case, msg.sender will be lost to the proxy.
+
+       E.g. this is bad:
+       `SomeModule(address(this)).setSomeValue(42)` or `SomeModule(address(this)).getSomeValue()`
+
+       The CommsMixin's `_intermoduleCall` function facilitates the usage of `delegatecall`,
+       but cannot guarantee "read only" in calls.
+    */
+
     /* MUTATIVE FUNCTIONS */
 
-    // Communicate with SomeModule using Cast mode
-    function setSomeValueCast(uint newValue) public {
-        SomeModule(address(this)).setSomeValue(newValue);
-    }
-
-    // Communicate with SomeModule using Router mode
-    function setSomeValueRouter(uint newValue) public {
-        (bool success, ) = _getRouter().delegatecall(abi.encodeWithSelector(SomeModule.setSomeValue.selector, newValue));
-
-        require(success, "Delegatecall failed");
+    function setSomeValueOnSomeModule(uint newValue) public {
+        _intermoduleCall(
+            abi.encodeWithSelector(SomeModule.setSomeValue.selector, newValue)
+        );
     }
 }
