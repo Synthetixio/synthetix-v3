ad4dca027e1a45410df6a8743be51f19d6302707
Matías <mjlescano@protonmail.com>
2021-09-21 18:59:57 +0200

    add transactions tests for all cases

--- packages/core-js/test/utils/assertions.spec.js
+++ packages/core-js/test/utils/assertions.spec.js
@@ -5,17 +5,30 @@ function mockValidTx() {
   return Promise.resolve({ wait: () => Promise.resolve() });
 }
 
+function mockRevertingTx(errorMsg = '') {
+  return Promise.resolve({ wait: () => Promise.reject(new Error(errorMsg)) });
+}
+
 describe('utils/assertions.js', function () {
   describe('#assertRevert', function () {
+    it('resolves when correctly reverting', async function () {
+      const message = 'The transaction reverted reversely';
+      await assertRevert(mockRevertingTx(message), message);
+    });
+
     it('rejects when not reverting', async function () {
-      await rejects(
-        async () => {
-          await assertRevert(mockValidTx(), 'Some message');
-        },
-        {
-          message: 'Transaction was expected to revert, but it did not',
-        }
-      );
+      await rejects(async () => {
+        await assertRevert(mockValidTx(), 'Some message');
+      }, new Error('Transaction was expected to revert, but it did not'));
+    });
+
+    it('rejects when throwing an unknown error', async function () {
+      const message = 'Unknown transaction error';
+      const expectedMessage = 'The expected transaction error';
+
+      await rejects(async () => {
+        await assertRevert(mockRevertingTx(message), expectedMessage);
+      }, new Error(`Transaction was expected to revert with "${expectedMessage}", but reverted with "Error: ${message}"`));
     });
   });
 });
