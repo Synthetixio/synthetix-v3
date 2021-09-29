cd7159a7e100eb99e87c05cfdd229ed10fc3f399
Thanos Polychronakis <thanpolas@gmail.com>
2021-09-29 15:46:13 +0300

    allow template literals on JS eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
     semi: ['error', 'always'],
     'no-inner-declarations': 'off',
     'max-len': ['error', { code: 160, comments: 100 }],
