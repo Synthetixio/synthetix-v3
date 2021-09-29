132b64523f49cb7ad0045156b42be461cc55ffd6
Alejandro Santander <Palebluedot@gmail.com>
2021-09-29 17:14:54 -0300

    Remove v2x type comments (#192)

--- packages/deployer/test/sample-project/contracts/mixins/OwnerMixin.sol
+++ packages/deployer/test/sample-project/contracts/mixins/OwnerMixin.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.0;
 import "../storage/OwnerNamespace.sol";
 
 contract OwnerMixin is OwnerNamespace {
-    /* MODIFIERS */
-
     modifier onlyOwner() {
         require(msg.sender == _ownerStorage().owner, "Only owner allowed");
         _;
