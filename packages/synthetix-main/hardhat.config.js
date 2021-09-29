ef8fb9b7401276a204c8f5b50652d4c567fe34b4
Alejandro <palebluedot@gmail.com>
2021-09-29 16:05:30 -0300

    Bootstraps main package

---
+++ packages/synthetix-main/hardhat.config.js
@@ -0,0 +1,14 @@
+require('@nomiclabs/hardhat-ethers');
+require('@synthetixio/deployer');
+
+module.exports = {
+  solidity: '0.8.4',
+  deployer: {},
+  defaultNetwork: 'local',
+  networks: {
+    local: {
+      url: 'http://localhost:8545',
+    },
+  },
+};
+;
