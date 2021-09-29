d822dcea29cdda7b7c5966314c2f4c6b182f606e
Matías <mjlescano@protonmail.com>
2021-09-29 16:04:36 -0300

    add integration tests

--- packages/deployer/test/src/tasks/deploy.spec.js
+++ packages/deployer/test/src/tasks/deploy.spec.js
@@ -1,5 +1,6 @@
 const { ok } = require('assert/strict');
 const { useEnvironment } = require('../../helpers');
+const { TASK_DEPLOY } = require('../../../task-names');
 
 describe('tasks/deploy.js', function () {
   useEnvironment('complete-run');
@@ -7,5 +8,25 @@ describe('tasks/deploy.js', function () {
   it('extends the configuration and the environment', function () {
     ok(this.hre.config.deployer);
     ok(this.hre.deployer);
+  });
+
+  it('correctly executes several deployments', async function () {
+    this.timeout(25000);
+
+    await this.hre.run(TASK_DEPLOY, {
+      noConfirm: true,
+      quiet: true,
+      clear: true,
+      alias: 'first',
+      instance: 'test',
+    });
+
+    await this.hre.run(TASK_DEPLOY, {
+      noConfirm: true,
+      quiet: true,
+      clear: false,
+      alias: 'second',
+      instance: 'test',
+    });
   });
 });
