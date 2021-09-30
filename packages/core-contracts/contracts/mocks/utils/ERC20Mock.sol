2dc1f6093cf95bdb8b3be8730c53424ec20c6d0f
i-stam <i.stamelakos@gmail.com>
2021-09-30 15:29:30 +0100

    Use a namespace compatoble struct for storage in ERC20

--- packages/core-contracts/contracts/mocks/utils/ERC20Mock.sol
+++ packages/core-contracts/contracts/mocks/utils/ERC20Mock.sol
@@ -15,37 +15,38 @@ contract ERC20Mock is ERC20 {
     bytes32 private _slot8;
     bytes32 private _slot9;
 
-    mapping(address => uint256) private _balance;
+    struct ERC20Storage {
+        mapping(address => uint256) balance;
+        mapping(address => mapping(address => uint256)) allowance;
+        uint256 totalSupply;
+        string name;
+        string symbol;
+    }
 
-    mapping(address => mapping(address => uint256)) private _allowance;
-
-    uint256 private _totalSupply;
-
-    string private _name;
-    string private _symbol;
+    ERC20Storage erc20Storage;
 
     function name() public view override returns (string memory) {
-        return _name;
+        return erc20Storage.name;
     }
 
     function symbol() public view override returns (string memory) {
-        return _symbol;
+        return erc20Storage.symbol;
     }
 
     function totalSupply() public view override returns (uint256) {
-        return _totalSupply;
+        return erc20Storage.totalSupply;
     }
 
     function balanceOf(address account) public view override returns (uint256) {
-        return _balance[account];
+        return erc20Storage.balance[account];
     }
 
     function allowance(address owner, address spender) public view override returns (uint256) {
-        return _allowance[owner][spender];
+        return erc20Storage.allowance[owner][spender];
     }
 
     function approve(address spender, uint256 amount) public override returns (bool) {
-        _allowance[msg.sender][spender] = amount;
+        erc20Storage.allowance[msg.sender][spender] = amount;
 
         emit Approval(msg.sender, spender, amount);
 
@@ -64,7 +65,7 @@ contract ERC20Mock is ERC20 {
         uint amount
     ) external override returns (bool) {
         // it reverts if the sender does not have sufficient allowance
-        _allowance[from][msg.sender] -= amount;
+        erc20Storage.allowance[from][msg.sender] -= amount;
 
         _transfer(from, to, amount);
 
@@ -77,11 +78,11 @@ contract ERC20Mock is ERC20 {
         uint256 amount
     ) private {
         // it reverts if the sender does not have sufficient balance
-        _balance[from] -= amount;
+        erc20Storage.balance[from] -= amount;
         // we are now sure that we can perform this operation safely since it didn't revert in the previous step
         // i.e. the total supply cannot exceed the maximum value of uint256
         unchecked {
-            _balance[to] += amount;
+            erc20Storage.balance[to] += amount;
         }
 
         emit Transfer(from, to, amount);
