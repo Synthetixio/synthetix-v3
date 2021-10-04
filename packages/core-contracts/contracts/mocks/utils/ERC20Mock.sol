847ed7a170696eafbe4a45607e1bf953fc6e4bd8
i-stam <i.stamelakos@gmail.com>
2021-10-04 14:54:01 +0100

    Add butn tests

--- packages/core-contracts/contracts/mocks/utils/ERC20Mock.sol
+++ packages/core-contracts/contracts/mocks/utils/ERC20Mock.sol
@@ -13,4 +13,8 @@ contract ERC20Mock is ERC20 {
     function mint(uint256 amount) external {
         _mint(msg.sender, amount);
     }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
 }
--- packages/core-contracts/test/utils/ERC20.test.js
+++ packages/core-contracts/test/utils/ERC20.test.js
@@ -1,5 +1,7 @@
 const { ethers } = hre;
 const assert = require('assert');
+const { assertRevert } = require('@synthetixio/core-js/utils/assertions');
+const { findEvent } = require('@synthetixio/core-js/utils/events');
 
 describe('ERC20', () => {
   let ERC20;
@@ -25,22 +27,64 @@ describe('ERC20', () => {
       assert.equal(await ERC20.symbol(), 'snx');
       assert.equal(await ERC20.decimals(), 18);
     });
+
+    it('reverts when trying to burn', async () => {
+      await assertRevert(
+        ERC20.burn(1),
+        'Arithmetic operation underflowed or overflowed outside of an unchecked block'
+      );
+    });
   });
 
-  describe('mint tokens', () => {
-    const totalSupply = '1000000';
+  describe('when tokens are minted', () => {
+    const totalSupply = ethers.BigNumber.from('1000000');
+    let receipt;
 
     before('mint', async () => {
       const tx = await ERC20.connect(user1).mint(totalSupply);
-      await tx.wait();
+      receipt = await tx.wait();
     });
 
     it('updates the total supply', async () => {
-      assert.equal(await ERC20.totalSupply(), totalSupply);
+      assert.equal(await ERC20.totalSupply(), totalSupply.toString());
     });
 
     it('mints the right amount to the user', async () => {
-      assert.equal(await ERC20.balanceOf(user1.address), totalSupply);
+      assert.equal(await ERC20.balanceOf(user1.address), totalSupply.toString());
+    });
+
+    it('emits a Transfer event', async () => {
+      const event = findEvent({ receipt, eventName: 'Transfer' });
+
+      assert.equal(event.args.from, '0x0000000000000000000000000000000000000000');
+      assert.equal(event.args.to, user1.address);
+      assert.equal(event.args.amount, totalSupply.toString());
+    });
+
+    describe('when tokens are burned', () => {
+      const tokensToBurn = ethers.BigNumber.from('1000');
+      const newSupply = totalSupply.sub(tokensToBurn);
+
+      before('burn', async () => {
+        const tx = await ERC20.connect(user1).burn(tokensToBurn);
+        receipt = await tx.wait();
+      });
+
+      it('updates the total supply', async () => {
+        assert.equal(await ERC20.totalSupply(), newSupply.toString());
+      });
+
+      it('reduces the user balance', async () => {
+        assert.equal(await ERC20.balanceOf(user1.address), newSupply.toString());
+      });
+
+      it('emits a Transfer event', async () => {
+        const event = findEvent({ receipt, eventName: 'Transfer' });
+
+        assert.equal(event.args.from, user1.address);
+        assert.equal(event.args.to, '0x0000000000000000000000000000000000000000');
+        assert.equal(event.args.amount, tokensToBurn.toString());
+      });
     });
   });
 });
