dc643cb6260a435c1e7b213022db79f57a620023
Alejandro Santander <Palebluedot@gmail.com>
2021-10-13 09:23:53 -0300

    Enables the usage of hardhat network in sample-project tests (#145)
    * Exposing a problem with hardhat network
    
    * Remove fn used for testing
    
    * Fix
    
    * Add links
    
    * Re-enable hardhat-network tests
    
    * Increase deployer tests timeout

--- packages/deployer/test/fixture-projects/sample-project/hardhat.config.js
+++ packages/deployer/test/fixture-projects/sample-project/hardhat.config.js
@@ -4,7 +4,7 @@ require('../../..');
 module.exports = {
   solidity: '0.8.4',
   deployer: {},
-  defaultNetwork: 'local',
+  defaultNetwork: 'hardhat',
   networks: {
     local: {
       url: 'http://localhost:8545',
