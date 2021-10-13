00622158b380f319e0ef1fc7d852e76c8cc4f408
Thanos Polychronakis <thanpolas@gmail.com>
2021-10-13 10:59:49 +0300

    create the EIP2612 mock

---
+++ packages/core-contracts/contracts/mocks/token/ERC20-EIP2612Mock.sol
@@ -0,0 +1,100 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "../../token/ERC20-EIP2612.sol";
+
+contract ERC20_2612Mock is ERC20_2612 {
+    // solhint-disable no-empty-blocks
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    ) ERC20_2612(name, symbol, decimals) {}
+
+    // solhint-enable
+
+    function mint(uint256 amount) external {
+        _mint(msg.sender, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function getDigest(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline
+    ) public view returns (bytes32) {
+        uint256 nonce = nonces[owner];
+
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonce, deadline))
+            )
+        );
+
+        return digest;
+    }
+
+    function getPacked(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline
+    ) public view returns (bytes memory) {
+        uint256 nonce = nonces[owner];
+
+        bytes memory packed = abi.encodePacked(
+            "\x19\x01",
+            DOMAIN_SEPARATOR,
+            keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonce, deadline))
+        );
+
+        return packed;
+    }
+
+    function getAbi(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline
+    ) public view returns (bytes memory) {
+        uint256 nonce = nonces[owner];
+
+        bytes memory packed = abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonce, deadline);
+
+        return packed;
+    }
+}
+
+contract HandlerMock {
+    bytes4 private constant _SELECTOR_PERMIT =
+        bytes4(keccak256(bytes("permit(address,address,uint256,uint256,uint8,bytes32,bytes32)")));
+    bytes4 private constant _SELECTOR_TRANSFER_FROM = bytes4(keccak256(bytes("transferFrom(address,address,uint256)")));
+
+    function approveAndReceive(
+        address token,
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public {
+        (bool successPermit, bytes memory dataPermit) = token.call(
+            abi.encodeWithSelector(_SELECTOR_PERMIT, owner, spender, value, deadline, v, r, s)
+        );
+        require(successPermit && (dataPermit.length == 0 || abi.decode(dataPermit, (bool))), "TRANSFER_FAILED_PERMIT");
+
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSelector(_SELECTOR_TRANSFER_FROM, owner, address(this), value)
+        );
+
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "TRANSFER_FAILED");
+    }
+}
