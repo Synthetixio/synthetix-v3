3c09e89b76a9eb946709e2b46583d049cc1a0ea5
Leonardo Massazza <lmassazza+github@gmail.com>
2021-10-13 17:30:59 -0300

    linter rules :)

--- packages/core-js/test/utils/storage-struct.test.js
+++ packages/core-js/test/utils/storage-struct.test.js
@@ -1,8 +1,5 @@
 const { equal, notEqual } = require('assert/strict');
-const {
-  buildContractsStructMap,
-} = require('../../utils/ast/storage-struct');
-const storage_struct = require('../../utils/ast/storage-struct');
+const { buildContractsStructMap } = require('../../utils/ast/storage-struct');
 const asts = require('../fixtures/asts.json');
 
 describe('utils/ast/storage-struct.js', function () {
@@ -10,10 +7,10 @@ describe('utils/ast/storage-struct.js', function () {
     let structsMap;
     before('builds a contract struct map from an asts object', async () => {
       structsMap = await buildContractsStructMap(asts);
-    })
+    });
     it('should have 4 structs', () => {
       equal(structsMap.length, 4, 'structsMap should have 4 structs');
-    })
+    });
     it('all strutcs should conform with the basic format', () => {
       for (const contractStruct of structsMap) {
         notEqual(contractStruct.contract, undefined);
@@ -23,18 +20,22 @@ describe('utils/ast/storage-struct.js', function () {
         notEqual(contractStruct.struct.name, undefined);
         notEqual(contractStruct.struct.members, undefined);
       }
-    })
+    });
     it('should find all the members', () => {
       const contractStruct = structsMap.find((item) => item.contract.name === 'ProxyNamespace');
       notEqual(contractStruct, undefined);
       equal(
-        contractStruct.struct.members.some((v) => v.name === 'implementation' && v.type === 'address'),
+        contractStruct.struct.members.some(
+          (v) => v.name === 'implementation' && v.type === 'address'
+        ),
         true
       );
       equal(
-        contractStruct.struct.members.some((v) => v.name === 'simulatingUpgrade' && v.type === 'bool'),
+        contractStruct.struct.members.some(
+          (v) => v.name === 'simulatingUpgrade' && v.type === 'bool'
+        ),
         true
       );
-    })
+    });
   });
 });
