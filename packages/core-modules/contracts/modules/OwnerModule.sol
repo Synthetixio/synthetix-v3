29a5b0cbca74f64bb711afab1ec859aab497ba8c
Matías <mjlescano@protonmail.com>
2021-10-14 13:48:06 -0300

    add core-modules package

---
+++ packages/core-modules/contracts/modules/OwnerModule.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@synthetixio/core-contracts/contracts/ownership/Ownable.sol";
+import "../storage/OwnerStorage.sol";
+
+contract OwnerModule is Ownable, OwnerStorage {
+    function _setOwner(address newOwner) internal override {
+        _ownerStorage().owner = newOwner;
+    }
+
+    function _getOwner() internal view override returns (address) {
+        return _ownerStorage().owner;
+    }
+
+    function _setNominatedOwner(address newNominatedOwner) internal override {
+        _ownerStorage().nominatedOwner = newNominatedOwner;
+    }
+
+    function _getNominatedOwner() internal view override returns (address) {
+        return _ownerStorage().nominatedOwner;
+    }
+}
