297bee1ae644b05f99d4697537323e0d26494b6f
Leonardo Massazza <lmassazza+github@gmail.com>
2021-10-15 16:29:48 -0300

    more tests

--- packages/deployer/test/src/internal/storage-ast-validator.test.js
+++ packages/deployer/test/src/internal/storage-ast-validator.test.js
@@ -20,8 +20,53 @@ describe('internal/storage-ast-validator.js', function () {
       errorsFound.push(...(await validator.findInvalidNamespaceMutations()));
     });
 
-    it('shold not find errors in a normal deployment', () => {
+    it('should not find errors in a normal deployment', () => {
       equal(errorsFound.length, 0);
+    });
+  });
+
+  describe('change slot address', async () => {
+    let currentAsts, previousAsts, validator;
+    let errorsFound = [];
+
+    before('set asts and validator', () => {
+      currentAsts = clone(asts);
+      previousAsts = clone(asts);
+      currentAsts[
+        'ProxyNamespace'
+      ].nodes[1].nodes[1].body.statements[0].AST.statements[0].value.value =
+        '0x9dbde58b6f7305fccdc5abd7ea1096e84de3f9ee47d83d8c3efc3e5557ac9c00';
+      validator = new ModuleStorageASTValidator(currentAsts, previousAsts);
+    });
+
+    before('use the validator to find problems', async () => {
+      errorsFound = validator.findNamespaceSlotChanges();
+    });
+
+    it('should find a change in the slot', () => {
+      equal(errorsFound.length, 1);
+    });
+  });
+
+  describe('duplicate slot address', async () => {
+    let currentAsts, previousAsts, validator;
+    let errorsFound = [];
+
+    before('set asts and validator', () => {
+      currentAsts = clone(asts);
+      currentAsts[
+        'ProxyNamespace'
+      ].nodes[1].nodes[1].body.statements[0].AST.statements[0].value.value =
+        '0x1f33674ed9c09f309c0798b8fcbe9c48911f48b2defee8aecb930c5ef6f80e37';
+      validator = new ModuleStorageASTValidator(currentAsts, previousAsts);
+    });
+
+    before('use the validator to find problems', async () => {
+      errorsFound = validator.findNamespaceCollisions();
+    });
+
+    it('should find a duplicate address', () => {
+      equal(errorsFound.length, 1);
     });
   });
 });
