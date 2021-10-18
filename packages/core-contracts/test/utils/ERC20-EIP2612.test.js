cd0094f5238703f418206beb3bce08f396ff52ef
Thanos Polychronakis <thanpolas@gmail.com>
2021-10-18 10:47:21 +0300

    add clarification on precalulated hash on test

--- packages/core-contracts/test/utils/ERC20-EIP2612.test.js
+++ packages/core-contracts/test/utils/ERC20-EIP2612.test.js
@@ -46,6 +46,7 @@ describe('ERC20Permit - EIP2612', () => {
   const value = 42;
   const maxDeadline = 1734045861;
 
+  // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)")
   const TYPE_HASH = '0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9';
 
   function produceDigest(domainSeperator, nonce) {
