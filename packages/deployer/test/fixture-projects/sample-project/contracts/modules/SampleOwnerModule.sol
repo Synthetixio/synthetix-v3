10ef6dbac4a48f3280b73eacdc2c2afe8ffaffdc
Matías <mjlescano@protonmail.com>
2021-10-20 16:14:41 -0300

    rename to SampleOwnerModule

--- packages/deployer/test/fixture-projects/sample-project/contracts/modules/SampleOwnerModule.sol
+++ packages/deployer/test/fixture-projects/sample-project/contracts/modules/SampleOwnerModule.sol
@@ -3,4 +3,4 @@ pragma solidity ^0.8.0;
 
 import "@synthetixio/core-modules/contracts/modules/OwnerModule.sol";
 
-contract OwnerInitializerModule is OwnerModule {}
+contract SampleOwnerModule is OwnerModule {}
