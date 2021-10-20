21b4a17a3e591db025cb1b21c5610251fd216e86
Matías <mjlescano@protonmail.com>
2021-10-20 10:57:36 -0300

    remove OwnerModule code from sample-project

--- packages/deployer/test/fixture-projects/sample-project/contracts/modules/UpgradeModule.sol
+++ packages/deployer/test/fixture-projects/sample-project/contracts/modules/UpgradeModule.sol
@@ -1,9 +1,9 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "../mixins/OwnerMixin.sol";
-import "../storage/ProxyStorage.sol";
 import "@synthetixio/core-contracts/contracts/proxy/UniversalProxyImplementation.sol";
+import "@synthetixio/core-modules/contracts/mixins/OwnerMixin.sol";
+import "../storage/ProxyStorage.sol";
 
 contract UpgradeModule is UniversalProxyImplementation, ProxyStorage, OwnerMixin {
     function upgradeTo(address newImplementation) public override onlyOwner {
