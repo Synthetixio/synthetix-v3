a4cb1623b59637c05788944146647ddc218a2e7c
i-stam <i.stamelakos@gmail.com>
2021-10-22 13:02:28 +0100

    Add tests for beacon proxy

--- packages/core-contracts/test/proxy/BeaconProxy.test.js
+++ packages/core-contracts/test/proxy/BeaconProxy.test.js
@@ -1,8 +1,9 @@
 const { ethers } = hre;
 const assert = require('assert');
+const { artifacts } = require('hardhat');
 
-describe('BeaconProxy', () => {
-  let Beacon, BeaconProxy, Implementation;
+describe.only('BeaconProxy', () => {
+  let Beacon, BeaconProxy, MockedBeaconProxy, Implementation;
 
   let owner;
 
@@ -22,6 +23,13 @@ describe('BeaconProxy', () => {
 
       factory = await ethers.getContractFactory('BeaconProxyMock');
       BeaconProxy = await factory.deploy(Beacon.address);
+
+      const ImplementationContract = await hre.artifacts.readArtifact('ImplementationMockA');
+      MockedBeaconProxy = new ethers.Contract(
+        BeaconProxy.address,
+        ImplementationContract.abi,
+        owner
+      );
     });
 
     it('shows that the Beacon is set correctly', async () => {
@@ -30,6 +38,18 @@ describe('BeaconProxy', () => {
 
     it('returns the correct implementation', async () => {
       assert.equal(await BeaconProxy.getImplementation(), Implementation.address);
+    });
+
+    describe('when interacting with the implementation via the beaconproxy', async () => {
+      describe('when setting and reading a value that exists in the implementation', () => {
+        before('set a value and send ETH', async () => {
+          await (await MockedBeaconProxy.setA(1337)).wait();
+        });
+
+        it('can read the value set', async () => {
+          assert.equal(await MockedBeaconProxy.getA(), 1337);
+        });
+      });
     });
   });
 });
