126620035e49ed0ddd322cab25a1d74c4ebcd6e3
Matías <mjlescano@protonmail.com>
2021-10-25 15:08:57 -0300

    remove deployer files

--- packages/core-modules/contracts/Proxy.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-import "./BaseProxy.sol";
-
-contract Proxy is BaseProxy {
-    // solhint-disable-next-line no-empty-blocks
-    constructor(address firstImplementation) BaseProxy(firstImplementation) {}
-}
