f367a42194544bf85c5e4989ae6b2427e468c5c1
Matías <mjlescano@protonmail.com>
2021-10-27 14:46:31 -0300

    add namespace collision integration test

---
+++ packages/deployer/test/fixture-projects/namespace-collision/test/helpers/initializer.js
@@ -0,0 +1,29 @@
+const { getProxyAddress } = require('@synthetixio/deployer/utils/deployments');
+const { TASK_DEPLOY } = require('@synthetixio/deployer/task-names');
+
+/**
+ * Initializer helper for the sample-project, allows to deploy and initialize
+ * the project on the given hardhat environment
+ * @param {import('hardhat/types').HardhatRuntimeEnvironment}
+ */
+module.exports = function createInitializer(hre) {
+  const deploymentInfo = {
+    network: hre.config.defaultNetwork,
+    instance: 'test',
+  };
+
+  return {
+    deploymentInfo,
+
+    async deploySystem(customOptions = {}) {
+      await hre.run(TASK_DEPLOY, {
+        ...deploymentInfo,
+        noConfirm: true,
+        quiet: true,
+        ...customOptions,
+      });
+    },
+
+    async initSystem() {},
+  };
+};
