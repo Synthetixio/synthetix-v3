85a0eb054fa55886f3b5c34ea41e3ff55849fe57
i-stam <i.stamelakos@gmail.com>
2021-11-02 22:46:32 +0000

    Modify BeaconProxy to have its own storage

--- packages/core-contracts/contracts/proxy/BeaconProxy.sol
+++ packages/core-contracts/contracts/proxy/BeaconProxy.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.8.0;
 import "./ForwardingProxy.sol";
 import "../common/CommonErrors.sol";
 import "../interfaces/IBeacon.sol";
-import "../ownership/OwnableMixin.sol";
 import "../utils/ContractUtil.sol";
 
-abstract contract BeaconProxy is ForwardingProxy {
+contract BeaconProxy is ForwardingProxy, ContractUtil, CommonErrors {
+    event NewBeacon(address indexed newBeacon);
+
+    struct BeaconSlot {
+        address beaconAddress;
+    }
+
     // solhint-disable-next-line no-empty-blocks
-    function _getBeacon() internal view virtual returns (address) {}
+    function _getBeacon() internal view virtual returns (address) {
+        return _beaconProxyStorage().beaconAddress;
+    }
 
     function _getImplementation() internal view override returns (address) {
         return IBeacon(_getBeacon()).getImplementation();
@@ -18,4 +25,22 @@ abstract contract BeaconProxy is ForwardingProxy {
     // the implementation can be set only by the Beacon
     // solhint-disable-next-line no-empty-blocks
     function _setImplementation(address newImplementation) internal override {}
+
+    function _setBeacon(address newBeacon) internal {
+        if (newBeacon == address(0)) {
+            revert InvalidAddress(newBeacon);
+        }
+        if (!_isContract(newBeacon)) {
+            revert InvalidContract(newBeacon);
+        }
+        _beaconProxyStorage().beaconAddress = newBeacon;
+        emit NewBeacon(newBeacon);
+    }
+
+    function _beaconProxyStorage() internal pure returns (BeaconSlot storage beacon) {
+        assembly {
+            // bytes32(uint(keccak256("io.synthetix.beaconproxy")) - 1)
+            beacon.slot := 0x2395510979f6f33ca6b2853f771301ac5a746521967963945952ce833e51405c
+        }
+    }
 }
