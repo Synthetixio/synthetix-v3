4fb9248fc9fcc146415d2d4dd56436ea4bd4da4b
Alejandro <palebluedot@gmail.com>
2021-11-06 12:06:44 -0300

    Fixes to sample projects for compilation after proxy refactor

--- packages/deployer/test/fixture-projects/namespace-collision/contracts/Proxy.sol
+++ packages/deployer/test/fixture-projects/namespace-collision/contracts/Proxy.sol
@@ -1,9 +1,9 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@synthetixio/core-modules/contracts/BaseProxy.sol";
+import "@synthetixio/core-contracts/contracts/proxy/UUPSProxy.sol";
 
-contract Proxy is BaseProxy {
+contract Proxy is UUPSProxy {
     // solhint-disable-next-line no-empty-blocks
-    constructor(address firstImplementation) BaseProxy(firstImplementation) {}
+    constructor(address firstImplementation) UUPSProxy(firstImplementation) {}
 }
