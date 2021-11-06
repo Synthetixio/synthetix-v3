a1bd9fd68d26cd383468601b012a3904b7e76163
Alejandro <palebluedot@gmail.com>
2021-11-06 12:09:32 -0300

    Linter fixes

--- packages/synthetix-main/contracts/Proxy.sol
+++ packages/synthetix-main/contracts/Proxy.sol
@@ -6,7 +6,5 @@ import "@synthetixio/core-contracts/contracts/proxy/UUPSProxy.sol";
 // solhint-disable-next-line no-empty-blocks
 contract Proxy is UUPSProxy {
     // solhint-disable-next-line no-empty-blocks
-    constructor(address firstImplementation) UUPSProxy(firstImplementation) {
-
-    }
+    constructor(address firstImplementation) UUPSProxy(firstImplementation) {}
 }
