3bf6cad5ac4427f264a5d08021e2debb12a68132
Alejandro <palebluedot@gmail.com>
2021-11-08 13:04:21 -0300

    Minor linter fix

--- packages/deployer/test/fixture-projects/sample-project/contracts/Proxy.sol
+++ packages/deployer/test/fixture-projects/sample-project/contracts/Proxy.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@synthetixio/core-contracts/contracts/proxy/UUPSProxy.sol";
 
-// solhint-disable-next-line no-empty-blocks
 contract Proxy is UUPSProxy {
     // solhint-disable-next-line no-empty-blocks
     constructor(address firstImplementation) UUPSProxy(firstImplementation) {}
