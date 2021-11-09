bb08eee3ae662e5f17e0e35921c57de14938b78e
Leonardo Massazza <leo@synthetix.io>
2021-11-09 11:59:23 -0300

    Replace constructor by initilize in ERC20 (#366)
    * Replace constructor by initilize in ERC20

--- packages/core-contracts/contracts/test/token/ERC20Mock.sol
+++ packages/core-contracts/contracts/test/token/ERC20Mock.sol
@@ -4,14 +4,13 @@ pragma solidity ^0.8.0;
 import "../../token/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
-    // solhint-disable no-empty-blocks
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint8 decimals
-    ) ERC20(name, symbol, decimals) {}
-
-    // solhint-enable
+    function initialize(
+        string memory tokenName,
+        string memory tokenSymbol,
+        uint8 tokenDecimals
+    ) public {
+        _initialize(tokenName, tokenSymbol, tokenDecimals);
+    }
 
     function mint(uint256 amount) external {
         _mint(msg.sender, amount);
