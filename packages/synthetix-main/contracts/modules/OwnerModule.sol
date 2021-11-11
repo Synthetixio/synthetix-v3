e5943979c8f8713af139f746c237eea72d26d053
Alejandro Santander <Palebluedot@gmail.com>
2021-11-11 16:21:40 -0300

    Adds a CoreOwnerModule (#389)
    * Adds a CoreOwnerModule
    
    * Linter fix

--- packages/synthetix-main/contracts/modules/OwnerModule.sol
+++ packages/synthetix-main/contracts/modules/OwnerModule.sol
@@ -1,9 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@synthetixio/core-contracts/contracts/ownership/Ownable.sol";
+import "@synthetixio/core-modules/contracts/modules/CoreOwnerModule.sol";
 
 // solhint-disable-next-line no-empty-blocks
-contract OwnerModule is Ownable {
+contract OwnerModule is CoreOwnerModule {
 
 }
