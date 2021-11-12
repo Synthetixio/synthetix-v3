e9e0c6ce546d64067520194857d3af25ad5feff3
i-stam <i.stamelakos@gmail.com>
2021-11-12 17:48:09 +0000

    Refactor SynthsModule

--- packages/synthetix-main/contracts/modules/SynthsModule.sol
+++ packages/synthetix-main/contracts/modules/SynthsModule.sol
@@ -1,37 +1,73 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@synthetixio/core-contracts/contracts/interfaces/IBeacon.sol";
+import "@synthetixio/core-contracts/contracts/proxy/Beacon.sol";
 import "@synthetixio/core-contracts/contracts/ownership/OwnableMixin.sol";
 import "@synthetixio/core-contracts/contracts/proxy/BeaconProxy.sol";
 import "../storage/SynthsModuleStorage.sol";
 
 contract SynthsModule is OwnableMixin, SynthsModuleStorage {
-    error SynthExists();
+    error BeaconAlreadyDeployed();
+    error BeaconNotDeployed();
+    error SynthAlreadyDeployed();
 
-    event NewSynthDeployed(bytes32 synth, address synthProxyAddress);
+    event BeaconDeployed(address beacon);
+    event SynthDeployed(bytes32 synth, address synthProxyAddress);
 
-    function deployNewSynth(bytes32 synth) external onlyOwner {
+    function SynthsModuleInit(address synthsImplemention) external onlyOwner {
+        _deployBeacon();
+        _upgradeSynthImplementation(synthsImplemention);
+    }
+
+    function deployBeacon() external onlyOwner {
+        _deployBeacon();
+    }
+
+    function deploySynth(bytes32 synth) external onlyOwner {
         if (_synthsModuleStore().synthProxies[synth] != address(0x0)) {
-            revert SynthExists();
+            revert SynthAlreadyDeployed();
+        }
+        // get the Beacon address and check if it has been deployed properly
+        address beaconAddress = _synthsModuleStore().beacon;
+        if (beaconAddress == address(0)) {
+            revert BeaconNotDeployed();
         }
         // deploy a BeaconProxy with the right Beacon address
-        address beaconAddress = _synthsModuleStore().beacon;
-        // TODO: check if beacon is not set?
         BeaconProxy synthProxy = new BeaconProxy(beaconAddress);
         // get the proxy address
         address synthProxyAddress = address(synthProxy);
         // register the new proxy in the mapping
         _synthsModuleStore().synthProxies[synth] = synthProxyAddress;
-        emit NewSynthDeployed(synth, synthProxyAddress);
+        emit SynthDeployed(synth, synthProxyAddress);
         // TODO: initialize Synth
     }
 
     function upgradeSynthImplementation(address newSynthsImplementation) external onlyOwner {
-        IBeacon(_synthsModuleStore().beacon).upgradeTo(newSynthsImplementation);
+        _upgradeSynthImplementation(newSynthsImplementation);
+    }
+
+    function getBeacon() external view returns (address) {
+        return _synthsModuleStore().beacon;
     }
 
     function getSynthProxy(bytes32 synth) external view returns (address) {
         return _synthsModuleStore().synthProxies[synth];
+    }
+
+    function _deployBeacon() internal {
+        SynthsModuleStore storage store = _synthsModuleStore();
+        if (store.beacon != address(0)) {
+            revert BeaconAlreadyDeployed();
+        }
+        address beacon = address(new Beacon(address(this)));
+        store.beacon = beacon;
+        emit BeaconDeployed(beacon);
+    }
+
+    function _upgradeSynthImplementation(address newSynthsImplementation) internal {
+        if (_synthsModuleStore().beacon != address(0)) {
+            _deployBeacon();
+        }
+        Beacon(_synthsModuleStore().beacon).upgradeTo(newSynthsImplementation);
     }
 }
