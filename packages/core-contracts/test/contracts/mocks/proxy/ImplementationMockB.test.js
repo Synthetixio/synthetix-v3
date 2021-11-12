1b5df3d55e0c21de872da2997a064f97ec53f344
Matías <mjlescano@protonmail.com>
2021-11-12 09:32:12 -0300

    remove unused hook

--- packages/core-contracts/test/contracts/mocks/proxy/ImplementationMockB.test.js
+++ packages/core-contracts/test/contracts/mocks/proxy/ImplementationMockB.test.js
@@ -10,8 +10,6 @@ describe('ImplementationMockB', () => {
   });
 
   describe('when interacting with ImplementationMockB', () => {
-    before('set a value', async () => {});
-
     it('can read the value set on A', async () => {
       await (await ImplementationMockB.setA(42)).wait();
       assert.equal(await ImplementationMockB.getA(), 42);
