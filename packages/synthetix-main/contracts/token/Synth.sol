27001b48eac3e46fd9352245ecc91860bca2ea70
i-stam <i.stamelakos@gmail.com>
2021-11-18 14:26:32 +0000

    Add synth tests

--- packages/synthetix-main/contracts/token/Synth.sol
+++ packages/synthetix-main/contracts/token/Synth.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "@synthetixio/core-contracts/contracts/token/ERC20.sol";
+import "../interfaces/ISynth.sol";
 
-contract Synth is ERC20 {
+contract Synth is ERC20, ISynth {
     function initialize(
         string memory synthName,
         string memory synthSymbol,
         uint8 synthDecimals
-    ) external {
+    ) external override {
         _initialize(synthName, synthSymbol, synthDecimals);
     }
 }
