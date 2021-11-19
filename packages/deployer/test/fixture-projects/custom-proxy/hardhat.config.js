ddf67275b6bfb3ea40e9e82379be5a734e54330d
Matías <mjlescano@protonmail.com>
2021-11-19 12:14:05 -0300

    fix proxy  name

--- packages/deployer/test/fixture-projects/custom-proxy/hardhat.config.js
+++ packages/deployer/test/fixture-projects/custom-proxy/hardhat.config.js
@@ -3,11 +3,6 @@ require('../../..');
 
 module.exports = {
   solidity: '0.8.4',
-  networks: {
-    local: {
-      url: 'http://localhost:8545',
-    },
-  },
   deployer: {
     proxyContract: 'CustomProxy',
   },
