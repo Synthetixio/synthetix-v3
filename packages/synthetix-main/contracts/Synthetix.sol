487f3b19d798a11d64607a570b7a445694d62fcf
Matías <mjlescano@protonmail.com>
2021-11-19 16:10:25 -0300

    update synhtetix main Proxy to Synthetix

--- packages/synthetix-main/contracts/Synthetix.sol
+++ packages/synthetix-main/contracts/Synthetix.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@synthetixio/core-contracts/contracts/proxy/UUPSProxy.sol";
 
-// solhint-disable-next-line no-empty-blocks
-contract Proxy is UUPSProxy {
+contract Synthetix is UUPSProxy {
     // solhint-disable-next-line no-empty-blocks
     constructor(address firstImplementation) UUPSProxy(firstImplementation) {}
 }
