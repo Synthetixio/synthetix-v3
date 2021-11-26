2f8122e87b5ab7d43d28c9602747a2ef578feccb
Yannis <i.stamelakos@gmail.com>
2021-11-26 16:36:15 +0000

    Bootstrap CLI package (#475)
    * Bootstrap CLI package
    
    * Import plugin in synthetix-main
    
    * Fix linter errors
    
    * Address PR comments
    
    * fix cli dependencies
    
    * fix package-lock file and rebase
    
    Co-authored-by: Matías <mjlescano@protonmail.com>

---
+++ packages/cli/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+// Forces "use strict" on all modules.
+require('use-strict');
+
+// Loads all plugin files.
+['tasks'].forEach((folder) =>
+  fs
+    .readdirSync(path.join(__dirname, folder))
+    .forEach((mod) => require(path.join(__dirname, folder, mod)))
+);
