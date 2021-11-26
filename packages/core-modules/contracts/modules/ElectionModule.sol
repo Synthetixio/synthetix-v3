9255204c3b467daaca6360dd650fe2e996fab466
Matías <mjlescano@protonmail.com>
2021-11-26 10:18:21 -0300

    add ElectionModule interface

--- packages/core-modules/contracts/modules/ElectionModule.sol
+++ packages/core-modules/contracts/modules/ElectionModule.sol
@@ -3,4 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@synthetixio/core-contracts/contracts/token/ERC20.sol";
 
-contract ElectionModule is ERC20 {}
+contract ElectionModule is ERC20 {
+    function getNominees() public view returns (address[]);
+
+    function selfNominate() external;
+
+    function selfUnnominate() external;
+}
