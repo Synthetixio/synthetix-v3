f162942dc5490065dca33c69f860010602108358
i-stam <i.stamelakos@gmail.com>
2021-11-29 16:42:15 +0000

    WIP: prepare for adding pick-contract task

--- packages/cli/task-names.js
+++ packages/cli/task-names.js
@@ -1,3 +1,4 @@
 module.exports = {
+  SUBTASK_PICK_CONTRACT: 'pick-contract',
   TASK_INTERACT: 'interact',
 };
--- packages/cli/tasks/interact.js
+++ packages/cli/tasks/interact.js
@@ -1,5 +1,9 @@
 const { task } = require('hardhat/config');
 
-const { TASK_INTERACT } = require('../task-names');
+const { SUBTASK_PICK_CONTRACT, TASK_INTERACT } = require('../task-names');
 
-task(TASK_INTERACT, 'Interacts with a given modular system deployment');
+task(TASK_INTERACT, 'Interacts with a given modular system deployment')
+  .addOptionalParam('deploymentPath', 'Specify the path to the deployment data directory')
+  .setAction(async (taskArguments, hre) => {
+    await hre.run(SUBTASK_PICK_CONTRACT, taskArguments);
+  });
