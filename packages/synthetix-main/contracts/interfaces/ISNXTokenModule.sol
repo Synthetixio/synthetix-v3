8ceb90fecdeb9a2178a9bbd4da9c2810e2ae4517
Alejandro <palebluedot@gmail.com>
2021-11-30 14:36:42 -0300

    Fix

--- packages/synthetix-main/contracts/interfaces/ISNXTokenModule.sol
+++ packages/synthetix-main/contracts/interfaces/ISNXTokenModule.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface SNXTokenModule {
+interface ISNXTokenModule {
     error SNXAlreadyCreated();
 
     event SNXTokenCreated(address snxAddress);
