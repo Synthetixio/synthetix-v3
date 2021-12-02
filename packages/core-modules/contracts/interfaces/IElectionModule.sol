98ee6eace842b67343a09b719445a56578ccd374
Matías Lescano <mjlescano@users.noreply.github.com>
2021-12-02 15:22:28 -0300

    Add election nominees (#511)
    * add empty ElectionModule
    
    * add ElectionModule interface
    
    * add self nomination logic
    
    * fix tests
    
    * update fn public to external
    
    * add AddressSet util
    
    * remove OZ in favor of AddressSet util
    
    * remove AddressSet in favor of inlining array modifications
    
    * add ZeroAddress validation
    
    * Remove AddressSetMock
    
    * fix election storage
    
    * update ElectionModule interface
    
    * fix interface
    
    * fix tests
    
    * fix events declaration
    
    * update nomination fns namings
    
    * fix typo
    
    * improve test coverage
    
    * improve test coverage
    
    * remove unused test
    
    * remove address(0) checks
    
    * update tests cases namings

--- packages/core-modules/contracts/interfaces/IElectionModule.sol
+++ packages/core-modules/contracts/interfaces/IElectionModule.sol
@@ -2,13 +2,15 @@
 pragma solidity ^0.8.0;
 
 interface IElectionModule {
-    error MemberTokenAlreadyCreated();
-
-    event MemberTokenCreated(address memberTokenAddress);
-
     function createMemberToken(string memory tokenName, string memory tokenSymbol) external;
 
     function upgradeMemberTokenImplementation(address newMemberTokenImplementation) external;
 
     function getMemberTokenAddress() external view returns (address);
+
+    function getNominees() external view returns (address[] memory);
+
+    function nominate() external;
+
+    function withdrawNomination() external;
 }
--- packages/core-modules/contracts/modules/CoreElectionModule.sol
+++ packages/core-modules/contracts/modules/CoreElectionModule.sol
@@ -8,6 +8,13 @@ import "../token/MemberToken.sol";
 import "../storage/ElectionStorage.sol";
 
 contract CoreElectionModule is IElectionModule, ElectionStorage, OwnableMixin {
+    error MemberTokenAlreadyCreated();
+
+    error AlreadyNominated(address addr);
+    error NotNominated(address addr);
+
+    event MemberTokenCreated(address memberTokenAddress);
+
     function createMemberToken(string memory tokenName, string memory tokenSymbol) external override onlyOwner {
         ElectionStore storage store = _electionStore();
 
@@ -36,5 +43,51 @@ contract CoreElectionModule is IElectionModule, ElectionStorage, OwnableMixin {
 
     function getMemberTokenAddress() public view override returns (address) {
         return _electionStore().memberTokenAddress;
+    }
+
+    function getNominees() external view override returns (address[] memory) {
+        return _electionStore().nominees;
+    }
+
+    function nominate() external override {
+        ElectionStore storage store = _electionStore();
+
+        if (store.nomineeIndexes[msg.sender] != 0) {
+            revert AlreadyNominated(msg.sender);
+        }
+
+        store.nominees.push(msg.sender);
+        store.nomineeIndexes[msg.sender] = store.nominees.length;
+    }
+
+    function withdrawNomination() external override {
+        ElectionStore storage store = _electionStore();
+
+        uint256 valueIndex = store.nomineeIndexes[msg.sender];
+
+        if (valueIndex == 0) {
+            revert NotNominated(msg.sender);
+        }
+
+        uint256 toDeleteIndex = valueIndex - 1;
+        uint256 lastIndex = store.nominees.length - 1;
+
+        // If the address is not the last one on the Array, we have to move it to
+        // swap it with the last element, and then pop it, so we don't leave any
+        // empty spaces.
+        if (lastIndex != toDeleteIndex) {
+            address lastvalue = store.nominees[lastIndex];
+
+            // Move the last value to the index where the value to delete is
+            store.nominees[toDeleteIndex] = lastvalue;
+            // Update the index for the moved value
+            store.nomineeIndexes[lastvalue] = valueIndex; // Replace lastvalue's index to valueIndex
+        }
+
+        // Delete the slot where the moved value was stored
+        store.nominees.pop();
+
+        // Delete the index for the deleted slot
+        delete store.nomineeIndexes[msg.sender];
     }
 }
