a7b11204da9d4769463706a39e3486ebfd5f5051
Alejandro <palebluedot@gmail.com>
2021-12-03 10:55:39 -0300

    Show contract addresses

--- packages/cli/subtasks/pick-contract.js
+++ packages/cli/subtasks/pick-contract.js
@@ -1,12 +1,13 @@
 const { subtask } = require('hardhat/config');
 const { SUBTASK_PICK_CONTRACT } = require('../task-names');
 const inquirer = require('inquirer');
+const chalk = require('chalk');
 
 subtask(SUBTASK_PICK_CONTRACT, 'Pick contract to interact with').setAction(
   async (taskArguments, hre) => {
     const contractNames = Object.keys(hre.deployer.deployment.general.contracts);
 
-    const { contractName } = await inquirer.prompt([
+    let { contractName } = await inquirer.prompt([
       {
         type: 'autocomplete',
         name: 'contractName',
@@ -18,10 +19,17 @@ subtask(SUBTASK_PICK_CONTRACT, 'Pick contract to interact with').setAction(
             }
 
             return true;
+          }).map((contractName) => {
+            const contractAddress = hre.deployer.deployment.general.contracts[contractName].deployedAddress;
+
+            return `${contractName} ${chalk.gray(contractAddress)}`;
           });
         },
       },
     ]);
+
+    // Remove address
+    contractName = contractName.split(' ')[0];
 
     hre.cli.contractName = contractName;
   }
