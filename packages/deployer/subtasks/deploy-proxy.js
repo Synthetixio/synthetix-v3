df5a6a3fdd834214f3f44efc8a7720e944803ac1
Alejandro <palebluedot@gmail.com>
2021-12-06 09:46:07 -0300

    Store proxy in modules

--- packages/deployer/subtasks/deploy-proxy.js
+++ packages/deployer/subtasks/deploy-proxy.js
@@ -8,10 +8,26 @@ subtask(SUBTASK_DEPLOY_PROXY, 'Deploys the main proxy if needed').setAction(asyn
   const routerName = 'Router';
   const proxyName = hre.config.deployer.proxyContract;
 
+  await _deployProxy(proxyName, routerName, hre);
+
+  _setProxyOnModules(proxyName, hre);
+});
+
+async function _deployProxy(proxyName, routerName, hre) {
   const routerAddress = hre.deployer.deployment.general.contracts[routerName].deployedAddress;
 
   await hre.run(SUBTASK_DEPLOY_CONTRACT, {
     contractName: proxyName,
     constructorArgs: [routerAddress],
   });
-});
+}
+
+function _setProxyOnModules(proxyName, hre) {
+  const proxy = hre.deployer.deployment.general.contracts[proxyName];
+
+  Object.values(hre.deployer.deployment.general.contracts).map((contract) => {
+    if (contract.isModule) {
+      contract.proxyAddress = proxy.deployedAddress;
+    }
+  });
+}
