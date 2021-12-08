d9118a6ae78ff4df26ff7aac93a02bb4b12999da
Alejandro <palebluedot@gmail.com>
2021-12-08 18:57:05 -0300

    Prettier output on cli pick function

--- packages/cli/subtasks/pick-function.js
+++ packages/cli/subtasks/pick-function.js
@@ -1,16 +1,22 @@
 const { subtask } = require('hardhat/config');
 const { SUBTASK_PICK_FUNCTION } = require('../task-names');
 const prompts = require('prompts');
+const chalk = require('chalk');
 const { getSignatureWithParameterNamesAndValues } = require('../internal/signatures');
+const { getSelectors } = require('@synthetixio/core-js/utils/contracts');
 
 subtask(SUBTASK_PICK_FUNCTION, 'Pick a function from the given contract').setAction(
   async (taskArguments, hre) => {
     const abi = hre.deployer.deployment.abis[hre.cli.contractName];
     const abiFunctions = abi.filter((abiItem) => abiItem.name && abiItem.type === 'function');
+    const selectors = await getSelectors(abi);
 
     const choices = abiFunctions.map((abiItem) => {
+      const fullSignature = getSignatureWithParameterNamesAndValues(hre.cli.contractName, abiItem.name);
+      const selector = selectors.find((selector) => selector.name === abiItem.name).selector;
+
       return {
-        title: getSignatureWithParameterNamesAndValues(hre.cli.contractName, abiItem.name),
+        title: `${chalk.gray(hre.cli.contractName)}.${fullSignature} ${chalk.gray(selector)}`,
         value: abiItem.name,
       };
     });
