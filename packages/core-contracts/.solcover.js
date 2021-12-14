52544ab0ae6aaf34766845e8654489631f1aa749
Matías <mjlescano@protonmail.com>
2021-12-14 15:41:46 -0300

    remove statement and function coverage instrumentation

--- packages/core-contracts/.solcover.js
+++ packages/core-contracts/.solcover.js
@@ -1,5 +1,4 @@
 module.exports = {
-  skipFiles: ['contracts/Router.sol'],
   // Reduce instrumentation footprint - volume of solidity code
   // passed to compiler causes it to crash
   // Line and branch coverage will still be reported.
