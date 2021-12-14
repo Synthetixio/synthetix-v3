73394a418e7cd9a8edbfbfedc6167cb7b0a60df8
Alejandro <palebluedot@gmail.com>
2021-12-14 15:41:57 -0300

    Fix

--- packages/core-js/test/utils/assertions/assert-revert.test.js
+++ packages/core-js/test/utils/assertions/assert-revert.test.js
@@ -1,5 +1,5 @@
 const { rejects } = require('assert/strict');
-const assertRevert = require('../../utils/assertions/assert-revert');
+const assertRevert = require('../../../utils/assertions/assert-revert');
 
 function mockValidTx() {
   return Promise.resolve({ wait: () => Promise.resolve() });
