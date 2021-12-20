475dca8617b89c2e8e98e1119199ebc8fcd005d0
Alejandro <palebluedot@gmail.com>
2021-12-20 13:16:39 -0300

    Remove TODO in assert-revert

--- packages/core-js/utils/assertions/assert-revert.js
+++ packages/core-js/utils/assertions/assert-revert.js
@@ -12,12 +12,7 @@ module.exports = async function assertRevert(tx, expectedMessage) {
   } else if (expectedMessage) {
     const receivedMessage = error.toString();
 
-    // TODO The condition covering 'unrecognized' is added temporarily to hack another issue.
-    // Remove it when https://github.com/Synthetixio/synthetix-v3/issues/273 is resolved
-    if (
-      !receivedMessage.includes(expectedMessage) &&
-      !receivedMessage.includes('reverted with an unrecognized custom error')
-    ) {
+    if (!receivedMessage.includes(expectedMessage)) {
       throw new Error(
         `Transaction was expected to revert with "${expectedMessage}", but reverted with "${receivedMessage}"`
       );
