b585aa309c409721483a9f7fbbcbd1b02c4445ae
Alejandro <palebluedot@gmail.com>
2021-12-20 13:56:46 -0300

    Do not fail silently

--- packages/core-js/utils/assertions/assert-revert.js
+++ packages/core-js/utils/assertions/assert-revert.js
@@ -13,6 +13,15 @@ module.exports = async function assertRevert(tx, expectedMessage) {
     const receivedMessage = error.toString();
 
     if (!receivedMessage.includes(expectedMessage)) {
+      // ----------------------------------------------------------------------------
+      // TODO: Remove this check once the issue is solved in hardhat.
+      // https://github.com/nomiclabs/hardhat/issues/1996
+      if (receivedMessage.includes('reverted with an unrecognized custom error')) {
+        console.warn(`Unable to parse reverse reason.\n${receivedMessage}`);
+        return;
+      }
+      // ----------------------------------------------------------------------------
+
       throw new Error(
         `Transaction was expected to revert with "${expectedMessage}", but reverted with "${receivedMessage}"`
       );
