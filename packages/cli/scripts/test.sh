b212fd2bb3f154a48ac0d170d81c1cac4a1c9336
Alejandro Santander <Palebluedot@gmail.com>
2021-12-21 10:31:11 -0300

    Better way to run CLI tests (#673)

---
+++ packages/cli/scripts/test.sh
@@ -0,0 +1,13 @@
+set -x
+
+# Start local chain
+cd test/fixture-projects/sample-project
+npx hardhat node > /dev/null 2>&1 &
+sleep 10
+
+# Deploy on local chain
+npx hardhat deploy --network local --clear --quiet --no-confirm --instance test
+
+# Run tests
+cd ../../..
+npx mocha
