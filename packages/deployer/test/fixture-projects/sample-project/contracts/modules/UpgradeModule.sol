7604ddb4a8712788053e3bd3cd95835a99158d0b
Matías <mjlescano@protonmail.com>
2021-12-23 14:40:34 +0100

    fix lint issues

--- packages/deployer/test/fixture-projects/sample-project/contracts/modules/UpgradeModule.sol
+++ packages/deployer/test/fixture-projects/sample-project/contracts/modules/UpgradeModule.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { UpgradeModule as BaseUpgradeModule } from "@synthetixio/core-modules/contracts/modules/UpgradeModule.sol";
+import {UpgradeModule as BaseUpgradeModule} from "@synthetixio/core-modules/contracts/modules/UpgradeModule.sol";
 
 // solhint-disable-next-line no-empty-blocks
 contract UpgradeModule is BaseUpgradeModule {
