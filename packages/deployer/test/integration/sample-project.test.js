2ed2c0571365d9b158a415d689683b4f255167c3
Matías <mjlescano@protonmail.com>
2021-12-23 16:08:51 +0100

    refactor sample-project tests to remove a deployment

--- packages/deployer/test/integration/sample-project.test.js
+++ packages/deployer/test/integration/sample-project.test.js
@@ -9,69 +9,64 @@ describe('sample-project', function () {
     hre = loadEnvironment('sample-project');
   });
 
-  it('correctly executes several deployments with no changes', async function () {
-    this.timeout(120000);
-
-    // Initial deployment
-    await deployOnEnvironment(hre, {
-      alias: 'first',
-      clear: true,
-    });
-
-    // Second deployment, without any changes
-    await deployOnEnvironment(hre, {
-      alias: 'second',
-    });
-  });
-
-  it('correctly executes several deployments with changes', async function () {
-    this.timeout(120000);
-
-    // Initial deployment
-    await deployOnEnvironment(hre, {
-      alias: 'first',
-      clear: true,
-    });
-
-    // Second deployment, without any changes
-    await deployOnEnvironment(hre, {
-      alias: 'second',
-    });
-
-    // Third deployment, with changes
-    const MODULES = hre.config.deployer.paths.modules;
-    const CONTRACTS = path.join(hre.config.paths.root, 'test-contracts');
-
-    const SomeModuleOriginal = await readFile(path.join(MODULES, 'SomeModule.sol'));
-    const AnotherModuleOriginal = await readFile(path.join(MODULES, 'AnotherModule.sol'));
-    const SettingsModuleOriginal = await readFile(path.join(MODULES, 'SettingsModule.sol'));
-
-    try {
-      // Make some file changes before deploying
-      await Promise.all([
-        // Create new module
-        copyFile(path.join(CONTRACTS, 'NewModule.sol'), path.join(MODULES, 'NewModule.sol')),
-        // Modify existing modules
-        copyFile(
-          path.join(CONTRACTS, 'SomeModule.modified.sol'),
-          path.join(MODULES, 'SomeModule.sol')
-        ),
-        // Delete existing modules
-        unlink(path.join(MODULES, 'AnotherModule.sol')),
-        unlink(path.join(MODULES, 'SettingsModule.sol')),
-      ]);
+  describe('when deploying without any changes', function () {
+    it('correctly runs the initial deployment', async function () {
+      this.timeout(60000);
 
       await deployOnEnvironment(hre, {
-        alias: 'third',
+        alias: 'first',
+        clear: true,
       });
-    } finally {
-      // Restore all the changes
-      await Promise.all([
-        unlink(path.join(MODULES, 'NewModule.sol')),
-        writeFile(path.join(MODULES, 'SomeModule.sol'), SomeModuleOriginal),
-        writeFile(path.join(MODULES, 'AnotherModule.sol'), AnotherModuleOriginal),
-        writeFile(path.join(MODULES, 'SettingsModule.sol'), SettingsModuleOriginal),
-      ]);
-    }
+    });
+
+    it('correctly executes the second time and makes no changes', async function () {
+      this.timeout(60000);
+
+      await deployOnEnvironment(hre, {
+        alias: 'second',
+      });
+    });
+
+    describe('when changing files', function () {
+      it('correctly applies changes on deployment', async function () {
+        this.timeout(120000);
+
+        // Third deployment, with changes
+        const MODULES = hre.config.deployer.paths.modules;
+        const CONTRACTS = path.join(hre.config.paths.root, 'test-contracts');
+
+        const SomeModuleOriginal = await readFile(path.join(MODULES, 'SomeModule.sol'));
+        const AnotherModuleOriginal = await readFile(path.join(MODULES, 'AnotherModule.sol'));
+        const SettingsModuleOriginal = await readFile(path.join(MODULES, 'SettingsModule.sol'));
+
+        try {
+          // Make some file changes before deploying
+          await Promise.all([
+            // Create new module
+            copyFile(path.join(CONTRACTS, 'NewModule.sol'), path.join(MODULES, 'NewModule.sol')),
+            // Modify existing modules
+            copyFile(
+              path.join(CONTRACTS, 'SomeModule.modified.sol'),
+              path.join(MODULES, 'SomeModule.sol')
+            ),
+            // Delete existing modules
+            unlink(path.join(MODULES, 'AnotherModule.sol')),
+            unlink(path.join(MODULES, 'SettingsModule.sol')),
+          ]);
+
+          await deployOnEnvironment(hre, {
+            alias: 'third',
+          });
+        } finally {
+          // Restore all the changes
+          await Promise.all([
+            unlink(path.join(MODULES, 'NewModule.sol')),
+            writeFile(path.join(MODULES, 'SomeModule.sol'), SomeModuleOriginal),
+            writeFile(path.join(MODULES, 'AnotherModule.sol'), AnotherModuleOriginal),
+            writeFile(path.join(MODULES, 'SettingsModule.sol'), SettingsModuleOriginal),
+          ]);
+        }
+      });
+    });
   });
 });
