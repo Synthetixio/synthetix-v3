a2f94c6952f2aab4dbe2bc886aca90aee0155079
Leonardo Massazza <leo@synthetix.io>
2022-01-17 19:50:19 -0300

    Use initializableMixin where possible (#735)
    * initial commit

--- packages/core-modules/contracts/modules/OwnerModule.sol
+++ packages/core-modules/contracts/modules/OwnerModule.sol
@@ -2,8 +2,21 @@
 pragma solidity ^0.8.0;
 
 import "@synthetixio/core-contracts/contracts/ownership/Ownable.sol";
+import "@synthetixio/core-contracts/contracts/initializable/InitializableMixin.sol";
+import "../interfaces/IOwnerModule.sol";
 
 // solhint-disable-next-line no-empty-blocks
-contract OwnerModule is Ownable {
+contract OwnerModule is Ownable, IOwnerModule, InitializableMixin {
+    function _isInitialized() internal view override returns (bool) {
+        return _ownableStore().owner != address(0);
+    }
 
+    function isOwnerModuleInitialized() external view override returns (bool) {
+        return _isInitialized();
+    }
+
+    function initializeOwnerModule(address initialOwner) external override onlyIfNotInitialized {
+        nominateNewOwner(initialOwner);
+        acceptOwnership();
+    }
 }
