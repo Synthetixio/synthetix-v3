0b4ab6f6168e05cee2979f2ceefcd0f1e669fe55
Matías <mjlescano@protonmail.com>
2022-01-18 12:30:42 +0100

    add new sample token interface to sample project

--- packages/deployer/test/fixture-projects/sample-project/contracts/modules/SampleTokenModule.sol
+++ packages/deployer/test/fixture-projects/sample-project/contracts/modules/SampleTokenModule.sol
@@ -2,11 +2,27 @@
 pragma solidity ^0.8.0;
 
 import "@synthetixio/core-contracts/contracts/satellite/SatelliteFactory.sol";
+import "../storage/TokenStorage.sol";
 import "../token/SampleToken.sol";
 
-contract SampleTokenModule is SatelliteFactory {
-    function createSampleToken() external {
-        SampleToken sampleToken = new SampleToken();
-        emit SatelliteCreated("contracts/token/SampleToken.sol:SampleToken", address(sampleToken));
+contract SampleTokenModule is SatelliteFactory, TokenStorage {
+    function _getSatellites() internal view override returns (Satellite[] memory) {
+        return _tokenStore().satellites;
+    }
+
+    function getSampleTokenModuleSatellites() public view returns (Satellite[] memory) {
+        return _getSatellites();
+    }
+
+    function createSampleToken(string memory id) external {
+        SampleToken token = new SampleToken();
+
+        Satellite memory satellite = Satellite({
+            id: id,
+            contractName: type(SampleToken).name,
+            deployedAddress: address(token)
+        });
+
+        _setSatellite(satellite);
     }
 }
