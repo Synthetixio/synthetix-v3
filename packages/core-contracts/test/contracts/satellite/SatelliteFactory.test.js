92374a8960c2c0ddd6f72ba13e2ff81039a0dd81
Matías <mjlescano@protonmail.com>
2022-01-19 13:39:14 +0100

    fix core-contracts satellites tests

--- packages/core-contracts/test/contracts/satellite/SatelliteFactory.test.js
+++ packages/core-contracts/test/contracts/satellite/SatelliteFactory.test.js
@@ -11,15 +11,17 @@ describe('SatelliteFactory', () => {
     });
 
     describe('when creating a mocked satellite', () => {
+      const SAT = ethers.utils.formatBytes32String('sat');
+
       before('prepare sattelite', async () => {
-        const tx = await SatelliteFactory.createSatelliteMock('SomeSatellite');
+        const tx = await SatelliteFactory.createSatelliteMock(SAT);
         await tx.wait();
       });
 
       it('gets newly created satellite', async () => {
         const result = await SatelliteFactory.getSatellite();
 
-        assert.equal(result.id, 'SomeSatellite');
+        assert.equal(result.id, SAT);
         assert.equal(result.contractName, 'ERC20');
         assert.notEqual(result.deployedAddress, '0x0000000000000000000000000000000000000000');
       });
@@ -29,6 +31,9 @@ describe('SatelliteFactory', () => {
   describe('when managing multiple satellites', () => {
     let SatellitesFactory;
 
+    const FIRST = ethers.utils.formatBytes32String('fst');
+    const SECOND = ethers.utils.formatBytes32String('snd');
+
     before('deploy the contract', async () => {
       const factory = await ethers.getContractFactory('SatellitesFactoryMock');
       SatellitesFactory = await factory.deploy();
@@ -36,20 +41,20 @@ describe('SatelliteFactory', () => {
 
     describe('when creating a mocked satellite', () => {
       before('create sattelites', async () => {
-        await (await SatellitesFactory.createSatelliteMock('First')).wait();
-        await (await SatellitesFactory.createSatelliteMock('Second')).wait();
+        await (await SatellitesFactory.createSatelliteMock(FIRST)).wait();
+        await (await SatellitesFactory.createSatelliteMock(SECOND)).wait();
       });
 
       it('gets newly created satellite', async () => {
         const result = await SatellitesFactory.getSatellites();
 
         assert.equal(result.length, 2);
-        assert.equal(result[0].id, 'First');
+        assert.equal(result[0].id, FIRST);
         assert.equal(result[0].contractName, 'ERC20');
         assert.notEqual(result[0].deployedAddress, '0x0000000000000000000000000000000000000000');
 
         assert.equal(result.length, 2);
-        assert.equal(result[1].id, 'Second');
+        assert.equal(result[1].id, SECOND);
         assert.equal(result[1].contractName, 'ERC20');
         assert.notEqual(result[1].deployedAddress, '0x0000000000000000000000000000000000000000');
       });
