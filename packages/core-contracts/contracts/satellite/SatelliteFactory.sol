de93582bb53272969f39132e383268538f0a7c51
Matías <mjlescano@protonmail.com>
2022-01-20 11:22:56 +0100

    singularize satellite factories

--- packages/core-contracts/contracts/satellite/SatelliteFactory.sol
+++ packages/core-contracts/contracts/satellite/SatelliteFactory.sol
@@ -1,16 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-struct Satellite {
-    bytes32 id;
-    string contractName;
-    address deployedAddress;
-}
-
 abstract contract SatelliteFactory {
-    function _getSatellite() internal view virtual returns (Satellite memory);
-}
+    struct Satellite {
+        bytes32 id;
+        string contractName;
+        address deployedAddress;
+    }
 
-abstract contract SatellitesFactory {
     function _getSatellites() internal view virtual returns (Satellite[] memory);
 }
