47fc071f66859e861ecb23d8f286a58be8cfa570
Matías Lescano <mjlescano@users.noreply.github.com>
2022-01-20 09:42:30 -0300

    Fix sample-project tests (#747)
    * remove hardhat TASK_CLEAN job on clear-deployment
    
    * update default deployer mocha timeout

--- packages/deployer/subtasks/clear-deployments.js
+++ packages/deployer/subtasks/clear-deployments.js
@@ -1,6 +1,5 @@
 const del = require('del');
 const { subtask } = require('hardhat/config');
-const { TASK_CLEAN } = require('hardhat/builtin-tasks/task-names');
 
 const logger = require('@synthetixio/core-js/utils/io/logger');
 const prompter = require('@synthetixio/core-js/utils/io/prompter');
@@ -12,8 +11,6 @@ subtask(
   SUBTASK_CLEAR_DEPLOYMENTS,
   'Delete all previous deployment data on the current environment'
 ).setAction(async ({ instance }, hre) => {
-  await hre.run(TASK_CLEAN);
-
   const deploymentsFolder = getDeploymentFolder({
     folder: hre.config.deployer.paths.deployments,
     network: hre.network.name,
