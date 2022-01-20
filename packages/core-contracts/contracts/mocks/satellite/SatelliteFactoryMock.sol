e7915f8b06a56ad3aef370ea728a7254b3ad7d0b
Matías <mjlescano@protonmail.com>
2022-01-20 16:50:36 +0100

    remove unused import

--- packages/core-contracts/contracts/mocks/satellite/SatelliteFactoryMock.sol
+++ packages/core-contracts/contracts/mocks/satellite/SatelliteFactoryMock.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "../../token/ERC20.sol";
 import "../../satellite/SatelliteFactory.sol";
-import "../../utils/SetUtil.sol";
 
 contract SatelliteFactoryMock is SatelliteFactory {
     Satellite[] private _satellites;
@@ -19,11 +18,7 @@ contract SatelliteFactoryMock is SatelliteFactory {
     function createSatelliteMock(bytes32 name) external {
         ERC20 newSatellite = new ERC20();
 
-        Satellite memory satellite = Satellite({
-            name: name,
-            contractName: "ERC20",
-            deployedAddress: address(newSatellite)
-        });
+        Satellite memory satellite = Satellite({name: name, contractName: "ERC20", deployedAddress: address(newSatellite)});
 
         _satellites.push(satellite);
     }
