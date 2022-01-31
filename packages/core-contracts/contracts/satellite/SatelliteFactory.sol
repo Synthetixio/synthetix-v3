bbdf3bf102903915ff0042b2f633b2b1ca3437b9
Leonardo Massazza <lmassazza@gmail.com>
2022-01-31 11:11:28 -0300

    add missing interfaces

--- packages/core-contracts/contracts/satellite/SatelliteFactory.sol
+++ packages/core-contracts/contracts/satellite/SatelliteFactory.sol
@@ -1,12 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-abstract contract SatelliteFactory {
-    struct Satellite {
-        bytes32 name;
-        bytes32 contractName;
-        address deployedAddress;
-    }
+import "../interfaces/ISatelliteFactory.sol";
+
+abstract contract SatelliteFactory is ISatelliteFactory {
 
     function _getSatellites() internal view virtual returns (Satellite[] memory);
 }
