9f1a03dd5b0ba0a0a21e62618abea91f30949777
Alejandro <palebluedot@gmail.com>
2022-02-02 11:42:51 -0300

    Linter fixes

--- packages/core-modules/contracts/storage/ElectionStorage.sol
+++ packages/core-modules/contracts/storage/ElectionStorage.sol
@@ -7,26 +7,19 @@ contract ElectionStorage {
     struct ElectionStore {
         // True if initializeElectionModule was called
         bool initialized;
-
         // Current epoch index
         // First epoch is 1
         uint currentEpochIndex;
-
         // The address of the council NFT
         address councilToken;
-
         // Council member addresses
         SetUtil.AddressSet councilMembers;
-
         // Council token id's by council member address
         mapping(address => uint) councilTokenIds;
-
         // EpochData's by epoch index
         mapping(uint => EpochData) epochs;
-
         // ElectionData's by epoch index
         mapping(uint => ElectionData) elections;
-
         // Pointer to ElectionSettings
         // To be always used via store.settings[address(this)]
         mapping(address => ElectionSettings) settings;
@@ -35,19 +28,14 @@ contract ElectionStorage {
     struct ElectionSettings {
         // Number of council members in the next epoch
         uint8 nextEpochSeatCount;
-
         // Minimum epoch duration when adjusting schedules
         uint64 minEpochDuration;
-
         // Minimum nomination period duration when adjusting schedules
         uint64 minNominationPeriodDuration;
-
         // Minimum voting period duration when adjusting schedules
         uint64 minVotingPeriodDuration;
-
         // Maximum size for tweaking epoch schedules (see tweakEpochSchedule)
         uint64 maxDateAdjustmentTolerance;
-
         // Default batch size when calling evaluate() with numBallots = 0
         uint defaultBallotEvaluationBatchSize;
     }
@@ -55,13 +43,10 @@ contract ElectionStorage {
     struct EpochData {
         // Date at which the epoch started
         uint64 startDate;
-
         // Date at which the epoch's voting period will end
         uint64 endDate;
-
         // Date at which the epoch's nomination period will start
         uint64 nominationPeriodStartDate;
-
         // Date at which the epoch's voting period will start
         uint64 votingPeriodStartDate;
     }
@@ -69,28 +54,20 @@ contract ElectionStorage {
     struct ElectionData {
         // True if ballots have been counted in this election
         bool evaluated;
-
         // True if NFTs have been re-shuffled in this election
         bool resolved;
-
         // Number of counted ballots in this election
         uint numEvaluatedBallots;
-
         // List of nominated candidates in this election
         SetUtil.AddressSet nominees;
-
         // List of winners of this election (requires evaluation)
         SetUtil.AddressSet winners;
-
         // List of all ballot ids in this election
         bytes32[] ballotIds;
-
         // BallotData by ballot id
         mapping(bytes32 => BallotData) ballotsById;
-
         // Ballot id that each user voted on
         mapping(address => bytes32) ballotIdsByAddress;
-
         // Number of votes for each candidate
         mapping(address => uint) candidateVotes;
     }
@@ -98,7 +75,6 @@ contract ElectionStorage {
     struct BallotData {
         // Total accumulated votes in this ballot (needs evaluation)
         uint votes;
-
         // List of candidates in this ballot
         address[] candidates;
     }
@@ -106,16 +82,12 @@ contract ElectionStorage {
     enum ElectionPeriod {
         // Not initialized
         Null,
-
         // Initialized, but not nominating nor voting
         Idle,
-
         // Accepts nominations
         Nomination,
-
         // Accepts votes
         Vote,
-
         // Votes being counted
         Evaluation
     }
