81519f054f8facfde90833316960c308a13e3a60
Leonardo Massazza <lmassazza@gmail.com>
2022-02-08 10:58:46 -0300

    fix inheritance order

--- packages/synthetix-main/contracts/token/SNXToken.sol
+++ packages/synthetix-main/contracts/token/SNXToken.sol
@@ -5,7 +5,7 @@ import "@synthetixio/core-contracts/contracts/proxy/UUPSImplementation.sol";
 import "@synthetixio/core-contracts/contracts/ownership/Ownable.sol";
 import "@synthetixio/core-contracts/contracts/token/ERC20.sol";
 
-contract SNXToken is Ownable, UUPSImplementation, ERC20 {
+contract SNXToken is ERC20, UUPSImplementation, Ownable {
     function initialize(
         string memory tokenName,
         string memory tokenSymbol,
