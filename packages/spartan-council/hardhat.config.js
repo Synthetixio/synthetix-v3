38f52f6a2dc0cd40f50672634b87edf0e1d7d84e
Leonardo Massazza <lmassazza@gmail.com>
2022-03-14 20:04:42 -0300

    Bootstrap Spartan Council project (#850)
    * Bootstrap Spartan Council project
    
    * fix package.json

---
+++ packages/spartan-council/hardhat.config.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+require('hardhat-contract-sizer');
+require('solidity-coverage');
+require('@nomiclabs/hardhat-ethers');
+require('@synthetixio/deployer');
+require('@synthetixio/cli');
+
+module.exports = {
+  solidity: {
+    version: '0.8.11',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    local: {
+      url: 'http://localhost:8545',
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
+  },
+  deployer: {
+    proxyContract: 'SpartanCouncil',
+  },
+  contractSizer: {
+    strict: true,
+  },
+};
