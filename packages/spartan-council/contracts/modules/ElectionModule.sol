f2c0b16d1670151b49d0d922a662998d9d263dfe
Alejandro <palebluedot@gmail.com>
2022-03-15 09:16:28 -0300

    Proper connection to ElectionModule

--- packages/spartan-council/contracts/modules/ElectionModule.sol
+++ packages/spartan-council/contracts/modules/ElectionModule.sol
@@ -1,9 +1,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ElectionModule as BaseElectionModule} from "@synthetixio/core-modules/contracts/modules/ElectionModule.sol";
+import {ElectionModule as ElectionModuleBase} from "@synthetixio/core-modules/contracts/submodules/election/ElectionModule.sol";
+import "@synthetixio/core-modules/contracts/submodules/election/strategies/ElectionVotesBasic.sol";
+import "@synthetixio/core-modules/contracts/submodules/election/strategies/ElectionTallyPlurality.sol";
 
 // solhint-disable-next-line no-empty-blocks
-contract ElectionModule is BaseElectionModule {
+contract ElectionModule is ElectionModuleBase, ElectionTallyPlurality, ElectionVotesBasic {
 
 }
