7d462a4e76885d3efa640e970144ddceab3ed6b6
Leonardo Massazza <lmassazza@gmail.com>
2022-03-16 11:29:47 -0300

    Util to validate Merkle tree proof in contracts (#849)
    * add contract lib
    
    * fix lint
    
    * add tests
    
    * rename test file

---
+++ packages/core-contracts/contracts/mocks/utils/MerkleProofMock.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../../utils/MerkleProof.sol";
+
+contract MerkleProofMock {
+    function verify(
+        bytes32[] memory proof,
+        bytes32 root,
+        bytes32 leaf
+    ) public pure returns (bool) {
+        return MerkleProof.verify(proof, root, leaf);
+    }
+}
