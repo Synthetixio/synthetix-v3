03ce12734b7a58d587a5b5403a03040827480495
Leonardo Massazza <lmassazza@gmail.com>
2022-03-18 16:39:26 -0300

    more tests

--- packages/spartan-council/test/contracts/modules/DebtShareMigrator.test.js
+++ packages/spartan-council/test/contracts/modules/DebtShareMigrator.test.js
@@ -1,6 +1,7 @@
-// const assert = require('assert/strict');
+const assert = require('assert/strict');
 const assertRevert = require('@synthetixio/core-js/utils/assertions/assert-revert');
 const assertBn = require('@synthetixio/core-js/utils/assertions/assert-bignumber');
+const { findEvent } = require('@synthetixio/core-js/utils/ethers/events');
 const { parseBalanceMap } = require('@synthetixio/core-js/utils/merkle-tree/parse-balance-tree');
 const { bootstrap } = require('@synthetixio/deployer/utils/tests');
 const initializer = require('../../helpers/initializer');
@@ -10,24 +11,21 @@ const { ethers } = hre;
 describe('DebtShareMigrator', function () {
   let owner, user;
 
-  let parsedTree, validRoot, invalidRoot, voter;
+  let parsedTree, validRoot, wrongTree, voter;
 
   before('build tree and related data', () => {
     const inputData = {};
     const wrongData = {};
     for (let i = 0; i < 10; i++) {
-      inputData[ethers.Wallet.createRandom().address] = '' + (i + 1);
+      const address = ethers.Wallet.createRandom().address;
+      inputData[address] = '' + (i + 1);
+      wrongData[address] = '' + (i + 2);
     }
 
     parsedTree = parseBalanceMap(inputData);
+    wrongTree = parseBalanceMap(wrongData);
 
     validRoot = parsedTree.merkleRoot;
-
-    for (let i = 0; i < 5; i++) {
-      wrongData[ethers.Wallet.createRandom().address] = '' + (i + 1);
-    }
-
-    invalidRoot = parseBalanceMap(wrongData).merkleRoot;
 
     voter = Object.keys(parsedTree.claims)[0];
   });
@@ -68,20 +66,71 @@ describe('DebtShareMigrator', function () {
     });
 
     describe('when the merkle root is set', () => {
+      let receipt;
+
       before('set merkle root', async () => {
         const tx = await ElectionModule.connect(owner).setNewRoot(validRoot);
-        await tx.wait();
+        receipt = await tx.wait();
+      });
+
+      it('emitted a MerkleRootSet event', async function () {
+        const event = findEvent({ receipt, eventName: 'MerkleRootSet' });
+
+        assert.ok(event);
+        assert.deepEqual(event.args.merkleRoot, validRoot);
       });
 
       describe('when attempting to set the merkle root again', () => {
         it('reverts', async function () {
-          await assertRevert(ElectionModule.connect(user).setNewRoot(invalidRoot), 'Unauthorized');
+          await assertRevert(
+            ElectionModule.connect(user).setNewRoot(wrongTree.merkleRoot),
+            'Unauthorized'
+          );
         });
       });
 
-      describe('shows that the voter migrated debt before claiming', () => {
+      describe('when attempting to claim with the wrong proof', () => {
+        it('reverts', async () => {
+          await assertRevert(
+            ElectionModule.migrateL1DebtShare(
+              voter,
+              wrongTree.claims[voter].amount,
+              wrongTree.claims[voter].proof
+            ),
+            'InvalidMerkleProof'
+          );
+        });
+      });
+
+      describe('when retrieving the voter migrated debt before claiming', () => {
         it('has 0 debt share', async () => {
           assertBn.equal(await ElectionModule.getL1DebtShare(voter), 0);
+        });
+      });
+
+      describe('when a voter has claimed', () => {
+        before('claim', async () => {
+          const tx = await ElectionModule.migrateL1DebtShare(
+            voter,
+            parsedTree.claims[voter].amount,
+            parsedTree.claims[voter].proof
+          );
+          receipt = await tx.wait();
+        });
+
+        it('emitted a DebtShareMigrated event', async function () {
+          const event = findEvent({ receipt, eventName: 'DebtShareMigrated' });
+
+          assert.ok(event);
+          assert.deepEqual(event.args.voter, voter);
+          assertBn.equal(event.args.debtShare, parsedTree.claims[voter].amount);
+        });
+
+        it('has the right debt share', async () => {
+          assertBn.equal(
+            await ElectionModule.getL1DebtShare(voter),
+            parsedTree.claims[voter].amount
+          );
         });
       });
     });
