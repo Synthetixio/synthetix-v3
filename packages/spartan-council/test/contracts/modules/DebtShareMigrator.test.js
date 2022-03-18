0f845cc434cc54bed20fa508174ef6963763d5ca
Leonardo Massazza <lmassazza@gmail.com>
2022-03-18 14:01:21 -0300

    add more test cases

--- packages/spartan-council/test/contracts/modules/DebtShareMigrator.test.js
+++ packages/spartan-council/test/contracts/modules/DebtShareMigrator.test.js
@@ -1,5 +1,6 @@
-const assert = require('assert/strict');
+// const assert = require('assert/strict');
 const assertRevert = require('@synthetixio/core-js/utils/assertions/assert-revert');
+const assertBn = require('@synthetixio/core-js/utils/assertions/assert-bignumber');
 const { parseBalanceMap } = require('@synthetixio/core-js/utils/merkle-tree/parse-balance-tree');
 const { bootstrap } = require('@synthetixio/deployer/utils/tests');
 const initializer = require('../../helpers/initializer');
@@ -7,9 +8,11 @@ const initializer = require('../../helpers/initializer');
 const { ethers } = hre;
 
 describe('DebtShareMigrator', function () {
-  let parsedTree, validRoot, invalidRoot;
+  let owner, user;
 
-  before('build tree and roots', () => {
+  let parsedTree, validRoot, invalidRoot, voter;
+
+  before('build tree and related data', () => {
     const inputData = {};
     const wrongData = {};
     for (let i = 0; i < 10; i++) {
@@ -25,10 +28,16 @@ describe('DebtShareMigrator', function () {
     }
 
     invalidRoot = parseBalanceMap(wrongData).merkleRoot;
+
+    voter = Object.keys(parsedTree.claims)[0];
+  });
+
+  before('identify users', async () => {
+    [owner, user] = await ethers.getSigners();
   });
 
   describe('when setting a merkle root', () => {
-    const { proxyAddress, routerAddress } = bootstrap(initializer);
+    const { proxyAddress } = bootstrap(initializer);
 
     let ElectionModule;
 
@@ -41,32 +50,39 @@ describe('DebtShareMigrator', function () {
 
     describe('when attempting to set the merkle root with a non owner signer', () => {
       it('reverts', async function () {
-        const [, user] = await ethers.getSigners();
-
         await assertRevert(ElectionModule.connect(user).setNewRoot(validRoot), 'Unauthorized');
       });
     });
 
-    describe('when the merkle root is set', () => {
-      let owner, user;
-
-      before('identify users', async () => {
-        [owner, user] = await ethers.getSigners();
+    describe('when attempting to claim and the merkle root is not set', () => {
+      it('reverts', async () => {
+        await assertRevert(
+          ElectionModule.migrateL1DebtShare(
+            voter,
+            parsedTree.claims[voter].amount,
+            parsedTree.claims[voter].proof
+          ),
+          'MerkleRootNotSet'
+        );
       });
+    });
 
+    describe('when the merkle root is set', () => {
       before('set merkle root', async () => {
         const tx = await ElectionModule.connect(owner).setNewRoot(validRoot);
         await tx.wait();
       });
 
-      it('sss', async () => {
-        assert.ok(true);
-        // assert.equal(await UpgradeModule.getImplementation(), routerAddress());
+      describe('when attempting to set the merkle root again', () => {
+        it('reverts', async function () {
+          await assertRevert(ElectionModule.connect(user).setNewRoot(invalidRoot), 'Unauthorized');
+        });
+      });
 
-        // const tx = await UpgradeModule.upgradeTo(NewRouter.address);
-        // await tx.wait();
-
-        // assert.equal(await UpgradeModule.getImplementation(), NewRouter.address);
+      describe('shows that the voter migrated debt before claiming', () => {
+        it('has 0 debt share', async () => {
+          assertBn.equal(await ElectionModule.getL1DebtShare(voter), 0);
+        });
       });
     });
   });
