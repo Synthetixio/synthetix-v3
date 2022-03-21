13fe5db62ae511a7711304ddea59083f865cf1bf
Leonardo Massazza <lmassazza@gmail.com>
2022-03-21 12:50:44 -0300

    move files (WIP)

--- packages/spartan-council/contracts/interfaces/ISpartanCouncil.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface ISpartanCouncil {
-    function setDebtShareContract(address newDebtShareContractAddress) external;
-
-    function getDebtShareContract() external view returns (address);
-}
