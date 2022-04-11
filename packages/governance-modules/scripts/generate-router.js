d6f62872be17eca42bcedf4df8e0289b6b877d8d
Matías <mjlescano@protonmail.com>
2022-04-11 17:11:18 +0200

    refactor parameters passing with literal json

--- packages/governance-modules/scripts/generate-router.js
+++ packages/governance-modules/scripts/generate-router.js
@@ -2,16 +2,15 @@ const path = require('path');
 const hre = require('hardhat');
 const { parseFullyQualifiedName } = require('hardhat/utils/contract-names');
 const { TASK_COMPILE } = require('hardhat/builtin-tasks/task-names');
-const chunk = require('lodash.chunk');
 const { SUBTASK_GENERATE_ROUTER_SOURCE } = require('../../deployer/task-names');
 
 /**
  * Generate the file contracts/Router.sol including the given modules in its source.
  */
-exports.generateRouter = async function generateRouter(...modulesData) {
-  const modules = {};
+exports.generateRouter = async function generateRouter(modulesData) {
+  const modules = JSON.parse(modulesData);
 
-  for (const [fullyQualifiedName, deployedAddress] of chunk(modulesData, 2)) {
+  for (const [fullyQualifiedName, deployedAddress] of Object.entries(modules)) {
     const { contractName } = parseFullyQualifiedName(fullyQualifiedName);
     modules[fullyQualifiedName] = { contractName, deployedAddress };
   }
