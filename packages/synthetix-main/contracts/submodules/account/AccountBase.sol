d688e6f7da0ad83a3ca15b63fc76b8cabff72f24
Leonardo Massazza <lmassazza@gmail.com>
2022-04-13 13:18:17 +0200

    Improve Permission identification

--- packages/synthetix-main/contracts/submodules/account/AccountBase.sol
+++ packages/synthetix-main/contracts/submodules/account/AccountBase.sol
@@ -8,35 +8,31 @@ import "../../storage/AccountStorage.sol";
 /// @dev Common utils, errors, and events to be used by any contracts that conform the AcccountModule
 contract AccountBase is ERC721, AccountStorage, InitializableMixin {
     // ---------------------------------------
-    // Constants
-    // ---------------------------------------
-
-    /// @dev use permissions packed in an uint32 (16 permissions). Set the values right shifting uint(1) from 0 to 15
-    uint32 private constant _PERMISSION_OWNER = uint32(1); // Actually, is not needed as a permission itself, but is a placeholder for onlyOwner operations
-    uint32 private constant _PERMISSION_STAKE = uint32(1) << 1;
-    uint32 private constant _PERMISSION_UNSTAKE = uint32(1) << 2;
-    uint32 private constant _PERMISSION_MINT = uint32(1) << 3;
-    uint32 private constant _PERMISSION_BURN = uint32(1) << 4;
-    uint32 private constant _PERMISSION_CLAIM_REWARDS = uint32(1) << 5;
-    uint32 private constant _PERMISSION_MANAGE_LOCKING = uint32(1) << 6;
-    uint32 private constant _PERMISSION_DELEGATE = uint32(1) << 7;
-
-    // ---------------------------------------
     // Enums
     // ---------------------------------------
+    enum Permission {
+        Owner, // Actually, is not needed as a permission itself, but is a placeholder for onlyOwner operations
+        Stake,
+        Unstake,
+        Mint,
+        Burn,
+        ClaimRewards,
+        ManageLocking,
+        Delegate
+    }
 
     // ---------------------------------------
     // Errors
     // ---------------------------------------
 
-    error NotAuthorized(uint256 account, address addr, uint32 permission);
+    error NotAuthorized(uint256 account, address addr, Permission permission);
 
     // ---------------------------------------
     // Events
     // ---------------------------------------
 
-    event PermissionGranted(uint256 account, address authorized, uint32 permission);
-    event PermissionRevoked(uint256 account, address authorized, uint32 permission);
+    event PermissionGranted(uint256 account, address authorized, Permission permission);
+    event PermissionRevoked(uint256 account, address authorized, Permission permission);
     event PermissionsSet(uint256 account, address authorized, uint32 packedPermissions);
     event PermissionsCleared(uint256 account, address authorized);
 
@@ -48,22 +44,26 @@ contract AccountBase is ERC721, AccountStorage, InitializableMixin {
         return _accountStore().initialized;
     }
 
+    function _getPermissionBit(Permission permission) internal pure returns (uint32) {
+        return uint32(1) << uint32(permission);
+    }
+
     function _hasPermission(
         uint256 account,
         address authorized,
-        uint32 permission
+        Permission permission
     ) internal view returns (bool) {
-        return _accountStore().accountDelegations[account][authorized] & permission != 0;
+        return _accountStore().accountDelegations[account][authorized] & _getPermissionBit(permission) != 0;
     }
 
     function _grantPermission(
         uint256 account,
         address authorized,
-        uint32 permission
+        Permission permission
     ) internal {
         _accountStore().accountDelegations[account][authorized] =
             _accountStore().accountDelegations[account][authorized] |
-            permission;
+            _getPermissionBit(permission);
 
         emit PermissionGranted(account, authorized, permission);
     }
@@ -71,11 +71,11 @@ contract AccountBase is ERC721, AccountStorage, InitializableMixin {
     function _revokePermission(
         uint256 account,
         address authorized,
-        uint32 permission
+        Permission permission
     ) internal {
         _accountStore().accountDelegations[account][authorized] =
             _accountStore().accountDelegations[account][authorized] &
-            ~permission;
+            ~_getPermissionBit(permission);
 
         emit PermissionRevoked(account, authorized, permission);
     }
@@ -89,12 +89,12 @@ contract AccountBase is ERC721, AccountStorage, InitializableMixin {
     function _setPermissions(
         uint256 account,
         address authorized,
-        uint32[] calldata permissions
+        Permission[] calldata permissions
     ) internal {
         uint32 packedPermissions = 0;
 
         for (uint i = 0; i < permissions.length; i++) {
-            packedPermissions = packedPermissions | permissions[i];
+            packedPermissions = packedPermissions | _getPermissionBit(permissions[i]);
         }
 
         _accountStore().accountDelegations[account][authorized] = packedPermissions;
@@ -103,7 +103,7 @@ contract AccountBase is ERC721, AccountStorage, InitializableMixin {
     }
 
     /// @dev Used to allow certain functions to operate on the account if owner or authorized to that operation
-    modifier onlyOwnerOrAuthorized(uint256 accountId, uint32 permission) {
+    modifier onlyOwnerOrAuthorized(uint256 accountId, Permission permission) {
         if (msg.sender != ownerOf(accountId) && !_hasPermission(accountId, msg.sender, permission)) {
             revert NotAuthorized(accountId, msg.sender, permission);
         }
