84349dddd99f261c9a5ec6f2b7139277d5d91495
Alejandro <palebluedot@gmail.com>
2022-05-04 10:51:36 -0600

    Added grants-council package

---
+++ packages/grants-council/.solcover.js
@@ -0,0 +1,8 @@
+module.exports = {
+  skipFiles: ['mocks', 'Router.sol'],
+  // Reduce instrumentation footprint - volume of solidity code
+  // passed to compiler causes it to crash
+  // Line and branch coverage will still be reported.
+  measureStatementCoverage: false,
+  measureFunctionCoverage: false,
+};
---
+++ packages/grants-council/hardhat.config.js
@@ -0,0 +1,48 @@
+require('dotenv/config');
+
+require('hardhat-contract-sizer');
+require('solidity-coverage');
+require('@nomiclabs/hardhat-ethers');
+require('@synthetixio/deployer');
+require('@synthetixio/cli');
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const config = {
+  solidity: {
+    version: '0.8.11',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    local: {
+      url: 'http://localhost:8545',
+    },
+    ['optimistic-kovan']: {
+      url: 'https://kovan.optimism.io',
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
+  },
+  deployer: {
+    proxyContract: 'SpartanCouncil',
+  },
+  contractSizer: {
+    strict: true,
+  },
+};
+
+if (process.env.FORK_PROVIDER_URL) {
+  config.networks.hardhat = {
+    forking: {
+      url: process.env.FORK_PROVIDER_URL,
+      blockNumber: 4838445, // 2022-03-25
+    },
+  };
+}
+
+module.exports = config;
