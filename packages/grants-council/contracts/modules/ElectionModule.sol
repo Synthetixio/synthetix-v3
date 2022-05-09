ef2f12011fb4531b88269c7c75e70d680071890d
Alejandro Santander <Palebluedot@gmail.com>
2022-05-09 13:47:53 -0300

    Adds new council packages (#920)
    * Remove unused interface
    
    * Added grants-council package
    
    * Added treasury-council package
    
    * Addded ambassador-council package

---
+++ packages/grants-council/contracts/modules/ElectionModule.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ElectionModule as SynthetixElectionModule} from "@synthetixio/synthetix-governance/contracts/modules/ElectionModule.sol";
+
+// solhint-disable-next-line no-empty-blocks
+contract ElectionModule is SynthetixElectionModule {
+
+}
