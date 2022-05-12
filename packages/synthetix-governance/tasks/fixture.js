ab6de2a1668998f8f2510d121dd97805331e71a4
Matías <mjlescano@protonmail.com>
2022-05-12 15:15:43 +0200

    add ballots votes fixtures

--- packages/synthetix-governance/tasks/fixture.js
+++ packages/synthetix-governance/tasks/fixture.js
@@ -8,11 +8,34 @@ const ElectionPeriod = {
   Evaluation: 3,
 };
 
-/**
- * Example:
- *  npx hardhat --network local fixture:candidates --address 0x...
- */
-task('fixture:candidates', 'create fixture wallets and nominate them')
+task('fixture:wallets', 'Create fixture wallets')
+  .addOptionalParam('amount', 'Amount of wallets to fixture', '50', types.int)
+  .setAction(async ({ amount }, hre) => {
+    console.log(`Fixturing ${amount} wallets\n`);
+
+    const wallets = createArray(amount).map(() => {
+      const { privateKey } = hre.ethers.Wallet.createRandom();
+      return new hre.ethers.Wallet(privateKey, hre.ethers.provider);
+    });
+
+    let i = 0;
+    return await Promise.all(
+      wallets.map(async (wallet) => {
+        hre.network.provider.request({
+          method: 'hardhat_setBalance',
+          params: [wallet.address, '0x10000000000000000000000'],
+        });
+
+        console.log(`Address #${++i}: `, wallet.address);
+        console.log('Private Key: ', wallet.privateKey);
+        console.log();
+
+        return wallet;
+      })
+    );
+  });
+
+task('fixture:candidates', 'Create fixture candidate nominations')
   .addParam('address', 'Deployed election module proxy address', undefined, types.address)
   .addOptionalParam('amount', 'Amount of candidates to fixture', '50', types.int)
   .setAction(async ({ address, amount }, hre) => {
@@ -27,29 +50,85 @@ task('fixture:candidates', 'create fixture wallets and nominate them')
       throw new Error('The election is not on ElectionPeriod.Nomination');
     }
 
-    console.log(`Fixturing ${amount} candidates for ${address}\n`);
+    const candidates = await hre.run('fixture:wallets', { amount });
 
-    const candidates = createArray(amount).map(() => hre.ethers.Wallet.createRandom());
+    console.log(`Nominating ${amount} candidates for ${address}\n`);
 
-    let i = 0;
     await Promise.all(
       candidates.map(async (candidate) => {
-        const signer = new hre.ethers.Wallet(candidate.privateKey, hre.ethers.provider);
-
-        await hre.network.provider.request({
-          method: 'hardhat_setBalance',
-          params: [signer.address, '0x10000000000000000000000'],
-        });
-
-        const tx = await ElectionModule.connect(signer).nominate();
+        const tx = await ElectionModule.connect(candidate).nominate();
         await tx.wait();
+      })
+    );
 
-        console.log(`Candidate #${++i}: `, candidate.address);
-        console.log('Private Key: ', candidate.privateKey);
+    return candidates;
+  });
+
+task('fixture:votes', 'Create fixture votes to nominated candidates')
+  .addParam('address', 'Deployed election module proxy address', undefined, types.address)
+  .addOptionalParam('amount', 'Amount of voters to fixture', '30', types.int)
+  .addOptionalParam('ballotSize', 'Amount of cadidates for each ballot', '5', types.int)
+  .setAction(async ({ address, amount, ballotSize }, hre) => {
+    const ElectionModule = await hre.ethers.getContractAt(
+      'contracts/modules/ElectionModule.sol:ElectionModule',
+      address
+    );
+
+    const currentPeriod = Number(await ElectionModule.getCurrentPeriod());
+
+    if (currentPeriod !== ElectionPeriod.Vote) {
+      throw new Error('The election is not on ElectionPeriod.Vote');
+    }
+
+    console.log(`Fixturing ${amount} voters for ${address}\n`);
+
+    const voters = await hre.run('fixture:wallets', { amount });
+    const candidates = await ElectionModule.getNominees();
+
+    const ballotsCount = Math.floor(candidates.length / Number(ballotSize));
+
+    console.log(`Fixturing ${ballotsCount} ballots from ${candidates.length} candidates\n`);
+
+    const ballots = createArray(ballotsCount).map(() => pickRand(candidates, ballotSize));
+
+    await Promise.all(
+      voters.map(async (voter) => {
+        const [ballot] = pickRand(ballots, 1);
+        const tx = await ElectionModule.connect(voter).cast(ballot);
+        await tx.wait();
+      })
+    );
+
+    await Promise.all(
+      ballots.map(async (ballot) => {
+        const ballotId = await ElectionModule.calculateBallotId(ballot);
+        const votes = await ElectionModule.getBallotVotes(ballotId);
+        console.log('BallotId: ', ballotId);
+        console.log('Candidates:');
+        ballot.forEach((address, i) => {
+          console.log(` #${i}: `, address);
+        });
+        console.log('Vote Count: ', votes.toString());
         console.log();
       })
     );
   });
+
+function pickRand(arr, amount = 1) {
+  if (!Array.isArray(arr) || arr.length < amount) throw new Error('Invalid data');
+
+  const res = [];
+  while (res.length < amount) {
+    const item = arr[rand(0, arr.length - 1)];
+    if (!res.includes(item)) res.push(item);
+  }
+
+  return res;
+}
+
+function rand(min = 1, max = 10) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function createArray(length) {
   return Array.from(Array(Number(length)));
