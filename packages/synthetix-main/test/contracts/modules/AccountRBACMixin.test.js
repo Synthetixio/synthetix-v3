6cdab80a62642073bcfbe023f5fc839d5f2f4d72
Leonardo Massazza <lmassazza@gmail.com>
2022-05-18 12:05:33 +0200

    complete tests for RBAC Mixin

--- packages/synthetix-main/test/contracts/modules/AccountRBACMixin.test.js
+++ packages/synthetix-main/test/contracts/modules/AccountRBACMixin.test.js
@@ -8,13 +8,13 @@ const initializer = require('../../helpers/initializer');
 describe('AccountRBACMixin', function () {
   const { proxyAddress } = bootstrap(initializer);
 
-  let owner, user1, user2;
+  let owner, user1, user2, user3;
 
   let AccountModule, accountTokenAddress, AccountToken;
   let AccountRBACMixinMock;
 
   before('identify signers', async () => {
-    [owner, user1, user2] = await ethers.getSigners();
+    [owner, user1, user2, user3] = await ethers.getSigners();
   });
 
   before('identify modules', async () => {
@@ -48,6 +48,93 @@ describe('AccountRBACMixin', function () {
           AccountRBACMixinMock.connect(user2).interactWithAccount(1, 1337),
           `RoleNotAuthorized(1, "${ethers.utils.formatBytes32String('stake')}", "${user2.address}")`
         );
+      });
+    });
+
+    describe('when transfering ownership of accountToken', async () => {
+      before('transfer to user2', async () => {
+        await (
+          await AccountToken.connect(user1).transferFrom(user1.address, user2.address, 1)
+        ).wait();
+      });
+
+      after('transfer back to user1', async () => {
+        await (
+          await AccountToken.connect(user2).transferFrom(user2.address, user1.address, 1)
+        ).wait();
+      });
+
+      it('can interact with the contract', async () => {
+        await (await AccountRBACMixinMock.connect(user2).interactWithAccount(1, 43)).wait();
+        assertBn.equal(await AccountRBACMixinMock.getRBACValue(), 43);
+      });
+
+      describe('when attempting to interact as non owner', async () => {
+        it('reverts', async () => {
+          await assertRevert(
+            AccountRBACMixinMock.connect(user1).interactWithAccount(1, 1337),
+            `RoleNotAuthorized(1, "${ethers.utils.formatBytes32String('stake')}", "${
+              user1.address
+            }")`
+          );
+        });
+      });
+    });
+
+    describe('when granting access', async () => {
+      before('grant access to some users', async () => {
+        await (
+          await AccountModule.connect(user1).grantRole(
+            1,
+            ethers.utils.formatBytes32String('stake'),
+            user2.address
+          )
+        ).wait();
+
+        await (
+          await AccountModule.connect(user1).grantRole(
+            1,
+            ethers.utils.formatBytes32String('otherRole'),
+            user3.address
+          )
+        ).wait();
+      });
+
+      it('can interact with the contract', async () => {
+        await (await AccountRBACMixinMock.connect(user2).interactWithAccount(1, 44)).wait();
+        assertBn.equal(await AccountRBACMixinMock.getRBACValue(), 44);
+      });
+
+      describe('when attempting to interact as non owner / non authorized with that role', async () => {
+        it('reverts', async () => {
+          await assertRevert(
+            AccountRBACMixinMock.connect(user3).interactWithAccount(1, 1337),
+            `RoleNotAuthorized(1, "${ethers.utils.formatBytes32String('stake')}", "${
+              user3.address
+            }")`
+          );
+        });
+      });
+
+      describe('when attempting to access after the role was revoked', async () => {
+        before('revoke access to user2', async () => {
+          await (
+            await AccountModule.connect(user1).revokeRole(
+              1,
+              ethers.utils.formatBytes32String('stake'),
+              user2.address
+            )
+          ).wait();
+        });
+
+        it('reverts', async () => {
+          await assertRevert(
+            AccountRBACMixinMock.connect(user2).interactWithAccount(1, 1337),
+            `RoleNotAuthorized(1, "${ethers.utils.formatBytes32String('stake')}", "${
+              user2.address
+            }")`
+          );
+        });
       });
     });
   });
