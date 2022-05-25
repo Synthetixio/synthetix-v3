c58daf2c2fabed75a5ad04c27a8f992393d1d6c5
Matías Lescano <mjlescano@users.noreply.github.com>
2022-05-25 19:01:17 +0200

    Fix CLI tests (#987)
    * fix coloring related tests fails
    
    * fix initializable tests
    
    * fix SomeModule selection test
    
    * fix execute-call tests
    
    * update cli tests
    
    * fix test failing error
    
    * update to only use fixture-project
    
    * remove extra error

--- packages/cli/test/fixture-projects/sample-project/contracts/modules/InitializableModule.sol
+++ packages/cli/test/fixture-projects/sample-project/contracts/modules/InitializableModule.sol
@@ -2,18 +2,19 @@
 pragma solidity ^0.8.0;
 
 import "@synthetixio/core-contracts/contracts/initializable/InitializableMixin.sol";
+import "../interfaces/IInitializableModule.sol";
 import "../storage/InitializableStorage.sol";
 
-contract InitializableModule is InitializableStorage, InitializableMixin {
+contract InitializableModule is InitializableStorage, InitializableMixin, IInitializableModule {
     function _isInitialized() internal view override returns (bool) {
         return _initializableStore().initialized;
     }
 
-    function isInitializableModuleInitialized() external view returns (bool) {
+    function isInitializableModuleInitialized() external view override returns (bool) {
         return _isInitialized();
     }
 
-    function initializeInitializableModule() external onlyIfNotInitialized {
+    function initializeInitializableModule() external override onlyIfNotInitialized {
         _initializableStore().initialized = true;
     }
 }
