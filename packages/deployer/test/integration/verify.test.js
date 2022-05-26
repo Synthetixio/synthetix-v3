e842199e13da3f1b325e70311c2d9179c753e962
Matías <mjlescano@protonmail.com>
2022-05-26 19:49:36 +0200

    update verify tests

--- packages/deployer/test/integration/verify.test.js
+++ packages/deployer/test/integration/verify.test.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { deepEqual, rejects } = require('assert/strict');
+const { deepEqual, rejects, equal } = require('assert/strict');
 const { task } = require('hardhat/config');
 const { TASK_VERIFY_VERIFY } = require('@nomiclabs/hardhat-etherscan/dist/src/constants');
 const { TASK_DEPLOY_VERIFY, SUBTASK_LOAD_DEPLOYMENT } = require('../../task-names');
@@ -39,11 +39,9 @@ describe('deploy:verify', function () {
       verifyCalls = [];
     });
 
-    before('call to verify', async function () {
-      await hre.run(TASK_DEPLOY_VERIFY, { quiet: true, instance: 'test' });
-    });
-
     it('made the appropiate verify calls', async function () {
+      await hre.run(TASK_DEPLOY_VERIFY, { quiet: true, instance: 'test' });
+
       const contracts = Object.values(hre.deployer.deployment.general.contracts);
       const expected = contracts.map((c) => {
         const r = {
@@ -77,6 +75,49 @@ describe('deploy:verify', function () {
           await hre.run(SUBTASK_LOAD_DEPLOYMENT, { instance: 'test' });
           hre.deployer.deployment.general.properties.completed = true;
         }
+      });
+    });
+
+    describe('when verifying a single contract', function () {
+      it('it correctly verifies it', async function () {
+        const contract = 'contracts/modules/SomeModule.sol:SomeModule';
+
+        await hre.run(TASK_DEPLOY_VERIFY, {
+          quiet: true,
+          instance: 'test',
+          contract,
+        });
+
+        const SomeModule = Object.values(hre.deployer.deployment.general.contracts).find(
+          (c) => c.contractFullyQualifiedName === contract
+        );
+
+        deepEqual(verifyCalls, [
+          {
+            address: SomeModule.deployedAddress,
+            constructorArguments: [],
+            contract: SomeModule.contractFullyQualifiedName,
+            libraries: {},
+          },
+        ]);
+      });
+    });
+
+    describe('when verifying a second deployment', function () {
+      before('prepare environment', async function () {
+        await deployOnEnvironment(hre, { alias: 'second' });
+      });
+
+      it('does not tries to verify the Proxy', async function () {
+        await hre.run(TASK_DEPLOY_VERIFY, {
+          quiet: true,
+          instance: 'test',
+        });
+
+        equal(
+          verifyCalls.some((c) => c.contract === 'contracts/Proxy.sol:Proxy'),
+          false
+        );
       });
     });
   });
