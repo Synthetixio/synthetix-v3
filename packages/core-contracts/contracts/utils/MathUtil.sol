d75040792dd2ce62feaf6a59731a5382fa9d6d99
Leonardo Massazza <lmassazza@gmail.com>
2022-06-06 22:19:59 +0200

    add utils

--- packages/core-contracts/contracts/utils/MathUtil.sol
+++ packages/core-contracts/contracts/utils/MathUtil.sol
@@ -7,6 +7,50 @@ pragma solidity ^0.8.0;
 */
 
 library MathUtil {
+    /*//////////////////////////////////////////////////////////////
+                    LOW LEVEL FIXED POINT OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function mulDivDown(
+        uint256 x,
+        uint256 y,
+        uint256 denominator
+    ) internal pure returns (uint256 z) {
+        assembly {
+            // Store x * y in z for now.
+            z := mul(x, y)
+
+            // Equivalent to require(denominator != 0 && (x == 0 || (x * y) / x == y))
+            if iszero(and(iszero(iszero(denominator)), or(iszero(x), eq(div(z, x), y)))) {
+                revert(0, 0)
+            }
+
+            // Divide z by the denominator.
+            z := div(z, denominator)
+        }
+    }
+
+    function mulDivUp(
+        uint256 x,
+        uint256 y,
+        uint256 denominator
+    ) internal pure returns (uint256 z) {
+        assembly {
+            // Store x * y in z for now.
+            z := mul(x, y)
+
+            // Equivalent to require(denominator != 0 && (x == 0 || (x * y) / x == y))
+            if iszero(and(iszero(iszero(denominator)), or(iszero(x), eq(div(z, x), y)))) {
+                revert(0, 0)
+            }
+
+            // First, divide z - 1 by the denominator and add 1.
+            // We allow z - 1 to underflow if z is 0, because we multiply the
+            // end result by 0 if z is zero, ensuring we return 0 if z is zero.
+            z := mul(iszero(iszero(z)), add(div(sub(z, 1), denominator), 1))
+        }
+    }
+
     function sqrt(uint256 x) internal pure returns (uint256 z) {
         assembly {
             // Start off with z at 1.
--- packages/core-contracts/test/contracts/utils/MathUtil.test.js
+++ packages/core-contracts/test/contracts/utils/MathUtil.test.js
@@ -1,6 +1,11 @@
 const { ethers } = hre;
 const assertBn = require('@synthetixio/core-js/utils/assertions/assert-bignumber');
+const assertRevert = require('@synthetixio/core-js/utils/assertions/assert-revert');
 const { bnSqrt } = require('@synthetixio/core-js/utils/ethers/bignumber');
+
+function e(base, exp) {
+  return ethers.BigNumber.from(base).mul(ethers.BigNumber.from(10).pow(exp));
+}
 
 describe('MathUtil', () => {
   let MathUtil;
@@ -43,6 +48,81 @@ describe('MathUtil', () => {
       await assertBnSqrt(ethers.utils.parseEther('450000'));
       await assertBnSqrt(ethers.utils.parseEther('10333333'));
       await assertBnSqrt(ethers.utils.parseEther('100000000'));
+    });
+  });
+
+  describe('mulDivDown()', () => {
+    async function assertMulDivDown(data, expected) {
+      assertBn.equal(
+        await MathUtil.mulDivDown(
+          ethers.BigNumber.from(data.x),
+          ethers.BigNumber.from(data.y),
+          ethers.BigNumber.from(data.denominator)
+        ),
+        ethers.BigNumber.from(expected)
+      );
+    }
+
+    it('get the expected results', async () => {
+      await assertMulDivDown({ x: e(250, 25), y: e(50, 25), denominator: e(100, 25) }, e(125, 25));
+      await assertMulDivDown({ x: e(250, 16), y: e(50, 16), denominator: e(100, 16) }, e(125, 16));
+      await assertMulDivDown({ x: e(250, 6), y: e(50, 6), denominator: e(100, 6) }, e(125, 6));
+      await assertMulDivDown({ x: 369, y: 271, denominator: 100 }, 999);
+
+      await assertMulDivDown({ x: e(10, 26), y: e(10, 26), denominator: e(20, 26) }, e(5, 26));
+      await assertMulDivDown({ x: e(100, 16), y: e(100, 16), denominator: e(200, 16) }, e(50, 16));
+      await assertMulDivDown({ x: 1e8, y: 1e8, denominator: 2e8 }, 0.5e8);
+
+      await assertMulDivDown({ x: e(2, 27), y: e(3, 27), denominator: e(2, 27) }, e(3, 27));
+      await assertMulDivDown({ x: e(3, 18), y: e(2, 18), denominator: e(3, 18) }, e(2, 18));
+      await assertMulDivDown({ x: 2e8, y: 3e8, denominator: 2e8 }, 3e8);
+    });
+
+    it('get the expected results on edge cases', async () => {
+      await assertMulDivDown({ x: 0, y: e(1, 18), denominator: e(1, 18) }, 0);
+      await assertMulDivDown({ x: e(1, 18), y: 0, denominator: e(1, 18) }, 0);
+      await assertMulDivDown({ x: 0, y: 0, denominator: e(1, 18) }, 0);
+    });
+
+    it('fails on div by zero', async () => {
+      await assertRevert(MathUtil.mulDivDown(e(1, 18), e(1, 18), 0));
+    });
+  });
+  describe('mulDivUp()', () => {
+    async function assertMulDivUp(data, expected) {
+      assertBn.equal(
+        await MathUtil.mulDivUp(
+          ethers.BigNumber.from(data.x),
+          ethers.BigNumber.from(data.y),
+          ethers.BigNumber.from(data.denominator)
+        ),
+        ethers.BigNumber.from(expected)
+      );
+    }
+
+    it('get the expected results', async () => {
+      await assertMulDivUp({ x: e(250, 25), y: e(50, 25), denominator: e(100, 25) }, e(125, 25));
+      await assertMulDivUp({ x: e(250, 16), y: e(50, 16), denominator: e(100, 16) }, e(125, 16));
+      await assertMulDivUp({ x: e(250, 6), y: e(50, 6), denominator: e(100, 6) }, e(125, 6));
+      await assertMulDivUp({ x: 369, y: 271, denominator: 100 }, 1000);
+
+      await assertMulDivUp({ x: e(10, 26), y: e(10, 26), denominator: e(20, 26) }, e(5, 26));
+      await assertMulDivUp({ x: e(100, 16), y: e(100, 16), denominator: e(200, 16) }, e(50, 16));
+      await assertMulDivUp({ x: 1e8, y: 1e8, denominator: 2e8 }, 0.5e8);
+
+      await assertMulDivUp({ x: e(2, 27), y: e(3, 27), denominator: e(2, 27) }, e(3, 27));
+      await assertMulDivUp({ x: e(3, 18), y: e(2, 18), denominator: e(3, 18) }, e(2, 18));
+      await assertMulDivUp({ x: 2e8, y: 3e8, denominator: 2e8 }, 3e8);
+    });
+
+    it('get the expected results on edge cases', async () => {
+      await assertMulDivUp({ x: 0, y: e(1, 18), denominator: e(1, 18) }, 0);
+      await assertMulDivUp({ x: e(1, 18), y: 0, denominator: e(1, 18) }, 0);
+      await assertMulDivUp({ x: 0, y: 0, denominator: e(1, 18) }, 0);
+    });
+
+    it('fails on div by zero', async () => {
+      await assertRevert(MathUtil.mulDivUp(e(1, 18), e(1, 18), 0));
     });
   });
 });
