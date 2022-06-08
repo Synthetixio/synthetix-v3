85e6b90b56204ba1c4c67e7e503c018463081eb2
Leonardo Massazza <lmassazza@gmail.com>
2022-06-08 04:53:08 -0300

    Apply suggestions from code review

--- packages/synthetix-main/contracts/interfaces/IAccountToken.sol
+++ packages/synthetix-main/contracts/interfaces/IAccountToken.sol
@@ -6,8 +6,8 @@ import "@synthetixio/core-contracts/contracts/interfaces/IERC721.sol";
 /// @title NFT token identifying an Account
 interface IAccountToken is IERC721 {
     /// @notice mints a new token (NFT) with the "requestedAccountId" id owned by "owner".
-    function mint(address owner, uint requestedAccountId) external;
+    function mint(address accountOwner, uint requestedAccountId) external;
 
     /// @notice mints a new token (NFT) with at the next available id owned by "owner".
-    function mintNext(address owner) external returns (uint256 accountId);
+    function mintNext(address accountOwner) external returns (uint256 accountId);
 }
