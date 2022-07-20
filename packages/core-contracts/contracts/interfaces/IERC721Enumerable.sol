e43693b9e8568d5f7965ea97981b3bbc89f4074a
Sunny Vempati <sunnyvempati@gmail.com>
2022-07-20 00:23:26 -0500

    remove interface inheritence

--- packages/core-contracts/contracts/interfaces/IERC721Enumerable.sol
+++ packages/core-contracts/contracts/interfaces/IERC721Enumerable.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./IERC721.sol";
 
-interface IERC721Enumerable is IERC721 {
+interface IERC721Enumerable {
     function totalSupply() external view returns (uint256);
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
