bc2398c09f4539e16b7177cd4870998cbac5ee6d
Alejandro <palebluedot@gmail.com>
2022-08-09 12:18:11 -0300

    Removed v3gm

--- packages/treasury-council/contracts/mocks/Imports.sol
+++
@@ -1,5 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-// @dev: these imports are only made so that artifacts are available in tests
-import "@synthetixio/synthetix-governance/contracts/mocks/DebtShareMock.sol";
