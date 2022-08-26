add0442fc1801558ff67edf9a467eafda1944629
Alejandro <palebluedot@gmail.com>
2022-08-26 10:16:59 -0300

    Testing account transfer hook

--- packages/synthetix-main/test/integration/modules/AccountModule.test.ts
+++ packages/synthetix-main/test/integration/modules/AccountModule.test.ts
@@ -6,34 +6,51 @@ import { bootstrap } from '../bootstrap';
 describe.only('AccountModule and AccountTokenModule', function () {
   const { signers, systems } = bootstrap();
 
-  let owner: ethers.Signer;
   let user1: ethers.Signer;
+  let user2: ethers.Signer;
 
   before('identify signers', async () => {
-    [owner, user1] = signers();
+    [, user1, user2] = signers();
   });
 
-  describe('when the core and account systems are deployed', () => {
-    it('sets the account system address in the core system', async () => {
+  describe('when the core and account systems are deployed', function () {
+    it('sets the account system address in the core system', async function () {
       assert.equal(await systems().Core.getAccountTokenAddress(), systems().Account.address);
     });
 
-    it('sets the core system as the owner of the account system', async () => {
+    it('sets the core system as the owner of the account system', async function () {
       assert.equal(await systems().Account.owner(), systems().Core.address);
     });
 
-    describe('when creating an account', () => {
-      before('create account', async () => {
+    describe('when creating an account', function () {
+      before('create the account', async function () {
         await systems().Core.connect(user1).createAccount(1);
       });
 
-      it('records the owner in the account system', async () => {
+      it('records the owner in the account system', async function () {
         assert.equal(await systems().Account.ownerOf(1), await user1.getAddress());
         assertBn.equal(await systems().Account.balanceOf(await user1.getAddress()), 1);
       });
 
-      it('records the owner in the core system', async () => {
+      it('records the owner in the core system', async function () {
         assert.equal(await systems().Core.accountOwner(1), await user1.getAddress());
+      });
+
+      describe('when an account NFT is transferred', function () {
+        before('transfer the account', async function () {
+          await systems().Account.connect(user1).transferFrom(await user1.getAddress(), await user2.getAddress(), 1);
+        });
+
+        it('records the new owner in the account system', async function () {
+          assert.equal(await systems().Account.ownerOf(1), await user2.getAddress());
+          assertBn.equal(await systems().Account.balanceOf(await user2.getAddress()), 1);
+
+          assertBn.equal(await systems().Account.balanceOf(await user1.getAddress()), 0);
+        });
+
+        it('records the new owner in the core system', async function () {
+          assert.equal(await systems().Core.accountOwner(1), await user2.getAddress());
+        });
       });
     });
   });
