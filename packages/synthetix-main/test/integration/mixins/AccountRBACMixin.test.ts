833c2cd0a97a437c8736140ada79e62815be590a
Alejandro <palebluedot@gmail.com>
2022-08-28 20:13:38 -0300

    Working AccountRBACMixin tests

--- packages/synthetix-main/test/integration/mixins/AccountRBACMixin.test.ts
+++ packages/synthetix-main/test/integration/mixins/AccountRBACMixin.test.ts
@@ -1,4 +1,3 @@
-import assert from 'assert/strict';
 import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber';
 import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
 import { ethers } from 'ethers';
@@ -14,83 +13,150 @@ describe.only('AccountRBACMixin', function () {
 
   let user1: ethers.Signer;
   let user2: ethers.Signer;
+  let user3: ethers.Signer;
+
+  function canStake({ user }: { user: number }) {
+    let value = Math.round(10000 * Math.random());
+
+    describe(`when user${user} stakes`, function () {
+      before('stake', async function () {
+        const signer = signers()[user];
+
+        await (await systems().Core.connect(signer).mock_AccountRBACMixin_stake(1, value));
+      });
+
+      it('sets the mock value', async function () {
+        assertBn.equal(await systems().Core.mock_AccountRBACMixin_getStakeMock(), value);
+      });
+    });
+  }
+
+  function canMint({ user }: { user: number }) {
+    let value = Math.round(10000 * Math.random());
+
+    describe(`when user${user} mints`, function () {
+      before('mint', async function () {
+        const signer = signers()[user];
+
+        await (await systems().Core.connect(signer).mock_AccountRBACMixin_mint(1, value));
+      });
+
+      it('sets the mock value', async function () {
+        assertBn.equal(await systems().Core.mock_AccountRBACMixin_getMintMock(), value);
+      });
+    });
+  }
+
+  function cannotStake({ user }: { user: number }) {
+    describe(`when user${user} tries to stake`, function () {
+      it('reverts', async function () {
+        const signer = signers()[user];
+
+        await assertRevert(
+          systems().Core.connect(signer).mock_AccountRBACMixin_stake(1, 666),
+          `RoleNotAuthorized("1", "${Roles.STAKE}", "${await signer.getAddress()}")`,
+          systems().Core
+        );
+      });
+    });
+  }
+
+  function cannotMint({ user }: { user: number }) {
+    describe(`when user${user} tries to mint`, function () {
+      it('reverts', async function () {
+        const signer = signers()[user];
+
+        await assertRevert(
+          systems().Core.connect(signer).mock_AccountRBACMixin_mint(1, 666),
+          `RoleNotAuthorized("1", "${Roles.MINT}", "${await signer.getAddress()}")`,
+          systems().Core
+        );
+      });
+    });
+  }
 
   describe('AccountRBACMixin', function () {
     before('identify signers', async () => {
-      [, user1, user2] = signers();
+      [, user1, user2, user3] = signers();
     });
 
     before('create an account', async () => {
       await (await systems().Core.connect(user1).createAccount(1)).wait();
     });
 
-    describe('when an unauthorized user interacts with the account', function () {
-      describe('trying to set value A', function () {
-        it('reverts', async function () {
-          await assertRevert(
-            systems().Core.connect(user2).mock_AccountRBACMixin_stake(1, 666),
-            `RoleNotAuthorized("1", "${Roles.STAKE}", "${await user2.getAddress()}")`,
-            systems().Core
-          );
-        });
-      });
-
-      describe('trying to set value B', function () {
-        it('reverts', async function () {
-          await assertRevert(
-            systems().Core.connect(user2).mock_AccountRBACMixin_mint(1, 666),
-            `RoleNotAuthorized("1", "${Roles.MINT}", "${await user2.getAddress()}")`,
-            systems().Core
-          );
-        });
-      });
+    describe('before granting access to any account', function () {
+      canStake({ user: 1 });
+      canMint({ user: 1 });
+      cannotStake({ user: 2 });
+      cannotMint({ user: 2 });
+      cannotStake({ user: 3 });
+      cannotMint({ user: 3 });
     });
 
-    describe('when the owner interacts with the account', function () {
-      describe('staking', function () {
-        before('stake', async function () {
-          await (await systems().Core.connect(user1).mock_AccountRBACMixin_stake(1, 1337));
-        });
-
-        it('sets the stake mock value', async function () {
-          assertBn.equal(await systems().Core.mock_AccountRBACMixin_getStakeMock(), 1337);
-        });
+    describe('when granting stake access to user2', function () {
+      before('grant', async function () {
+        await (await systems().Core.connect(user1).grantRole(1, Roles.STAKE, await user2.getAddress())).wait();
       });
 
-      describe('minting', function () {
-        before('mint', async function () {
-          await (await systems().Core.connect(user1).mock_AccountRBACMixin_mint(1, 33));
-        });
+      canStake({ user: 1 });
+      canMint({ user: 1 });
+      canStake({ user: 2 });
+      cannotMint({ user: 2 });
+      cannotStake({ user: 3 });
+      cannotMint({ user: 3 });
 
-        it('sets the mint mock value', async function () {
-          assertBn.equal(await systems().Core.mock_AccountRBACMixin_getMintMock(), 33);
-        });
-      });
-    });
-
-    describe('when the owner grants access to another accout', function () {
-      describe('granting stake access', function () {
+      describe('when granting mint access to user2', function () {
         before('grant', async function () {
-          await (await systems().Core.connect(user1).grantRole(1, Roles.STAKE, await user2.getAddress())).wait();
+          await (await systems().Core.connect(user1).grantRole(1, Roles.MINT, await user2.getAddress())).wait();
         });
 
-        describe('when the authorized user uses stake access', function () {
-          before('stake', async function () {
-            await (await systems().Core.connect(user1).mock_AccountRBACMixin_stake(1, 42));
+        canStake({ user: 1 });
+        canMint({ user: 1 });
+        canStake({ user: 2 });
+        canMint({ user: 2 });
+        cannotStake({ user: 3 });
+        cannotMint({ user: 3 });
+
+        describe('when revoking stake access from user2', function () {
+          before('revoke', async function () {
+            await (await systems().Core.connect(user1).revokeRole(1, Roles.STAKE, await user2.getAddress())).wait();
           });
 
-          it('sets the stake mock value', async function () {
-            assertBn.equal(await systems().Core.mock_AccountRBACMixin_getStakeMock(), 42);
-          });
-        });
+          canStake({ user: 1 });
+          canMint({ user: 1 });
+          canMint({ user: 2 });
+          cannotStake({ user: 2 });
+          cannotStake({ user: 3 });
+          cannotMint({ user: 3 });
 
-        describe('when the authorized user tries to use mint access', function () {
-          it('reverts', async function () {
-            await assertRevert(
-              systems().Core.connect(user2).mock_AccountRBACMixin_mint(1, 666),
-              `RoleNotAuthorized("1", "${Roles.MINT}", "${await user2.getAddress()}")`,
-              systems().Core
-            );
+          describe('when granting stake access to user3', function () {
+            before('grant', async function () {
+              await (await systems().Core.connect(user1).grantRole(1, Roles.STAKE, await user3.getAddress())).wait();
+            });
+
+            canStake({ user: 1 });
+            canMint({ user: 1 });
+            canMint({ user: 2 });
+            cannotStake({ user: 2 });
+            canStake({ user: 3 });
+            cannotMint({ user: 3 });
+
+            describe('when the account owner transfers the account to user3', function () {
+              before('transfer account', async function () {
+                await (await systems().Account.connect(user1).transferFrom(
+                  await user1.getAddress(),
+                  await user3.getAddress(),
+                  1
+                )).wait();
+              });
+
+              cannotStake({ user: 1 });
+              cannotMint({ user: 1 });
+              canMint({ user: 2 });
+              cannotStake({ user: 2 });
+              canStake({ user: 3 });
+              canMint({ user: 3 });
+            });
           });
         });
       });
