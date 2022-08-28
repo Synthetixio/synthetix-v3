3167787984dd9961960de8e0d00ab0bcf10094a3
Alejandro <palebluedot@gmail.com>
2022-08-28 12:42:30 -0300

    Progress with AccountRBACMixin test review

---
+++ packages/synthetix-main/test/integration/mixins/AccountRBACMixin-OLD.test.ts
@@ -0,0 +1,131 @@
+import assert from 'assert/strict';
+import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber';
+import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
+import { ethers } from 'ethers';
+
+import { bootstrap } from '../bootstrap';
+
+// TODO
+describe.skip('AccountRBACMixin', function () {
+  const { signers, systems } = bootstrap();
+
+  let owner: ethers.Signer, user1: ethers.Signer, user2: ethers.Signer, user3: ethers.Signer;
+
+  let AccountRBACMixinMock: ethers.Contract;
+
+  // before('identify signers', async () => {
+  //   [owner, user1, user2, user3] = signers();
+  // });
+
+  // before('mint an account token', async () => {
+  //   await (await systems().Core.connect(user1).createAccount(1)).wait();
+  // });
+
+  // it('is minted', async () => {
+  //   assert.equal(await systems().Account.ownerOf(1), await user1.getAddress());
+  //   assertBn.equal(await systems().Account.balanceOf(await user1.getAddress()), 1);
+  // });
+
+  describe('when accessing as owner', () => {
+    // it('can interact with the contract', async () => {
+    //   await (await AccountRBACMixinMock.connect(user1).interactWithAccount(1, 42)).wait();
+    //   assertBn.equal(await AccountRBACMixinMock.getRBACValue(), 42);
+    // });
+
+    // describe('when attempting to interact as non owner', async () => {
+    //   it('reverts', async () => {
+    //     await assertRevert(
+    //       AccountRBACMixinMock.connect(user2).interactWithAccount(1, 1337),
+    //       `RoleNotAuthorized(1, "${ethers.utils.formatBytes32String(
+    //         'stake'
+    //       )}", "${user2.getAddress()}")`
+    //     );
+    //   });
+    // });
+
+    describe('when transfering ownership of systems().Account', async () => {
+      before('transfer to user2', async () => {
+        await (
+          await systems()
+            .Account.connect(user1)
+            .transferFrom(user1.getAddress(), user2.getAddress(), 1)
+        ).wait();
+      });
+
+      after('transfer back to user1', async () => {
+        await (
+          await systems()
+            .Account.connect(user2)
+            .transferFrom(user2.getAddress(), user1.getAddress(), 1)
+        ).wait();
+      });
+
+      it('can interact with the contract', async () => {
+        await (await AccountRBACMixinMock.connect(user2).interactWithAccount(1, 43)).wait();
+        assertBn.equal(await AccountRBACMixinMock.getRBACValue(), 43);
+      });
+
+      describe('when attempting to interact as non owner', async () => {
+        it('reverts', async () => {
+          await assertRevert(
+            AccountRBACMixinMock.connect(user1).interactWithAccount(1, 1337),
+            `RoleNotAuthorized(1, "${ethers.utils.formatBytes32String(
+              'stake'
+            )}", "${await user1.getAddress()}")`
+          );
+        });
+      });
+    });
+
+    describe('when granting access', async () => {
+      before('grant', async () => {
+        await (
+          await systems()
+            .Core.connect(user1)
+            .grantRole(1, ethers.utils.formatBytes32String('stake'), await user2.getAddress())
+        ).wait();
+
+        await (
+          await systems()
+            .Core.connect(user1)
+            .grantRole(1, ethers.utils.formatBytes32String('otherRole'), await user3.getAddress())
+        ).wait();
+      });
+
+      it('can interact with the contract', async () => {
+        await (await AccountRBACMixinMock.connect(user2).interactWithAccount(1, 44)).wait();
+        assertBn.equal(await AccountRBACMixinMock.getRBACValue(), 44);
+      });
+
+      describe('when attempting to interact as non owner / non authorized with that role', async () => {
+        it('reverts', async () => {
+          await assertRevert(
+            AccountRBACMixinMock.connect(user3).interactWithAccount(1, 1337),
+            `RoleNotAuthorized(1, "${ethers.utils.formatBytes32String(
+              'stake'
+            )}", "${await user3.getAddress()}")`
+          );
+        });
+      });
+
+      describe('when attempting to access after the role was revoked', async () => {
+        before('revoke access to user2', async () => {
+          await (
+            await systems()
+              .Core.connect(user1)
+              .revokeRole(1, ethers.utils.formatBytes32String('stake'), await user2.getAddress())
+          ).wait();
+        });
+
+        it('reverts', async () => {
+          await assertRevert(
+            AccountRBACMixinMock.connect(user2).interactWithAccount(1, 1337),
+            `RoleNotAuthorized(1, "${ethers.utils.formatBytes32String(
+              'stake'
+            )}", "${await user2.getAddress()}")`
+          );
+        });
+      });
+    });
+  });
+});
--- packages/synthetix-main/test/integration/mixins/AccountRBACMixin.test.ts
+++ packages/synthetix-main/test/integration/mixins/AccountRBACMixin.test.ts
@@ -3,33 +3,97 @@ import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber'
 import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
 import { ethers } from 'ethers';
 import { bootstrap } from '../bootstrap';
-import { FakeContract, smock } from '@defi-wonderland/smock';
+
+const Roles = {
+  STAKE: ethers.utils.formatBytes32String('ROLE_STAKE'),
+  MINT: ethers.utils.formatBytes32String('ROLE_MINT'),
+};
 
 describe.only('AccountRBACMixin', function () {
-  const { signers, systems, provider } = bootstrap();
+  const { signers, systems } = bootstrap();
 
   let user1: ethers.Signer;
   let user2: ethers.Signer;
-
-  let fakeSystem: FakeContract;
 
   describe('AccountRBACMixin', function () {
     before('identify signers', async () => {
       [, user1, user2] = signers();
     });
 
-    before('smock system', async function () {
-      fakeSystem = await smock.fake('CollateralModule');
-
-      fakeSystem.stake.returns('woof');
+    before('create an account', async () => {
+      await (await systems().Core.connect(user1).createAccount(1)).wait();
     });
 
-    it('dummy', async function () {
-      console.log('dummy test');
+    describe('when an unauthorized user interacts with the account', function () {
+      describe('trying to set value A', function () {
+        it('reverts', async function () {
+          await assertRevert(
+            systems().Core.connect(user2).mock_AccountRBACMixin_stake(1, 666),
+            `RoleNotAuthorized("1", "${Roles.STAKE}", "${await user2.getAddress()}")`,
+            systems().Core
+          );
+        });
+      });
 
-      const tx = await fakeSystem.connect(user1).stake();
-      const receipt = await tx.wait();
-      console.log(receipt);
+      describe('trying to set value B', function () {
+        it('reverts', async function () {
+          await assertRevert(
+            systems().Core.connect(user2).mock_AccountRBACMixin_mint(1, 666),
+            `RoleNotAuthorized("1", "${Roles.MINT}", "${await user2.getAddress()}")`,
+            systems().Core
+          );
+        });
+      });
+    });
+
+    describe('when the owner interacts with the account', function () {
+      describe('staking', function () {
+        before('stake', async function () {
+          await (await systems().Core.connect(user1).mock_AccountRBACMixin_stake(1, 1337));
+        });
+
+        it('sets the stake mock value', async function () {
+          assertBn.equal(await systems().Core.mock_AccountRBACMixin_getStakeMock(), 1337);
+        });
+      });
+
+      describe('minting', function () {
+        before('mint', async function () {
+          await (await systems().Core.connect(user1).mock_AccountRBACMixin_mint(1, 33));
+        });
+
+        it('sets the mint mock value', async function () {
+          assertBn.equal(await systems().Core.mock_AccountRBACMixin_getMintMock(), 33);
+        });
+      });
+    });
+
+    describe('when the owner grants access to another accout', function () {
+      describe('granting stake access', function () {
+        before('grant', async function () {
+          await (await systems().Core.connect(user1).grantRole(1, Roles.STAKE, await user2.getAddress())).wait();
+        });
+
+        describe('when the authorized user uses stake access', function () {
+          before('stake', async function () {
+            await (await systems().Core.connect(user1).mock_AccountRBACMixin_stake(1, 42));
+          });
+
+          it('sets the stake mock value', async function () {
+            assertBn.equal(await systems().Core.mock_AccountRBACMixin_getStakeMock(), 42);
+          });
+        });
+
+        describe('when the authorized user tries to use mint access', function () {
+          it('reverts', async function () {
+            await assertRevert(
+              systems().Core.connect(user2).mock_AccountRBACMixin_mint(1, 666),
+              `RoleNotAuthorized("1", "${Roles.MINT}", "${await user2.getAddress()}")`,
+              systems().Core
+            );
+          });
+        });
+      });
     });
   });
 });
