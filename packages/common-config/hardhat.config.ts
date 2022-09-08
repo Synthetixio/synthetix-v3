01d62025b65223761b0a6a6886935bfc652f0aa0
Sunny Vempati <sunnyvempati@gmail.com>
2022-09-08 15:15:40 -0500

    deploy to goerli; fix ast

--- packages/common-config/hardhat.config.ts
+++ packages/common-config/hardhat.config.ts
@@ -51,6 +51,18 @@ const config = {
     project: 'synthetix',
     username: 'synthetix-services',
   },
+  cannon: {
+    ipfsConnection: {
+      protocol: 'https',
+      host: 'ipfs.infura.io',
+      port: 5001,
+      headers: {
+        authorization: `Basic ${Buffer.from(
+          process.env.INFURA_IPFS_ID + ':' + process.env.INFURA_IPFS_SECRET
+        ).toString('base64')}`,
+      },
+    },
+  },
 };
 
 export default config;
