a0d454f4b7050e4d767eb2de01b503ed4e46036e
Peiman <peymannaderi3@yahoo.com>
2022-09-28 18:55:21 +0330

    oracle manager package added

---
+++ packages/oracle-manager/hardhat.config.ts
@@ -0,0 +1,16 @@
+//import '@typechain/hardhat'
+
+import '@nomiclabs/hardhat-ethers';
+import 'solidity-docgen';
+
+import commonConfig from '@synthetixio/common-config/hardhat.config';
+
+const config = {
+  ...commonConfig,
+  solidity: '0.8.11',
+  docgen: {
+    exclude: ['./interfaces/external', './modules', './storage', './Proxy.sol', './Router.sol'],
+  },
+};
+
+export default config;
