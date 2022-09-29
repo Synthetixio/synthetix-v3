6650792d3a74b764438a7555eca9473c43186859
Sunny Vempati <sunnyvempati@gmail.com>
2022-09-29 00:52:26 -0500

    remove interface

--- markets/spot-market/interfaces/ISynth.sol
+++ markets/spot-market/interfaces/ISynth.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@synthetixio/main/contracts/interfaces/external/IMarket.sol";
-
 /// @title Spot Market Synth Interface
 interface ISynth {
     function mint(address to, uint amount) external;
