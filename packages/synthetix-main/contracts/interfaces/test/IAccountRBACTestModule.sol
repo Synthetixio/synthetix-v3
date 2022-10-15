7c44736d31000e0e9f0bb88eb068270da994dfa7
Daniel Beal <git@danb.email>
2022-10-15 16:45:02 -0700

    remove library testing files for now

--- packages/synthetix-main/contracts/interfaces/test/IAccountRBACTestModule.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-/// @title Module for managing preferred and approved pools via SCCPs
-interface IAccountRBACTestModule {
-    // empty for now
-}
--- packages/synthetix-main/contracts/modules/test/AccountRBACTestModule.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "../../interfaces/test/IAccountRBACTestModule.sol";
-
-contract AccountRBACTestModule is IAccountRBACTestModule {
-    // empty for now
-}
