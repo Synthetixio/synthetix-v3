71cd1570e335190492e453670a3c2b7087d2d7c3
Sunny Vempati <sunnyvempati@gmail.com>
2022-10-21 14:44:10 -0500

    add events

--- packages/core-modules/contracts/modules/FeatureFlagModule.sol
+++ packages/core-modules/contracts/modules/FeatureFlagModule.sol
@@ -9,15 +9,25 @@ import "../interfaces/IFeatureFlag.sol";
 contract FeatureFlagModule is IFeatureFlag, FeatureFlagMixin, OwnableMixin {
     using SetUtil for SetUtil.AddressSet;
 
+    event FeatureFlagSet(bytes32 feature, bool value);
+    event FeatureFlagAddressAdded(bytes32 feature, address account);
+    event FeatureFlagAddressRemoved(bytes32 feature, address account);
+
     function setFeatureFlag(bytes32 feature, bool value) external override onlyOwner {
         _featureFlagStore().featureFlags[feature].enabled = value;
+
+        emit FeatureFlagSet(feature, value);
     }
 
     function addToFeatureFlag(bytes32 feature, address permissioned) external override onlyOwner {
         _featureFlagStore().featureFlags[feature].permissionedAddresses.add(permissioned);
+
+        emit FeatureFlagAddressAdded(feature, permissioned);
     }
 
     function removeFromFeatureFlag(bytes32 feature, address permissioned) external override onlyOwner {
         _featureFlagStore().featureFlags[feature].permissionedAddresses.remove(permissioned);
+
+        emit FeatureFlagAddressRemoved(feature, permissioned);
     }
 }
