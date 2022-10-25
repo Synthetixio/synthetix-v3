eacf577e8c9832b66e9c38cbcd939f26016c5c86
Matías <mjlescano@protonmail.com>
2022-10-25 16:39:34 +0200

    update .solcover config

--- packages/synthetix-main/.solcover.js
+++ packages/synthetix-main/.solcover.js
@@ -1,12 +1,4 @@
 module.exports = {
   ...require('@synthetixio/common-config/.solcover.js'),
-  skipFiles: [
-    'mocks',
-    'Router.sol',
-    'AccountRouter.sol',
-    'CoreRouter.sol',
-    'ESNXRouter.sol',
-    'SNXRouter.sol',
-    'USDRouter.sol',
-  ],
+  skipFiles: ['mocks', 'routers'],
 };
