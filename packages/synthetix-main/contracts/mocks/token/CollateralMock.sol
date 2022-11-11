1dd36098fd75f6b9f40cecabf0e0ed156f60d3d2
Peiman <peymannaderi3@yahoo.com>
2022-11-11 17:24:05 +0330

    fix: CollateralMock version

--- packages/synthetix-main/contracts/mocks/token/CollateralMock.sol
+++ packages/synthetix-main/contracts/mocks/token/CollateralMock.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import "@synthetixio/core-contracts/contracts/token/ERC20.sol";
 import "../../interfaces/external/IRewardDistributor.sol";
