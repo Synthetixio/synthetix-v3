ebf2a927f0f1c34835f670023ceac626cb8ac188
Matías <mjlescano@protonmail.com>
2022-11-16 21:57:26 +0100

    update cannon deployment

---
+++ packages/oracle-manager/contracts/modules/OwnerModule.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {OwnerModule as BaseOwnerModule} from "@synthetixio/core-modules/contracts/modules/OwnerModule.sol";
+
+// solhint-disable-next-line no-empty-blocks
+contract OwnerModule is BaseOwnerModule {
+
+}
