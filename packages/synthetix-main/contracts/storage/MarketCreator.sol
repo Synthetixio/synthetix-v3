f75f96208d0f1e6f301ccfd7a583ed2d2619dfde
Alejandro <palebluedot@gmail.com>
2022-11-29 15:12:28 -0600

    WIP MarketCreator

--- packages/synthetix-main/contracts/storage/MarketCreator.sol
+++ packages/synthetix-main/contracts/storage/MarketCreator.sol
@@ -1,12 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./Market.sol";
+
 /**
  * @title TODO
  */
 library MarketCreator {
     struct Data {
-        uint[] marketIdsForAddress;
+        mapping(address => uint[]) marketIdsForAddress;
         uint lastCreatedMarketId;
     }
 
@@ -18,12 +20,34 @@ library MarketCreator {
     }
 
     /**
+     * @dev Given an external contract address representing an `IMarket`, creates a new id for the market, and tracks it internally in the system.
+     *
+     * The id used to track the market will be automatically assigned by the system according to the last id used.
+     *
+     * Note: If an external `IMarket` contract tracks several market ids, this function should be called for each market it tracks, resulting in multiple ids for the same address.
+     */
+    function create(address marketAddress) internal returns (Market.Data storage market) {
+        uint128 id = _loadLastId();
+
+        id++;
+
+        market = Market.load(id);
+
+        market.id = id;
+        market.marketAddress = marketAddress;
+
+        // set indexes
+        _storeLastId(id);
+        loadIdsByAddress(marketAddress).push(id);
+    }
+
+    /**
      * @dev Returns an array of market ids representing the markets linked to the system at a particular external contract address.
      *
      * Note: A contract implementing the `IMarket` interface may represent more than just one market, and thus several market ids could be associated to a single external contract address.
      */
-    function loadIdsByAddress(address addr) internal pure returns (uint[] storage ids) {
-        return _loadMarketStore().marketIdsForAddress[addr];
+    function loadIdsByAddress(address marketAddress) internal pure returns (uint[] storage ids) {
+        return _loadMarketStore().marketIdsForAddress[marketAddress];
     }
 
     /**
@@ -31,7 +55,7 @@ library MarketCreator {
      *
      * TODO: Use constants for storage slots, here and possible everywhere in the code.
      */
-    function loadLastId() internal view returns (uint128 data) {
+    function _loadLastId() private view returns (uint128 data) {
         return _loadMarketStore().lastCreatedMarketId;
     }
 
@@ -42,29 +66,7 @@ library MarketCreator {
      *
      * TODO: Use constants for storage slots, here and possible everywhere in the code.
      */
-    function storeLastId(uint128 newValue) internal {
+    function _storeLastId(uint128 newValue) private {
         _loadMarketStore().lastCreatedMarketId = newValue;
-    }
-
-    /**
-     * @dev Given an external contract address representing an `IMarket`, creates a new id for the market, and tracks it internally in the system.
-     *
-     * The id used to track the market will be automatically assigned by the system according to the last id used.
-     *
-     * Note: If an external `IMarket` contract tracks several market ids, this function should be called for each market it tracks, resulting in multiple ids for the same address.
-     */
-    function create(address market) internal returns (Market.Data storage self) {
-        uint128 id = loadLastId();
-
-        id++;
-
-        self = load(id);
-
-        self.id = id;
-        self.marketAddress = market;
-
-        // set indexes
-        storeLastId(id);
-        loadIdsByAddress(market).push(id);
     }
 }
