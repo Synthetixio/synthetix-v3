c5501c245f6d805359dd17bd4213f5bea12a184e
Alejandro <palebluedot@gmail.com>
2022-11-29 15:48:59 -0600

    Clean ups

--- packages/synthetix-main/contracts/storage/MarketCreator.sol
+++ packages/synthetix-main/contracts/storage/MarketCreator.sol
@@ -5,6 +5,8 @@ import "./Market.sol";
 
 /**
  * @title TODO
+ *
+ * TODO: Rename to MarketIndexes
  */
 library MarketCreator {
     struct Data {
@@ -12,7 +14,7 @@ library MarketCreator {
         uint128 lastCreatedMarketId;
     }
 
-    function _loadMarketStore() private pure returns (Data storage data) {
+    function _getStore() private pure returns (Data storage data) {
         bytes32 s = keccak256("MarketStore");
         assembly {
             data.slot := s
@@ -27,8 +29,9 @@ library MarketCreator {
      * Note: If an external `IMarket` contract tracks several market ids, this function should be called for each market it tracks, resulting in multiple ids for the same address.
      */
     function create(address marketAddress) internal returns (Market.Data storage market) {
-        uint128 id = _loadLastId();
+        Data storage store = _getStore();
 
+        uint128 id = store.lastCreatedMarketId;
         id++;
 
         market = Market.load(id);
@@ -36,8 +39,8 @@ library MarketCreator {
         market.id = id;
         market.marketAddress = marketAddress;
 
-        // set indexes
-        _storeLastId(id);
+        store.lastCreatedMarketId = id;
+
         loadIdsByAddress(marketAddress).push(id);
     }
 
@@ -47,26 +50,6 @@ library MarketCreator {
      * Note: A contract implementing the `IMarket` interface may represent more than just one market, and thus several market ids could be associated to a single external contract address.
      */
     function loadIdsByAddress(address marketAddress) internal view returns (uint128[] storage ids) {
-        return _loadMarketStore().marketIdsForAddress[marketAddress];
-    }
-
-    /**
-     * @dev Retrieves the id of the last market created.
-     *
-     * TODO: Use constants for storage slots, here and possible everywhere in the code.
-     */
-    function _loadLastId() private view returns (uint128 data) {
-        return _loadMarketStore().lastCreatedMarketId;
-    }
-
-    /**
-     * @dev Caches the id of the last market that was created.
-     *
-     * Used to automatically generate a new id when a market is created.
-     *
-     * TODO: Use constants for storage slots, here and possible everywhere in the code.
-     */
-    function _storeLastId(uint128 newValue) private {
-        _loadMarketStore().lastCreatedMarketId = newValue;
+        return _getStore().marketIdsForAddress[marketAddress];
     }
 }
