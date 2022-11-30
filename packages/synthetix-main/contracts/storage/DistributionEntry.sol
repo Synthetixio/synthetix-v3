f040902e266e0383567adf9591fd8e5d43795805
Leonardo Massazza <lmassazza@gmail.com>
2022-11-30 14:40:37 -0600

    wip checkpoint

--- packages/synthetix-main/contracts/storage/DistributionEntry.sol
+++ packages/synthetix-main/contracts/storage/DistributionEntry.sol
@@ -9,7 +9,7 @@ library DistributionEntry {
     error InvalidParameters(string incorrectParameter, string help);
 
     struct Data {
-        // amount which should be applied to valuePerShare at the given time below, or
+        // amount which should be applied at the given time below, or
         int128 scheduledValueD18;
         // set to <= block.timestamp to distribute immediately to currently staked users
         int64 start;
@@ -25,26 +25,26 @@ library DistributionEntry {
     function distribute(
         Data storage entry,
         Distribution.Data storage dist,
-        int amount,
+        int amountD18,
         uint start,
         uint duration
-    ) internal returns (int diff) {
-        uint totalShares = dist.totalSharesD18;
+    ) internal returns (int diffD18) {
+        uint totalSharesD18 = dist.totalSharesD18;
 
-        if (totalShares == 0) {
+        if (totalSharesD18 == 0) {
             revert InvalidParameters("amount", "can't distribute to empty distribution");
         }
 
         int curTime = int128(int(block.timestamp));
 
         // ensure any previously active distribution has applied
-        diff += updateEntry(entry, dist.totalSharesD18);
+        diffD18 += updateEntry(entry, dist.totalSharesD18);
 
         if (start + duration <= uint(curTime)) {
             // update any rewards which may have accrued since last run
 
             // instant distribution--immediately disperse amount
-            diff += (amount * 1e18) / int(totalShares);
+            diffD18 += (amountD18 * 1e18) / int(totalSharesD18);
 
             entry.lastUpdate = 0;
             entry.start = 0;
@@ -52,22 +52,22 @@ library DistributionEntry {
             entry.scheduledValueD18 = 0;
         } else {
             // set distribution schedule
-            entry.scheduledValueD18 = int128(amount);
+            entry.scheduledValueD18 = int128(amountD18);
             entry.start = int64(int(start));
             entry.duration = int32(int(duration));
 
             // the amount is actually the amount distributed already *plus* whatever has been specified now
             entry.lastUpdate = 0;
 
-            diff += updateEntry(entry, dist.totalSharesD18);
+            diffD18 += updateEntry(entry, dist.totalSharesD18);
         }
     }
 
     /**
      * call every time before `totalShares` changes
      */
-    function updateEntry(Data storage entry, uint totalSharesAmount) internal returns (int128) {
-        if (entry.scheduledValueD18 == 0 || totalSharesAmount == 0) {
+    function updateEntry(Data storage entry, uint totalSharesAmountD18) internal returns (int128) {
+        if (entry.scheduledValueD18 == 0 || totalSharesAmountD18 == 0) {
             // cannot process distributed rewards if a pool is empty.
             return 0;
         }
@@ -75,7 +75,7 @@ library DistributionEntry {
         // exciting type casting here
         int128 curTime = int128(int(block.timestamp));
 
-        int valuePerShareChange = 0;
+        int valuePerShareChangeD18 = 0;
 
         if (curTime < entry.start) {
             return 0;
@@ -83,23 +83,23 @@ library DistributionEntry {
 
         // determine whether this is an instant distribution or a delayed distribution
         if (entry.duration == 0 && entry.lastUpdate < entry.start) {
-            valuePerShareChange = (int(entry.scheduledValueD18) * 1e18) / int(totalSharesAmount);
+            valuePerShareChangeD18 = (int(entry.scheduledValueD18) * 1e18) / int(totalSharesAmountD18);
         } else if (entry.lastUpdate < entry.start + entry.duration) {
             // find out what is "newly" distributed
-            int lastUpdateDistributed = entry.lastUpdate < entry.start
+            int lastUpdateDistributedD18 = entry.lastUpdate < entry.start
                 ? int128(0)
                 : (int(entry.scheduledValueD18) * (entry.lastUpdate - entry.start)) / entry.duration;
 
-            int curUpdateDistributed = entry.scheduledValueD18;
+            int curUpdateDistributedD18 = entry.scheduledValueD18;
             if (curTime < entry.start + entry.duration) {
-                curUpdateDistributed = (curUpdateDistributed * (curTime - entry.start)) / entry.duration;
+                curUpdateDistributedD18 = (curUpdateDistributedD18 * (curTime - entry.start)) / entry.duration;
             }
 
-            valuePerShareChange = (int(curUpdateDistributed - lastUpdateDistributed) * 1e18) / int(totalSharesAmount);
+            valuePerShareChangeD18 = (int(curUpdateDistributedD18 - lastUpdateDistributedD18) * 1e18) / int(totalSharesAmountD18);
         }
 
         entry.lastUpdate = int32(curTime);
 
-        return int128(valuePerShareChange);
+        return int128(valuePerShareChangeD18);
     }
 }
