4f543cd6d834f6ddc394b9ae0da8c0becce480ce
Sunny Vempati <sunnyvempati@gmail.com>
2022-11-29 22:28:55 -0600

    fix stress test

--- packages/synthetix-main/test/integration/storage/Distribution.test.ts
+++ packages/synthetix-main/test/integration/storage/Distribution.test.ts
@@ -5,7 +5,7 @@ import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert
 import { Contract, ethers } from 'ethers';
 import { bootstrap } from '../bootstrap';
 import { snapshotCheckpoint } from '../../utils';
-import Wei, { wei } from '@synthetixio/wei';
+import { wei } from '@synthetixio/wei';
 
 const distUtils = {
   getActor: (id: string) => ethers.utils.formatBytes32String(id),
@@ -14,7 +14,7 @@ const distUtils = {
 const bn = (n: number) => wei(n).toBN();
 const hp = wei(10).pow(27);
 
-describe.only('Distribution', async () => {
+describe('Distribution', async () => {
   const { systems, signers, provider } = bootstrap();
   const restore = snapshotCheckpoint(provider);
   let FakeDistributionModule: Contract;
@@ -343,7 +343,7 @@ describe.only('Distribution', async () => {
       });
 
       it('has correct actor values', async () => {
-        console.log(await FakeDistributionModule.Distribution_getActorValue(actor1));
+        // TODO
         assertBn.equal(await FakeDistributionModule.Distribution_getActorValue(actor1), bn(10));
         assertBn.equal(await FakeDistributionModule.Distribution_getActorValue(actor2), bn(20));
       });
