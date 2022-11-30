2ad6736c35c62138691ee1f08e4693102617c71d
Sunny Vempati <sunnyvempati@gmail.com>
2022-11-30 16:09:09 -0600

    chainlink twap func

--- packages/oracle-manager/contracts/utils/ChainlinkNodeLibrary.sol
+++ packages/oracle-manager/contracts/utils/ChainlinkNodeLibrary.sol
@@ -6,40 +6,42 @@ import "../interfaces/external/IAggregatorV3Interface.sol";
 
 library ChainlinkNodeLibrary {
     function process(bytes memory parameters) internal view returns (Node.Data memory) {
-        (address chainlinkAggr, bool useTwap) = abi.decode(parameters, (address, bool));
+        (address chainlinkAggr, uint twapTimeInterval) = abi.decode(parameters, (address, uint));
 
         (uint80 roundId, int256 price, , uint256 updatedAt, ) = IAggregatorV3Interface(chainlinkAggr).latestRoundData();
-        int256 finalPrice = useTwap ? getTwapPrice(chainlinkAggr, roundId) : price;
+        int256 finalPrice = twapTimeInterval == 0 ? price : getTwapPrice(chainlinkAggr, roundId, price, twapTimeInterval);
         return Node.Data(finalPrice, updatedAt, 0, 0);
     }
 
-    function getTwapPrice(address chainlinkAggr, uint256 previousRoundId) internal view returns (int256) {
-        (uint64 phaseId, uint64 aggregatorRoundId) = decryptRoundId(previousRoundId);
+    function getTwapPrice(
+        address chainlinkAggr,
+        uint80 latestRoundId,
+        int latestPrice,
+        uint twapTimeInterval
+    ) internal view returns (int256) {
+        int priceSum = latestPrice;
+        uint priceCount = 1;
 
-        uint256 sum = 0;
-        uint256 count = 0;
+        uint startTime = block.timestamp - twapTimeInterval;
 
-        for (uint256 i = 0; i < 10; i++) {
-            (uint80 roundId, int256 price, , uint256 updatedAt, ) = IAggregatorV3Interface(chainlinkAggr).getRoundData(
-                aggregatorRoundId
-            );
-            (uint64 roundPhaseId, uint64 roundAggregatorRoundId) = decryptRoundId(roundId);
-
-            if (roundPhaseId != phaseId) {
+        while (latestRoundId > 0) {
+            try IAggregatorV3Interface(chainlinkAggr).getRoundData(--latestRoundId) returns (
+                uint80,
+                int256 answer,
+                uint256,
+                uint256 updatedAt,
+                uint80
+            ) {
+                if (updatedAt < startTime) {
+                    break;
+                }
+                priceSum += answer;
+                priceCount++;
+            } catch {
                 break;
             }
-
-            sum += uint256(price);
-            count += 1;
-            aggregatorRoundId = roundAggregatorRoundId;
         }
 
-        return int256(count == 0 ? 0 : sum / count);
-    }
-
-    function decryptRoundId(uint256 roundId) internal pure returns (uint64 phaseId, uint64 aggregatorRoundId) {
-        uint64 largestNum = 0xFFFFFFFFFFFFFFFF;
-        phaseId = uint64(roundId >> 64);
-        aggregatorRoundId = uint64(roundId & largestNum);
+        return priceSum / int(priceCount);
     }
 }
