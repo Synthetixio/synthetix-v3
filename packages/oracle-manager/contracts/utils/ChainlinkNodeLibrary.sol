d40f694255b9c1df41f0b3da621e63888a4e2dd3
Sunny Vempati <sunnyvempati@gmail.com>
2022-11-30 09:03:30 -0600

    wip

--- packages/oracle-manager/contracts/utils/ChainlinkNodeLibrary.sol
+++ packages/oracle-manager/contracts/utils/ChainlinkNodeLibrary.sol
@@ -6,8 +6,40 @@ import "../interfaces/external/IAggregatorV3Interface.sol";
 
 library ChainlinkNodeLibrary {
     function process(bytes memory parameters) internal view returns (Node.Data memory) {
-        IAggregatorV3Interface chainlinkAggr = IAggregatorV3Interface(abi.decode(parameters, (address)));
-        (, int256 price, , uint256 updatedAt, ) = chainlinkAggr.latestRoundData();
-        return Node.Data(price, updatedAt, 0, 0);
+        (address chainlinkAggr, bool useTwap) = abi.decode(parameters, (address, bool));
+
+        (uint80 roundId, int256 price, , uint256 updatedAt, ) = IAggregatorV3Interface(chainlinkAggr).latestRoundData();
+        int256 finalPrice = useTwap ? getTwapPrice(chainlinkAggr, roundId) : price;
+        return Node.Data(finalPrice, updatedAt, 0, 0);
+    }
+
+    function getTwapPrice(address chainlinkAggr, uint256 previousRoundId) internal view returns (int256) {
+        (uint64 phaseId, uint64 aggregatorRoundId) = decryptRoundId(previousRoundId);
+
+        uint256 sum = 0;
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < 10; i++) {
+            (uint80 roundId, int256 price, , uint256 updatedAt, ) = IAggregatorV3Interface(chainlinkAggr).getRoundData(
+                aggregatorRoundId
+            );
+            (uint64 roundPhaseId, uint64 roundAggregatorRoundId) = decryptRoundId(roundId);
+
+            if (roundPhaseId != phaseId) {
+                break;
+            }
+
+            sum += uint256(price);
+            count += 1;
+            aggregatorRoundId = roundAggregatorRoundId;
+        }
+
+        return int256(count == 0 ? 0 : sum / count);
+    }
+
+    function decryptRoundId(uint256 roundId) internal pure returns (uint64 phaseId, uint64 aggregatorRoundId) {
+        uint64 largestNum = 0xFFFFFFFFFFFFFFFF;
+        phaseId = uint64(roundId >> 64);
+        aggregatorRoundId = uint64(roundId & largestNum);
     }
 }
