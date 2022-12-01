121f5de15e710848ea12bb0621d5fb8ee0958ea2
Alejandro <palebluedot@gmail.com>
2022-12-01 10:24:47 -0600

    Remove empty file

--- packages/core-contracts/contracts/utils/SafeCast.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "./SafeCast/SafeCastI128.sol";
-import "./SafeCast/SafeCastI256.sol";
-import "./SafeCast/SafeCastU128.sol";
-import "./SafeCast/SafeCastU256.sol";
