5a2ce723b4a31295c078c2eee2b5c15f536b6636
Alejandro <palebluedot@gmail.com>
2022-12-01 10:30:57 -0600

    Revert "Remove empty file"
    This reverts commit 121f5de15e710848ea12bb0621d5fb8ee0958ea2.

---
+++ packages/core-contracts/contracts/utils/SafeCast.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./SafeCast/SafeCastI128.sol";
+import "./SafeCast/SafeCastI256.sol";
+import "./SafeCast/SafeCastU128.sol";
+import "./SafeCast/SafeCastU256.sol";
