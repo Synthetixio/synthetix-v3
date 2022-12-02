e08b9fbe2019836e464a81c5f6aaade3c2d06055
Alejandro <palebluedot@gmail.com>
2022-12-02 11:21:32 -0600

    Split CollateralModule

--- packages/synthetix-main/contracts/interfaces/ICollateralModule.sol
+++ packages/synthetix-main/contracts/interfaces/ICollateralModule.sol
@@ -6,11 +6,6 @@ import "../storage/CollateralConfiguration.sol";
 /// @title Module for managing collateral
 interface ICollateralModule {
     /**
-     * @notice Emitted when a collateral type’s configuration is created or updated.
-     */
-    event CollateralConfigured(address indexed collateralType, CollateralConfiguration.Data config);
-
-    /**
      * @notice Emitted when `amount` of collateral of type `collateralType` is deposited to account `accountId` by `sender`.
      */
     event Deposited(uint128 indexed accountId, address indexed collateralType, uint amount, address indexed sender);
@@ -19,39 +14,6 @@ interface ICollateralModule {
      * @notice Emitted when `amount` of collateral of type `collateralType` is withdrawn from account `accountId` by `sender`.
      */
     event Withdrawn(uint128 indexed accountId, address indexed collateralType, uint amount, address indexed sender);
-
-    /**
-     * @notice Creates or updates the configuration for given `collateralType`.
-     *
-     * Requirements:
-     *
-     * - `msg.sender` must be the owner of the system.
-     *
-     * Emits a {CollateralConfigured} event.
-     *
-     */
-    function configureCollateral(CollateralConfiguration.Data memory config) external;
-
-    /**
-     * @notice Returns a list of detailed information pertaining to all collateral types registered in the system.
-     */
-    function getCollateralConfigurations(bool hideDisabled)
-        external
-        view
-        returns (CollateralConfiguration.Data[] memory collaterals);
-
-    /**
-     * @notice Returns detailed information pertaining the specified collateral type.
-     */
-    function getCollateralConfiguration(address collateralType)
-        external
-        view
-        returns (CollateralConfiguration.Data memory collateral);
-
-    /**
-     * @notice Returns the current value of a specified collateral type
-     */
-    function getCollateralPrice(address collateralType) external view returns (uint);
 
     /**
      * @notice Deposits `amount` of collateral of type `collateralType` into account `accountId`.
@@ -116,23 +78,4 @@ interface ICollateralModule {
         uint amount,
         uint64 expireTimestamp
     ) external;
-
-    /*
-    /// @notice Returns the amount of collateral of type `collateralType` staked with account `accountId` that can be unstaked.
-    /// @dev DEPENDENT ON 305 (Would be combined with `getAccountUnstakebleCollateral` into `getAccountAvailableCollateral`)
-    function getAccountUnstakebleCollateral(uint accountId, address collateralType) external view returns (uint);
-
-    /// @notice Returns the amount of collateral of type `collateralType` staked with account `accountId` that can be delegated to a pool.
-    /// @dev DEPENDENT ON 305 (Would be combined with `getAccountUnstakebleCollateral` into `getAccountAvailableCollateral`)
-    function getAccountUnassignedCollateral(uint accountId, address collateralType) external view returns (uint);
-
-
-    /// @notice Redeems the system escrow tokens into reward tokens
-    /// @dev DEPENDENT ON 305
-    function redeemReward(
-        uint128 accountId,
-        uint amount,
-        uint duration
-    ) external;
-*/
 }
