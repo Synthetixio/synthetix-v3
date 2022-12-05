93b75fa53f115a770e54444d4595d8e1fdbbe9d1
Alejandro <palebluedot@gmail.com>
2022-12-05 14:44:32 -0600

    WIP @params

--- protocol/synthetix/contracts/interfaces/IMarketCollateralModule.sol
+++ protocol/synthetix/contracts/interfaces/IMarketCollateralModule.sol
@@ -7,6 +7,10 @@ pragma solidity ^0.8.0;
 interface IMarketCollateralModule {
     /**
      * @notice Emitted when `amount` of collateral of type `collateralType` is deposited to market `marketId` by `sender`.
+     * @param marketId The id of the market in which collateral was deposited.
+     * @param collateralType The address of the collateral that was directly deposited in the market.
+     * @param tokenAmount The amount of tokens that were deposited.
+     * @param sender The address that triggered the deposit.
      */
     event MarketCollateralDeposited(
         uint128 indexed marketId,
@@ -17,6 +21,10 @@ interface IMarketCollateralModule {
 
     /**
      * @notice Emitted when `amount` of collateral of type `collateralType` is withdrawn from market `marketId` by `sender`.
+     * @param marketId The id of the market from which collateral was withdrawn.
+     * @param collateralType The address of the collateral that was withdrawn from the market.
+     * @param tokenAmount The amount of tokens that were withdrawn.
+     * @param sender The address that triggered the withdrawal.
      */
     event MarketCollateralWithdrawn(
         uint128 indexed marketId,
@@ -27,6 +35,10 @@ interface IMarketCollateralModule {
 
     /**
      * @notice Emitted when the system owner specifies the maximum depositable collateral of a given type in a given market.
+     * @param marketId The id of the market for which the maximum was configured.
+     * @param collateralType The address of the collateral for which the maximum was configured.
+     * @param systemAmount The amount to which the maximum was set.
+     * @param sender The address that triggered the configuration change.
      */
     event MaximumMarketCollateralConfigured(
         uint128 indexed marketId,
@@ -37,6 +49,9 @@ interface IMarketCollateralModule {
 
     /**
      * @notice Allows a market to deposit collateral.
+     * @param marketId The id of the market in which the collateral was directly deposited.
+     * @param collateralType The address of the collateral that was deposited in the market.
+     * @param amount The amount of collateral that was deposited.
      */
     function depositMarketCollateral(
         uint128 marketId,
@@ -46,6 +61,9 @@ interface IMarketCollateralModule {
 
     /**
      * @notice Allows a market to withdraw collateral that it has previously deposited.
+     * @param marketId The id of the market from which the collateral was withdrawn.
+     * @param collateralType The address of the collateral that was withdrawn from the market.
+     * @param amount The amount of collateral that was withdrawn.
      */
     function withdrawMarketCollateral(
         uint128 marketId,
@@ -55,6 +73,9 @@ interface IMarketCollateralModule {
 
     /**
      * @notice Allow the system owner to configure the maximum amount of a given collateral type that a specified market is allowed to deposit.
+     * @param marketId The id of the market for which the maximum is to be configured.
+     * @param collateralType The address of the collateral for which the maximum is to be applied.
+     * @param amount The amount that is to be set as the new maximum.
      */
     function configureMaximumMarketCollateral(
         uint128 marketId,
@@ -64,17 +85,20 @@ interface IMarketCollateralModule {
 
     /**
      * @notice Return the total maximum amount of a given collateral type that a specified market is allowed to deposit.
+     * @param marketId The id of the market for which the maximum is being queried.
+     * @param collateralType The address of the collateral for which the maximum is being queried.
+     * @returns amount The maximum amount of collateral set for the market.
      */
-    function getMaximumMarketCollateral(
-        uint128 marketId,
-        address collateralType
-    ) external returns (uint256);
+    function getMaximumMarketCollateral(uint128 marketId, address collateralType)
+        external
+        returns (uint256);
 
     /**
      * @notice Return the total amount of a given collateral type that a specified market has deposited.
+     * @param marketId The id of the market for which the directly deposited collateral amount is being queried.
+     * @param collateralType The address of the collateral for which the amount is being queried.
      */
-    function getMarketCollateralAmount(
-        uint128 marketId,
-        address collateralType
-    ) external returns (uint256);
+    function getMarketCollateralAmount(uint128 marketId, address collateralType)
+        external
+        returns (uint256);
 }
