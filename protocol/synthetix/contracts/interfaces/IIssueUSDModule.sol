a30162dd307827f2705ea2c8844ff00babc503dc
Alejandro <palebluedot@gmail.com>
2022-12-05 14:15:39 -0600

    WIP @params

--- protocol/synthetix/contracts/interfaces/IIssueUSDModule.sol
+++ protocol/synthetix/contracts/interfaces/IIssueUSDModule.sol
@@ -7,28 +7,42 @@ pragma solidity ^0.8.0;
 interface IIssueUSDModule {
     /**
      * @notice Emitted when {sender} mints {amount} of snxUSD with the specified staking position.
+     * @param accountId The id of the account for which USD was emitted.
+     * @param poolId The id of the pool whose collateral was used to emit the USD.
+     * @param collateralType The address of the collateral that is backing up the emitted USD.
+     * @param amount The amount of USD emitted.
+     * @param sender The address that triggered the operation.
      */
     event UsdMinted(
         uint128 indexed accountId,
         uint128 indexed poolId,
         address collateralType,
-        uint amount,
+        uint256 amount,
         address indexed sender
     );
 
     /**
      * @notice Emitted when {sender} burns {amount} of snxUSD with the specified staking position.
+     * @param accountId The id of the account for which USD was burnt.
+     * @param poolId The id of the pool whose collateral was used to emit the USD.
+     * @param collateralType The address of the collateral that was backing up the emitted USD.
+     * @param amount The amount of USD burnt.
+     * @param sender The address that triggered the operation.
      */
     event UsdBurned(
         uint128 indexed accountId,
         uint128 indexed poolId,
         address collateralType,
-        uint amount,
+        uint256 amount,
         address indexed sender
     );
 
     /**
      * @notice Mints {amount} of snxUSD with the specified staking position.
+     * @param accountId The id of the account that is minting USD.
+     * @param poolId The id of the pool whose collateral will be used to back up the mint.
+     * @param collateralType The address of the collateral that will be used to back up the mint.
+     * @param amount The amount of USD to be minted.
      *
      * Requirements:
      *
@@ -41,11 +55,15 @@ interface IIssueUSDModule {
         uint128 accountId,
         uint128 poolId,
         address collateralType,
-        uint amount
+        uint256 amount
     ) external;
 
     /**
      * @notice Burns {amount} of snxUSD with the specified staking position.
+     * @param accountId The id of the account that is burning USD.
+     * @param poolId The id of the pool whose collateral was used to back up the USD.
+     * @param collateralType The address of the collateral that was used to back up the USD.
+     * @param amount The amount of USD to be burnt.
      *
      * Requirements:
      *
@@ -57,6 +75,6 @@ interface IIssueUSDModule {
         uint128 accountId,
         uint128 poolId,
         address collateralType,
-        uint amount
+        uint256 amount
     ) external;
 }
--- protocol/synthetix/contracts/interfaces/ILiquidationModule.sol
+++ protocol/synthetix/contracts/interfaces/ILiquidationModule.sol
@@ -7,6 +7,12 @@ pragma solidity ^0.8.0;
 interface ILiquidationModule {
     /**
      * @notice Emitted when an account is liquidated.
+     * @param accountId The id of the account that was liquidated.
+     * @param poolId The pool id of the position that was liquidated.
+     * @param collateralType The collateral type used in the position that was liquidated.
+     * @param liquidationData Information about the position that was liquidated.
+     * @param liquidateAsAccountID Account id that will receive the rewards from the liquidation.
+     * @param sender The address of the account that is triggering the liquidation.
      */
     event Liquidation(
         uint128 indexed accountId,
@@ -19,6 +25,11 @@ interface ILiquidationModule {
 
     /**
      * @notice Emitted when a vault is liquidated.
+     * @param poolId The id of the pool whose vault was liquidated.
+     * @param collateralType The collateral address of the vault that was liquidated.
+     * @param liquidationData Information about the vault that was liquidated.
+     * @param liquidateAsAccountID Account id that will receive the rewards from the liquidation.
+     * @param sender The address of the account that is triggering the liquidation.
      */
     event VaultLiquidation(
         uint128 indexed poolId,
@@ -32,13 +43,27 @@ interface ILiquidationModule {
      * @notice Data structure that holds liquidation information, used in events and in return statements.
      */
     struct LiquidationData {
+        /**
+         * @dev The debt of the position that was liquidated.
+         */
         uint256 debtLiquidated;
+        /**
+         * @dev The collateral of the position that was liquidated.
+         */
         uint256 collateralLiquidated;
+        /**
+         * @dev The amount rewarded in the liquidation.
+         */
         uint256 amountRewarded;
     }
 
     /**
      * @notice Liquidates a position by distributing its debt and collateral among other positions in its vault.
+     * @param accountId The id of the account whose position is to be liquidated.
+     * @param poolId The id of the pool which holds the position that is to be liquidated.
+     * @param collateralType The address of the collateral being used in the position that is to be liquidated.
+     * @param liquidateAsAccountID Account id that will receive the rewards from the liquidation.
+     * @returns Information about the position that was liquidated.
      */
     function liquidate(
         uint128 accountId,
@@ -52,6 +77,10 @@ interface ILiquidationModule {
      * @dev Can only be done if the vault itself is under collateralized.
      * @dev LiquidateAsAccountId determines which account to deposit the seized collateral into (this is necessary particularly if the collateral in the vault is vesting).
      * @dev Will only liquidate a portion of the debt for the vault if `maxUsd` is supplied.
+     * @param poolId The id of the pool whose vault is being liquidated.
+     * @param collateralType The address of the collateral whose vault is being liquidated.
+     * @param maxUsd The maximum amount of USD that the liquidator is willing to provide for the liquidation.
+     * @returns liquidationData Information about the vault that was liquidated.
      */
     function liquidateVault(
         uint128 poolId,
@@ -62,6 +91,10 @@ interface ILiquidationModule {
 
     /**
      * @notice Determines whether a specified position is liquidatable.
+     * @param accountId The id of the account whose position is being queried for liquidation.
+     * @param poolId The id of the pool whose position is being queried for liquidation.
+     * @param collateralType The address of the collateral backing up the position being queried for liquidation.
+     * @returns A boolean with the response to the query.
      */
     function isPositionLiquidatable(
         uint128 accountId,
@@ -71,6 +104,9 @@ interface ILiquidationModule {
 
     /**
      * @notice Determines whether a specified vault is liquidatable.
+     * @param poolId The id of the pool that owns the vault that is being queried for liquidation.
+     * @param collateralType The address of the collateral being held at the vault that is being queried for liquidation.
+     * @returns A boolean with the response to the query.
      */
     function isVaultLiquidatable(uint128 poolId, address collateralType) external returns (bool);
 }
