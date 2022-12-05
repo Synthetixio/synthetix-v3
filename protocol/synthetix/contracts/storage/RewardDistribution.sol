da40126197e1c340d0f8ff3aff16c9657431a2f6
Alejandro <palebluedot@gmail.com>
2022-12-05 10:34:27 -0600

    Rename DistributionEntry to RewardDistributionEntry

--- protocol/synthetix/contracts/storage/RewardDistribution.sol
+++ protocol/synthetix/contracts/storage/RewardDistribution.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.0;
 
 import "../interfaces/external/IRewardDistributor.sol";
 
-import "./DistributionEntry.sol";
+import "./RewardDistributionEntry.sol";
 import "./RewardDistributionStatus.sol";
 
 library RewardDistribution {
     struct Data {
         // 3rd party smart contract which holds/mints the pools
         IRewardDistributor distributor;
-        DistributionEntry.Data entry;
+        RewardDistributionEntry.Data entry;
         uint128 rewardPerShareD18;
         mapping(uint256 => RewardDistributionStatus.Data) actorInfo;
     }
