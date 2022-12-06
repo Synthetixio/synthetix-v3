418c8c78a96012ef54f5c459e63b34bbc05f3ccc
Alejandro <palebluedot@gmail.com>
2022-12-06 07:47:47 -0300

    Docs for RewardsDistributionEntry

--- protocol/synthetix/contracts/storage/RewardDistributionEntry.sol
+++ protocol/synthetix/contracts/storage/RewardDistributionEntry.sol
@@ -7,6 +7,9 @@ import "@synthetixio/core-contracts/contracts/utils/SafeCast.sol";
 
 import "./Distribution.sol";
 
+/**
+ * @title Data structure that holds a certain amount of value, to be distributed as rewards between a given set of dates.
+ */
 library RewardDistributionEntry {
     using DecimalMath for int256;
     using SafeCastU128 for uint128;
@@ -15,18 +18,32 @@ library RewardDistributionEntry {
     using SafeCastI256 for int256;
 
     struct Data {
-        // amount which should be applied at the given time below, or
+        /**
+         * @dev Value to be distributed as rewards in a scheduled form.
+         */
         int128 scheduledValueD18;
-        // set to <= block.timestamp to distribute immediately to currently staked users
+        /**
+         * @dev Date at which the entry's rewards will begin to be claimable.
+         *
+         * Note: Set to <= block.timestamp to distribute immediately to currently staked users.
+         */
         uint64 start;
+        /**
+         * @dev Time span after the start date, in which the whole of the entry's rewards will become claimable.
+         */
         uint32 duration;
+        /**
+         * @dev Date on which this distribution entry was last updated.
+         */
         uint32 lastUpdate;
     }
 
     /**
-     * this function allows for more special cases such as distributing at a future date or distributing over time.
-     * if you want to apply the distribution to the pool, call `distribute` with the return value. Otherwise, you can
-     * record this independently as well
+     * @dev Distributes rewards into a new rewards distribution entry.
+     *
+     * Note: this function allows for more special cases such as distributing at a future date or distributing over time.
+     * If you want to apply the distribution to the pool, call `distribute` with the return value. Otherwise, you can
+     * record this independently as well.
      */
     function distribute(
         Data storage entry,
@@ -46,27 +63,27 @@ library RewardDistributionEntry {
 
         int256 curTime = block.timestamp.toInt().to128();
 
-        // ensure any previously active distribution has applied
+        // Unlocks the entry's distributed amount into its value per share.
         diffD18 += updateEntry(entry, dist.totalSharesD18);
 
+        // If the current time is past the end of the entry's duration,
+        // update any rewards which may have accrued since last run.
+        // (instant distribution--immediately disperse amount).
         if (start + duration <= curTime.toUint()) {
-            // update any rewards which may have accrued since last run
-
-            // instant distribution--immediately disperse amount
             diffD18 += amountD18.divDecimal(totalSharesD18.toInt());
 
             entry.lastUpdate = 0;
             entry.start = 0;
             entry.duration = 0;
             entry.scheduledValueD18 = 0;
+            // Else, schedule the amount to distribute.
         } else {
-            // set distribution schedule
             entry.scheduledValueD18 = amountD18.to128();
 
             entry.start = start;
             entry.duration = duration;
 
-            // the amount is actually the amount distributed already *plus* whatever has been specified now
+            // The amount is actually the amount distributed already *plus* whatever has been specified now.
             entry.lastUpdate = 0;
 
             diffD18 += updateEntry(entry, dist.totalSharesD18);
@@ -74,14 +91,16 @@ library RewardDistributionEntry {
     }
 
     /**
-     * call every time before `totalShares` changes
+     * @dev Updates the total shares of a reward distribution entry, and releases its unlocked value into its value per share, depending on the time elapsed since the start of the distribution's entry.
+     *
+     * Note: call every time before `totalShares` changes.
      */
-    function updateEntry(
-        Data storage entry,
-        uint256 totalSharesAmountD18
-    ) internal returns (int256) {
+    function updateEntry(Data storage entry, uint256 totalSharesAmountD18)
+        internal
+        returns (int256)
+    {
+        // Cannot process distributed rewards if a pool is empty or if it has no rewards.
         if (entry.scheduledValueD18 == 0 || totalSharesAmountD18 == 0) {
-            // cannot process distributed rewards if a pool is empty.
             return 0;
         }
 
@@ -89,22 +108,30 @@ library RewardDistributionEntry {
 
         int256 valuePerShareChangeD18 = 0;
 
+        // Cannot update an entry whose start date has not being reached.
         if (curTime < int64(entry.start)) {
             return 0;
         }
 
-        // determine whether this is an instant distribution or a delayed distribution
+        // If the entry's duration is zero and the its last update is zero,
+        // consider the entry to be an instant distribution.
         if (entry.duration == 0 && entry.lastUpdate < entry.start) {
+            // Simply update the value per share to the total value divided by the total shares.
             valuePerShareChangeD18 = int256(entry.scheduledValueD18).divDecimal(
                 totalSharesAmountD18.toInt()
             );
+            // Else, if the last update was before the end of the duration.
         } else if (entry.lastUpdate < entry.start + entry.duration) {
-            // find out what is "newly" distributed
+            // Determine how much was previously distributed.
+            // If the last update is zero, then nothing was distributed,
+            // otherwise the amount is proportional to the time elapsed since the start.
             int256 lastUpdateDistributedD18 = entry.lastUpdate < entry.start
                 ? int128(0)
                 : (entry.scheduledValueD18 * int64(entry.lastUpdate - entry.start)) /
                     int32(entry.duration);
 
+            // If the current time is beyond the duration, then consider all scheduled value to be distributed.
+            // Else, the amount distributed is proportional to the elapsed time.
             int256 curUpdateDistributedD18 = entry.scheduledValueD18;
             if (curTime < int64(entry.start + entry.duration)) {
                 curUpdateDistributedD18 =
@@ -112,6 +139,7 @@ library RewardDistributionEntry {
                     int32(entry.duration);
             }
 
+            // The final value per share change is the difference between what is to be distributed and what was distributed.
             valuePerShareChangeD18 = (curUpdateDistributedD18 - lastUpdateDistributedD18)
                 .divDecimal(totalSharesAmountD18.toInt());
         }
