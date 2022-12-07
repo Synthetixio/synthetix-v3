a7de933da75a8c3864f376d9ef39a47d327a6e14
Alejandro <palebluedot@gmail.com>
2022-12-07 15:44:57 -0300

    Removed unused file

--- utils/core-modules/contracts/interfaces/IAssociatedSystemsConsumerModule.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-/// @title Module for managing snxUSD token as a Satellite
-interface IAssociatedSystemsConsumerModule {
-    function getToken(bytes32 id) external view returns (address);
-
-    function getNft(bytes32 id) external view returns (address);
-}
