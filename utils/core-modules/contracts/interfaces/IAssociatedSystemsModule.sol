9eaafa681f688b2dfe691907e9374d575b247700
Alejandro <palebluedot@gmail.com>
2022-12-07 09:55:50 -0300

    Document AssociatedSystemsModule

--- utils/core-modules/contracts/interfaces/IAssociatedSystemsModule.sol
+++ utils/core-modules/contracts/interfaces/IAssociatedSystemsModule.sol
@@ -1,9 +1,42 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/// @title Allows for the registration and tracking of auxillery contracts which also follow the proxy architecture
+/**
+ * @title Module for connecting a system with other associated systems.
+ *
+ * Associated systems can me managed or unmanaged.
+ *
+ * Managed systems are connected via a proxy, which means that their implementation can be updated, and the system controls the execution context of the associated system. Example, an snxUSD token connected to the system, and controlled by the system.
+ *
+ * Unmanaged systems are just addresses tracked by the system, for which it has no control whatsoever. Example, Uniswap v3, Curve, etc.
+ *
+ * Associated systems, managed or unmanaged, become available to all system modules for communication and interaction, but as opposed to inter-modular communications, associated system interactions will require the use of `CALL`.
+ */
 interface IAssociatedSystemsModule {
-    /// @notice create or initialize a new token
+    /**
+     * @notice Emitted when an associated system is set.
+     * @param kind The type of associated system (managed ERC20, managed ERC721, unmanaged, etc - See the AssociatedSystem util).
+     * @param id The bytes32 identifier of the associated system.
+     * @param proxy The main external contract address of the associated system.
+     * @param impl The address of the implementation of the associated system (if not behind a proxy, will equal `proxy`).
+     */
+    event AssociatedSystemSet(
+        bytes32 indexed kind,
+        bytes32 indexed id,
+        address proxy,
+        address impl
+    );
+
+    /**
+     * @notice Creates or initializes a managed associated ERC20 token.
+     * @param id The bytes32 identifier of the associated system.
+     * @param name The token name that will be used to initialize the proxy.
+     * @param symbol The token symbol that will be used to initialize the proxy.
+     * @param decimals The token decimals that will be used to initialize the proxy.
+     * @param impl The first/new ERC20 implementation of the proxy.
+     *
+     * Note: If the id is new, it will create a new proxy and set its first implementation. Otherwise, it will upgrade the implementation of the existing proxy.
+     */
     function initOrUpgradeToken(
         bytes32 id,
         string memory name,
@@ -12,6 +45,16 @@ interface IAssociatedSystemsModule {
         address impl
     ) external;
 
+    /**
+     * @notice Creates or initializes a managed associated ERC721 token.
+     * @param id The bytes32 identifier of the associated system.
+     * @param name The token name that will be used to initialize the proxy.
+     * @param symbol The token symbol that will be used to initialize the proxy.
+     * @param uri The token uri that will be used to initialize the proxy.
+     * @param impl The first/new ERC721 implementation of the proxy.
+     *
+     * Note: If the id is new, it will create a new proxy and set its first implementation. Otherwise, it will upgrade the implementation of the existing proxy.
+     */
     function initOrUpgradeNft(
         bytes32 id,
         string memory name,
@@ -20,7 +63,20 @@ interface IAssociatedSystemsModule {
         address impl
     ) external;
 
+    /**
+     * @notice Registers an unmanaged external contract in the system.
+     * @param id The bytes32 identifier to use to reference the associated system.
+     * @param endpoint The address of the associated system.
+     *
+     * Note: The system will not be able to control or upgrade the associated system, only communicate with it.
+     */
     function registerUnmanagedSystem(bytes32 id, address endpoint) external;
 
-    function getAssociatedSystem(bytes32 id) external view returns (address proxy, bytes32 kind);
+    /**
+     * @notice Retrieves an associated system.
+     * @param id The bytes32 identifier used to reference the associated system.
+     * @return addr The external contract address of the associated system.
+     * @return kind The type of associated system (managed ERC20, managed ERC721, unmanaged, etc - See the AssociatedSystem util).
+     */
+    function getAssociatedSystem(bytes32 id) external view returns (address addr, bytes32 kind);
 }
