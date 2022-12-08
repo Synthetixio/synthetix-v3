5506be04a30c7e6005e08f1dab2e3ed1210d7cb8
Alejandro <palebluedot@gmail.com>
2022-12-08 09:53:14 -0300

    Remove unused files from core-contracts

--- utils/core-contracts/contracts/interfaces/IAuthorizable.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IAuthorizable {
-    function setNewAuthorized(address newNominatedOwner) external;
-
-    function authorized() external view returns (address);
-}
