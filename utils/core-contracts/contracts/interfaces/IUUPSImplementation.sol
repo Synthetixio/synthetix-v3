782435563a216784b3e9b773fdad1afdcdcb47db
Alejandro <palebluedot@gmail.com>
2022-12-08 09:21:52 -0300

    WIP core-contracts docs

--- utils/core-contracts/contracts/interfaces/IUUPSImplementation.sol
+++ utils/core-contracts/contracts/interfaces/IUUPSImplementation.sol
@@ -1,10 +1,37 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/**
+ * @title Contract to be used as the implementation of a Universal Upgradeable Proxy Standard (UUPS) proxy.
+ *
+ * Important: A UUPS proxy requires its upgradeability functions to be in the implementation as opposed to the proxy. This means that if the proxy is upgraded to an implementation that does not support this interface, it will no longer be upgradeable.
+ */
 interface IUUPSImplementation {
+    /**
+     * @notice Emitted when the implementation of the proxy has been upgraded.
+     * @param self The address of the proxy whose implementation was upgraded.
+     * @param implementation The address of the proxy's new implementation.
+     */
+    event Upgraded(address indexed self, address implementation);
+
+    /**
+     * @notice Allows the proxy to be upgraded to a new implementation.
+     * @param newImplementation The address of the proxy's new implementation.
+     * @dev Will revert if `newImplementation` is not upgradeable.
+     * @dev The implementation of this function needs to be protected by some sort of access control such as `onlyOwner`.
+     */
     function upgradeTo(address newImplementation) external;
 
+    /**
+     * @notice Function used to determine if a new implementation will be able to receive future upgrades in `upgradeTo`.
+     * @param newImplementation The address of the new implementation being tested for future upgradeability.
+     * @dev This function will always revert, but will revert with different error messages. The function `upgradeTo` uses this error to determine the future upgradeability of the implementation in question.
+     */
     function simulateUpgradeTo(address newImplementation) external;
 
+    /**
+     * @notice Retrieves the current implementation of the proxy.
+     * @return The address of the current implementation.
+     */
     function getImplementation() external view returns (address);
 }
