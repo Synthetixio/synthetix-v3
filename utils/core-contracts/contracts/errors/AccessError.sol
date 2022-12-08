2a7e550d39f42eb6f690a5f0fb0beb309cedad26
Alejandro <palebluedot@gmail.com>
2022-12-08 08:08:59 -0300

    Document errors

--- utils/core-contracts/contracts/errors/AccessError.sol
+++ utils/core-contracts/contracts/errors/AccessError.sol
@@ -1,6 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/**
+ * @title Library for access related errors.
+ */
 library AccessError {
+    /**
+     * @dev Thrown when an address tries to perform an unauthorized action.
+     * @param addr The address that attempts the action.
+     */
     error Unauthorized(address addr);
 }
--- utils/core-contracts/contracts/errors/ChangeError.sol
+++ utils/core-contracts/contracts/errors/ChangeError.sol
@@ -1,6 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/**
+ * @title Library for change related errors.
+ */
 library ChangeError {
+    /**
+     * @dev Thrown when a change is expected but none is detected.
+     */
     error NoChange();
 }
