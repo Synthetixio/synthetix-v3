d72154b9b696b735c392bf74b366de752870ac69
Alejandro Santander <Palebluedot@gmail.com>
2022-12-09 14:42:11 -0300

    Changes dec 2022 revisited (#1285)
    * Modify dates and improve script
    
    * Fix on dates

--- packages/core-js/utils/misc/dates.js
+++ packages/core-js/utils/misc/dates.js
@@ -8,7 +8,7 @@ function formatDate(date = new Date()) {
     throw new Error('Invalid date given');
   }
 
-  return date.toISOString().slice(0, 10);
+  return date.toISOString();
 }
 
 function getUnixTimestamp(date = new Date()) {
@@ -19,11 +19,16 @@ function fromUnixTimestamp(timestamp) {
   return new Date(timestamp * 1000);
 }
 
+function toUnixTimestamp(date) {
+  return Math.floor(date.getTime() / 1000);
+}
+
 const daysToSeconds = (days) => days * 3600 * 24;
 
 module.exports = {
   formatDate,
   getUnixTimestamp,
   fromUnixTimestamp,
+  toUnixTimestamp,
   daysToSeconds,
 };
