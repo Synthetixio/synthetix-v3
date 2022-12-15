41716c94bc4183e26796f10cf835f764db0dc885
Alejandro Santander <Palebluedot@gmail.com>
2022-12-15 08:56:48 -0300

    Erc165 helper (#1297)
    * WIP ERC165Helper
    
    * Working ERC165Helper
    
    * Add tests

---
+++ utils/core-contracts/contracts/mocks/utils/ERC165HelperMock.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../../interfaces/IERC20.sol";
+import "../../interfaces/IERC721.sol";
+import "../../utils/ERC165Helper.sol";
+
+contract ERC165HelperMock {
+    function supportsInterface(address candidate, bytes4 interfaceId) external returns (bool) {
+        return ERC165Helper.safeSupportsInterface(candidate, interfaceId);
+    }
+
+    function getERC20InterfaceId() external pure returns (bytes4) {
+        return type(IERC20).interfaceId;
+    }
+
+    function getERC721InterfaceId() external pure returns (bytes4) {
+        return type(IERC721).interfaceId;
+    }
+}
