d65c021857c534ec7a54c55ddd169aee6d75574a
Matías <mjlescano@protonmail.com>
2022-12-19 22:48:05 +0800

    migrate CommsMixing tests

--- utils/core-modules/test/contracts/modules/FeatureFlagModule.test.ts
+++ utils/core-modules/test/contracts/modules/FeatureFlagModule.test.ts
@@ -5,7 +5,7 @@ import { ethers } from 'ethers';
 import { FeatureFlagModule, SampleFeatureFlagModule } from '../../../typechain-types';
 import { bootstrap } from '../../bootstrap';
 
-describe('FeatureFlagModule', () => {
+describe('FeatureFlagModule', function () {
   const { getContract, getSigners } = bootstrap({ implementation: 'FeatureFlagModuleRouter' });
 
   let FeatureFlagModule: FeatureFlagModule;
@@ -15,33 +15,33 @@ describe('FeatureFlagModule', () => {
 
   const FEATURE_FLAG_NAME = ethers.utils.formatBytes32String('SAMPLE_FEATURE');
 
-  before('identify signers', () => {
+  before('identify signers', function () {
     [, permissionedUser, user] = getSigners();
   });
 
-  before('identify modules', () => {
+  before('identify modules', function () {
     FeatureFlagModule = getContract('FeatureFlagModule');
     SampleFeatureFlagModule = getContract('SampleFeatureFlagModule');
   });
 
-  describe('when a feature flag is enabled', async () => {
+  describe('when a feature flag is enabled', async function () {
     let addAddressTx: ethers.ContractTransaction;
 
-    before('setup permissioned user for feature flag', async () => {
+    before('setup permissioned user for feature flag', async function () {
       addAddressTx = await FeatureFlagModule.addToFeatureFlagAllowlist(
         FEATURE_FLAG_NAME,
         await permissionedUser.getAddress()
       );
     });
 
-    it('does not allow non-owners to set feature flags', async () => {
+    it('does not allow non-owners to set feature flags', async function () {
       await assertRevert(
         FeatureFlagModule.connect(user).setFeatureFlagAllowAll(FEATURE_FLAG_NAME, true),
         `Unauthorized("${await user.getAddress()}")`
       );
     });
 
-    it('does not allow non-owners to set feature flag addresses', async () => {
+    it('does not allow non-owners to set feature flag addresses', async function () {
       await assertRevert(
         FeatureFlagModule.connect(user).addToFeatureFlagAllowlist(
           FEATURE_FLAG_NAME,
@@ -51,7 +51,7 @@ describe('FeatureFlagModule', () => {
       );
     });
 
-    it('emits event for adding address', async () => {
+    it('emits event for adding address', async function () {
       await assertEvent(
         addAddressTx,
         `FeatureFlagAllowlistAdded("${FEATURE_FLAG_NAME}", "${await permissionedUser.getAddress()}")`,
@@ -60,21 +60,21 @@ describe('FeatureFlagModule', () => {
     });
   });
 
-  describe('call function behind feature flag', async () => {
-    it('reverts when user does not have permission', async () => {
+  describe('call function behind feature flag', async function () {
+    it('reverts when user does not have permission', async function () {
       await assertRevert(
         SampleFeatureFlagModule.connect(user).setFeatureFlaggedValue(15),
         'FeatureUnavailable'
       );
     });
 
-    it('allows permissioned user to call function', async () => {
+    it('allows permissioned user to call function', async function () {
       await SampleFeatureFlagModule.connect(permissionedUser).setFeatureFlaggedValue(15);
       assertBn.equal(await SampleFeatureFlagModule.getFeatureFlaggedValue(), 15);
     });
   });
 
-  it('does not allow non-owners to remove feature flag addresses', async () => {
+  it('does not allow non-owners to remove feature flag addresses', async function () {
     await assertRevert(
       FeatureFlagModule.connect(user).removeFromFeatureFlagAllowlist(
         FEATURE_FLAG_NAME,
@@ -92,17 +92,17 @@ describe('FeatureFlagModule', () => {
     );
   });
 
-  describe('remove permissioned user from feature flag', async () => {
+  describe('remove permissioned user from feature flag', async function () {
     let removeAddressTx: ethers.ContractTransaction;
 
-    before('remove user', async () => {
+    before('remove user', async function () {
       removeAddressTx = await FeatureFlagModule.removeFromFeatureFlagAllowlist(
         FEATURE_FLAG_NAME,
         await permissionedUser.getAddress()
       );
     });
 
-    it('emits event', async () => {
+    it('emits event', async function () {
       await assertEvent(
         removeAddressTx,
         `FeatureFlagAllowlistRemoved("${FEATURE_FLAG_NAME}", "${await permissionedUser.getAddress()}")`,
@@ -110,7 +110,7 @@ describe('FeatureFlagModule', () => {
       );
     });
 
-    it('does not allow removed permissionedUser to set value', async () => {
+    it('does not allow removed permissionedUser to set value', async function () {
       await assertRevert(
         SampleFeatureFlagModule.connect(permissionedUser).setFeatureFlaggedValue(25),
         'FeatureUnavailable'
@@ -118,14 +118,14 @@ describe('FeatureFlagModule', () => {
     });
   });
 
-  describe('enable feature for all', async () => {
+  describe('enable feature for all', async function () {
     let setupTx: ethers.ContractTransaction;
 
-    before('allow all', async () => {
+    before('allow all', async function () {
       setupTx = await FeatureFlagModule.setFeatureFlagAllowAll(FEATURE_FLAG_NAME, true);
     });
 
-    it('emits event', async () => {
+    it('emits event', async function () {
       await assertEvent(
         setupTx,
         `FeatureFlagAllowAllSet("${FEATURE_FLAG_NAME}", true)`,
@@ -133,7 +133,7 @@ describe('FeatureFlagModule', () => {
       );
     });
 
-    it('reverts when feature flag is disabled', async () => {
+    it('reverts when feature flag is disabled', async function () {
       await SampleFeatureFlagModule.connect(user).setFeatureFlaggedValue(25);
       assertBn.equal(await SampleFeatureFlagModule.getFeatureFlaggedValue(), 25);
     });
