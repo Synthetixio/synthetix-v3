7357e7de3f0af3b842cd347fe1ac3644aa79d474
Matías Lescano <mjlescano@users.noreply.github.com>
2022-12-20 18:12:30 +0100

    update documentation (#1314)

--- utils/sample-project/storage.dump.sol
+++ utils/sample-project/storage.dump.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.0;
 
 // @custom:artifact @synthetixio/core-contracts/contracts/ownership/OwnableStorage.sol:OwnableStorage
 library OwnableStorage {
-    bytes32 private constant _slotOwnableStorage = keccak256(abi.encode("io.synthetix.core-contracts.Ownable"));
+    bytes32 private constant _SLOT_OWNABLE_STORAGE = keccak256(abi.encode("io.synthetix.core-contracts.Ownable"));
     struct Data {
         bool initialized;
         address owner;
         address nominatedOwner;
     }
     function load() internal pure returns (Data storage store) {
-        bytes32 s = _slotOwnableStorage;
+        bytes32 s = _SLOT_OWNABLE_STORAGE;
         assembly {
             store.slot := s
         }
@@ -19,13 +19,13 @@ library OwnableStorage {
 
 // @custom:artifact @synthetixio/core-contracts/contracts/proxy/ProxyStorage.sol:ProxyStorage
 contract ProxyStorage {
-    bytes32 private constant _slotProxyStorage = keccak256(abi.encode("io.synthetix.core-contracts.Proxy"));
+    bytes32 private constant _SLOT_PROXY_STORAGE = keccak256(abi.encode("io.synthetix.core-contracts.Proxy"));
     struct ProxyStore {
         address implementation;
         bool simulatingUpgrade;
     }
     function _proxyStore() internal pure returns (ProxyStore storage store) {
-        bytes32 s = _slotProxyStorage;
+        bytes32 s = _SLOT_PROXY_STORAGE;
         assembly {
             store.slot := s
         }
@@ -39,13 +39,13 @@ contract AnotherModule {
 
 // @custom:artifact contracts/storage/GlobalStorage.sol:GlobalStorage
 contract GlobalStorage {
-    bytes32 private constant _slotGlobalStorage = keccak256(abi.encode("io.synthetix.sample-project.Global"));
+    bytes32 private constant _SLOT_GLOBAL_STORAGE = keccak256(abi.encode("io.synthetix.sample-project.Global"));
     struct GlobalStore {
         uint value;
         uint someValue;
     }
     function _globalStore() internal pure returns (GlobalStore storage store) {
-        bytes32 s = _slotGlobalStorage;
+        bytes32 s = _SLOT_GLOBAL_STORAGE;
         assembly {
             store.slot := s
         }
@@ -54,12 +54,12 @@ contract GlobalStorage {
 
 // @custom:artifact contracts/storage/InitializableStorage.sol:InitializableStorage
 contract InitializableStorage {
-    bytes32 private constant _slotInitializableStorage = keccak256(abi.encode("io.synthetix.sample-project.Initializable"));
+    bytes32 private constant _SLOT_INITIALIZABLE_STORAGE = keccak256(abi.encode("io.synthetix.sample-project.Initializable"));
     struct InitializableStore {
         bool initialized;
     }
     function _initializableStore() internal pure returns (InitializableStore storage store) {
-        bytes32 s = _slotInitializableStorage;
+        bytes32 s = _SLOT_INITIALIZABLE_STORAGE;
         assembly {
             store.slot := s
         }
@@ -68,12 +68,12 @@ contract InitializableStorage {
 
 // @custom:artifact contracts/storage/SettingsStorage.sol:SettingsStorage
 contract SettingsStorage {
-    bytes32 private constant _slotSettingsStorage = keccak256(abi.encode("io.synthetix.sample-project.Settings"));
+    bytes32 private constant _SLOT_SETTINGS_STORAGE = keccak256(abi.encode("io.synthetix.sample-project.Settings"));
     struct SettingsStore {
         uint aSettingValue;
     }
     function _settingsStore() internal pure returns (SettingsStore storage store) {
-        bytes32 s = _slotSettingsStorage;
+        bytes32 s = _SLOT_SETTINGS_STORAGE;
         assembly {
             store.slot := s
         }
