e68d5c7bbff8c8f4b2b3366a2993ee4d47c0c5fe
Alejandro <palebluedot@gmail.com>
2023-01-11 14:59:50 -0300

    Remove unsed permission in javascript

--- protocol/synthetix/test/integration/mixins/AccountRBACMixin.permissions.ts
+++ protocol/synthetix/test/integration/mixins/AccountRBACMixin.permissions.ts
@@ -1,7 +1,6 @@
 import { ethers } from 'ethers';
 
 export default {
-  DEPOSIT: ethers.utils.formatBytes32String('DEPOSIT'),
   ADMIN: ethers.utils.formatBytes32String('ADMIN'),
   WITHDRAW: ethers.utils.formatBytes32String('WITHDRAW'),
   MINT: ethers.utils.formatBytes32String('MINT'),
