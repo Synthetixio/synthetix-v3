3f497ad9dd501bda2b804eaf83a810add7d8056d
Alejandro <palebluedot@gmail.com>
2023-01-11 11:06:44 -0300

    Restored needed file

---
+++ utils/core-modules/contracts/storage/SampleStorage.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library SampleStorage {
+    bytes32 private constant _SLOT_SAMPLE_STORAGE =
+        keccak256(abi.encode("io.synthetix.core-modules.Sample"));
+
+    struct Data {
+        uint someValue;
+        uint protectedValue;
+    }
+
+    function load() internal pure returns (Data storage store) {
+        bytes32 s = _SLOT_SAMPLE_STORAGE;
+        assembly {
+            store.slot := s
+        }
+    }
+}
