274e1a8a992085f6356f2963d1576e686bc579a1
Peiman <peymannaderi3@yahoo.com>
2023-01-24 20:57:45 +0330

    fix: handle error in fastForwardTo

--- utils/core-utils/src/utils/hardhat/rpc.ts
+++ utils/core-utils/src/utils/hardhat/rpc.ts
@@ -24,6 +24,10 @@ export async function fastForward(seconds: number, provider: ethers.providers.Js
 }
 
 export async function fastForwardTo(time: number, provider: ethers.providers.JsonRpcProvider) {
+  const timestamp = await getTime(provider);
+  if (timestamp >= time) {
+    return;
+  }
   await provider.send('evm_setNextBlockTimestamp', [time]);
 
   await mineBlock(provider);
