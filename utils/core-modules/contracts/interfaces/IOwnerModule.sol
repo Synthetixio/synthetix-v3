a75d928bcf5a3dcb7190f487bb76179e2dc279ce
Daniel Beal <git@danb.email>
2023-02-07 23:22:55 -0800

    lint fix

--- utils/core-modules/contracts/interfaces/IOwnerModule.sol
+++ utils/core-modules/contracts/interfaces/IOwnerModule.sol
@@ -5,4 +5,6 @@ pragma solidity >=0.8.11 <0.9.0;
  * @title Module for giving a system owner based access control.
  */
 // solhint-disable-next-line no-empty-blocks
-interface IOwnerModule {}
+interface IOwnerModule {
+
+}
