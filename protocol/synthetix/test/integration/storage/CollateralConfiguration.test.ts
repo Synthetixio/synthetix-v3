da79b4624bf48197d19a3859656cf6114d8e3a4e
JaredBorders <jlbxsxs@gmail.com>
2023-02-10 16:42:48 -0500

    ✅ Update tests

--- protocol/synthetix/test/integration/storage/CollateralConfiguration.test.ts
+++ protocol/synthetix/test/integration/storage/CollateralConfiguration.test.ts
@@ -1,26 +1,66 @@
-// import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber';
-// import { ethers } from 'ethers';
-// import { snapshotCheckpoint } from '../../utils/snapshot';
-// import { bootstrapWithMockMarketAndPool } from '../bootstrap';
+import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
+import { ethers } from 'ethers';
+import hre from 'hardhat';
+import { bootstrap } from '../bootstrap';
+import { CollateralMock } from '../../../typechain-types/contracts/mocks/CollateralMock';
 
 describe('CollateralConfiguration', function () {
-  // const {
-  //   signers,
-  // } = bootstrapWithMockMarketAndPool();
+  const { systems, signers } = bootstrap();
 
-  // let owner: ethers.Signer;
-  // let user1: ethers.Signer;
+  let owner: ethers.Signer;
 
-  // before('init', async () => {
-  //   [owner, user1] = signers();
-  // });
+  let fakeCollateral: CollateralMock;
+
+  const initFakeCollateralConfig = async (decimals: number) => {
+    [owner] = signers();
+    fakeCollateral = await (
+      await hre.ethers.getContractFactory('MockMarket')
+    ).deploy('token', 'TOKEN', decimals);
+    await fakeCollateral.deployed();
+    await (
+      await systems()
+        .Core.connect(owner)
+        .configureCollateral({
+          tokenAddress: fakeCollateral.address,
+          oracleNodeId: ethers.utils.formatBytes32String(''),
+          issuanceRatioD18: '5000000000000000000',
+          liquidationRatioD18: '1500000000000000000',
+          liquidationRewardD18: '20000000000000000000',
+          minDelegationD18: '20000000000000000000',
+          depositingEnabled: true,
+        })
+    ).wait();
+  };
+
+  before('initialize fake collateral config', async () => {
+    await initFakeCollateralConfig(0);
+  });
 
   describe('convertTokenToSystemAmount()', async () => {
-    it('scales token with 0 decimals to system amount', async () => {});
+    it('scales token with 0 decimals to system amount', async () => {
+      await systems().Core.CollateralConfiguration_convertTokenToSystemAmount(
+        fakeCollateral.address,
+        ethers.utils.parseEther('1')
+      );
+    });
+    
     it('scales token with 6 decimals system amount', async () => {});
+
     it('scales token with 18 decimals to system amount', async () => {});
+
     it('scales token with greater than 18 decimals to system amount', async () => {});
+
     it('scales token that does not define decimals to system amount', async () => {});
-    it('reverts when scaling to system amount results in loss of precision', async () => {});
+
+    it('reverts when scaling to system amount results in loss of precision', async () => {
+      await assertRevert(
+        systems().Core.CollateralConfiguration_convertTokenToSystemAmount(
+          ethers.constants.AddressZero,
+          1
+        ),
+        'PrecisionLost()',
+        systems().Core
+      );
+    });
   });
 });
