2fc3d4c7ee168ddde34ee2f0c795d171796e8181
JaredBorders <jlbxsxs@gmail.com>
2023-02-10 15:13:30 -0500

    ✅ Add test descriptions

---
+++ protocol/synthetix/test/integration/storage/CollateralConfiguration.test.ts
@@ -0,0 +1,40 @@
+import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber';
+import { ethers } from 'ethers';
+import { snapshotCheckpoint } from '../../utils/snapshot';
+import { bootstrapWithMockMarketAndPool } from '../bootstrap';
+
+describe('CollateralConfiguration', function () {
+  const {
+    systems,
+    provider,
+    signers,
+    marketId,
+    poolId,
+    accountId,
+    MockMarket,
+    depositAmount,
+    collateralAddress,
+  } = bootstrapWithMockMarketAndPool();
+
+  let owner: ethers.Signer;
+  let user1: ethers.Signer;
+
+  before('init', async () => {
+    [owner, user1] = signers();
+  });
+
+  describe('convertTokenToSystemAmount()', async () => {
+    it('scales token with 0 decimals to system amount', async () => {
+    });
+    it('scales token with 6 decimals system amount', async () => {
+    });
+    it('scales token with 18 decimals to system amount', async () => {
+    });
+    it('scales token with greater than 18 decimals to system amount', async () => {
+    });
+    it('scales token that does not define decimals to system amount', async () => {
+    });
+    it('reverts when scaling to system amount results in loss of precision', async () => {
+    });
+  });
+});
