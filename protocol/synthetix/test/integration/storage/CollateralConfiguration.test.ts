fbf15484cb28b56a8d01d82092fa1c85d7a3f1f0
JaredBorders <jlbxsxs@gmail.com>
2023-02-10 19:24:38 -0500

    ✅ Test convertTokenToSystemAmount()

--- protocol/synthetix/test/integration/storage/CollateralConfiguration.test.ts
+++ protocol/synthetix/test/integration/storage/CollateralConfiguration.test.ts
@@ -1,22 +1,25 @@
 import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
+import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber';
+import { bootstrap } from '../bootstrap';
 import { ethers } from 'ethers';
 import hre from 'hardhat';
-import { bootstrap } from '../bootstrap';
-import { CollateralMock } from '../../../typechain-types/contracts/mocks/CollateralMock';
 
 describe('CollateralConfiguration', function () {
   const { systems, signers } = bootstrap();
 
   let owner: ethers.Signer;
 
-  let fakeCollateral: CollateralMock;
+  let fakeCollateral: ethers.Contract;
+
+  const ONE = ethers.utils.parseEther('1');
 
   const initFakeCollateralConfig = async (decimals: number) => {
     [owner] = signers();
-    fakeCollateral = await (
-      await hre.ethers.getContractFactory('MockMarket')
-    ).deploy('token', 'TOKEN', decimals);
+
+    const CollateralMock = await hre.ethers.getContractFactory('CollateralMock');
+    fakeCollateral = await CollateralMock.deploy();
     await fakeCollateral.deployed();
+    await fakeCollateral.initialize('token', 'TOKEN', decimals);
     await (
       await systems()
         .Core.connect(owner)
@@ -32,31 +35,129 @@ describe('CollateralConfiguration', function () {
     ).wait();
   };
 
-  before('initialize fake collateral config', async () => {
-    await initFakeCollateralConfig(0);
-  });
-
   describe('convertTokenToSystemAmount()', async () => {
     it('scales token with 0 decimals to system amount', async () => {
-      await systems().Core.CollateralConfiguration_convertTokenToSystemAmount(
+      const DECIMALS = 0;
+      await initFakeCollateralConfig(DECIMALS);
+      const amountD18 = await systems().Core.CollateralConfiguration_convertTokenToSystemAmount(
         fakeCollateral.address,
-        ethers.utils.parseEther('1')
+        ONE
       );
+
+      let expectedAmountD18 = ONE.mul(ethers.BigNumber.from(10).pow(18)).div(
+        ethers.BigNumber.from(10).pow(DECIMALS)
+      );
+
+      // expect 1 * 10^36
+      assertBn.equal(amountD18, expectedAmountD18);
     });
-    
-    it('scales token with 6 decimals system amount', async () => {});
 
-    it('scales token with 18 decimals to system amount', async () => {});
+    it('scales token with 6 decimals system amount', async () => {
+      const DECIMALS = 6;
+      await initFakeCollateralConfig(DECIMALS);
+      const amountD18 = await systems().Core.CollateralConfiguration_convertTokenToSystemAmount(
+        fakeCollateral.address,
+        ONE
+      );
 
-    it('scales token with greater than 18 decimals to system amount', async () => {});
+      let expectedAmountD18 = ONE.mul(ethers.BigNumber.from(10).pow(18)).div(
+        ethers.BigNumber.from(10).pow(DECIMALS)
+      );
 
-    it('scales token that does not define decimals to system amount', async () => {});
+      // expect 1 * 10^30
+      assertBn.equal(amountD18, expectedAmountD18);
+    });
 
-    it('reverts when scaling to system amount results in loss of precision', async () => {
+    it('scales token with 18 decimals to system amount', async () => {
+      const DECIMALS = 18;
+      await initFakeCollateralConfig(DECIMALS);
+      const amountD18 = await systems().Core.CollateralConfiguration_convertTokenToSystemAmount(
+        fakeCollateral.address,
+        ONE
+      );
+
+      let expectedAmountD18 = ONE.mul(ethers.BigNumber.from(10).pow(18)).div(
+        ethers.BigNumber.from(10).pow(DECIMALS)
+      );
+
+      // expect 1 * 10^18
+      assertBn.equal(amountD18, expectedAmountD18);
+    });
+
+    it('scales token with greater than 18 decimals to system amount', async () => {
+      const ONE_HUNDRED = ONE.mul(10).mul(10);
+      const DECIMALS = 20;
+      await initFakeCollateralConfig(DECIMALS);
+      const amountD18 = await systems().Core.CollateralConfiguration_convertTokenToSystemAmount(
+        fakeCollateral.address,
+        ONE_HUNDRED // with 20 decimals, this is 1.0, or in the context of the system 1 * 10^18
+      );
+
+      let expectedAmountD18 = ONE_HUNDRED.mul(ethers.BigNumber.from(10).pow(18)).div(
+        ethers.BigNumber.from(10).pow(DECIMALS)
+      );
+
+      // expect 1 * 10^18
+      assertBn.equal(amountD18, expectedAmountD18);
+    });
+
+    it('scales token that does not define decimals to system amount', async () => {
+      const CollateralMockWithoutDecimals = await hre.ethers.getContractFactory(
+        'CollateralMockWithoutDecimals'
+      );
+      fakeCollateral = await CollateralMockWithoutDecimals.deploy();
+      await fakeCollateral.deployed();
+      await fakeCollateral.initialize('token', 'TOKEN');
+      await (
+        await systems()
+          .Core.connect(owner)
+          .configureCollateral({
+            tokenAddress: fakeCollateral.address,
+            oracleNodeId: ethers.utils.formatBytes32String(''),
+            issuanceRatioD18: '5000000000000000000',
+            liquidationRatioD18: '1500000000000000000',
+            liquidationRewardD18: '20000000000000000000',
+            minDelegationD18: '20000000000000000000',
+            depositingEnabled: true,
+          })
+      ).wait();
+
+      const DECIMALS = 18; // assumed decimals when ERC20 token does not define decimals()
+      await initFakeCollateralConfig(DECIMALS);
+      const amountD18 = await systems().Core.CollateralConfiguration_convertTokenToSystemAmount(
+        fakeCollateral.address,
+        ONE
+      );
+
+      let expectedAmountD18 = ONE.mul(ethers.BigNumber.from(10).pow(18)).div(
+        ethers.BigNumber.from(10).pow(DECIMALS)
+      );
+
+      // expect 1 * 10^18
+      assertBn.equal(amountD18, expectedAmountD18);
+    });
+
+    it('reverts when scaling down to system amount results in loss of precision', async () => {
+      const ONE_HUNDRED = ONE.mul(10).mul(10);
+      const DECIMALS = 20;
+      await initFakeCollateralConfig(DECIMALS);
       await assertRevert(
         systems().Core.CollateralConfiguration_convertTokenToSystemAmount(
-          ethers.constants.AddressZero,
-          1
+          fakeCollateral.address,
+          ONE_HUNDRED.add(1) // with 20 decimals, this is 1.00000000000000000001, or in the context of the system 1.00000000000000000001 * 10^18
+        ),
+        'PrecisionLost()',
+        systems().Core
+      );
+    });
+
+    it('reverts when scaling down to system amount results in too small of an amount', async () => {
+      const DECIMALS = 20;
+      await initFakeCollateralConfig(DECIMALS);
+      await assertRevert(
+        systems().Core.CollateralConfiguration_convertTokenToSystemAmount(
+          fakeCollateral.address,
+          99 // 100 would not result in a revert, but [99, 98, 97, ... 2, 1] would
         ),
         'PrecisionLost()',
         systems().Core
