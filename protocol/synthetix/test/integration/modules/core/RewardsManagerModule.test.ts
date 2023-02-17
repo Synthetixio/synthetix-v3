db4a6fae8695be05edd64df03967b18bc0efb79e
dbeal <git@danb.email>
2023-02-17 12:08:50 -0800

    fix reward per share and add tests (#1464)
    should be incrementing reward per share, otherwise accumulated rewards
    over time may not be accounted

--- protocol/synthetix/test/integration/modules/core/RewardsManagerModule.test.ts
+++ protocol/synthetix/test/integration/modules/core/RewardsManagerModule.test.ts
@@ -2,7 +2,7 @@ import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber'
 import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
 import hre from 'hardhat';
 import { ethers } from 'ethers';
-import { fastForwardTo, getTime } from '@synthetixio/core-utils/utils/hardhat/rpc';
+import { fastForward, fastForwardTo, getTime } from '@synthetixio/core-utils/utils/hardhat/rpc';
 
 import Permissions from '../../mixins/AccountRBACMixin.permissions';
 import { bootstrapWithStakedPool } from '../../bootstrap';
@@ -621,6 +621,17 @@ describe('RewardsManagerModule', function () {
     });
 
     describe('successful invoke', async () => {
+      before('distribute some reward', async () => {
+        const time = await getTime(provider());
+        await RewardDistributor.connect(owner).distributeRewards(
+          poolId,
+          collateralAddress(),
+          rewardAmount,
+          time,
+          time + 100
+        );
+      });
+
       before('remove', async () => {
         await systems()
           .Core.connect(owner)
@@ -642,9 +653,24 @@ describe('RewardsManagerModule', function () {
       });
 
       it('can still claim accumulated rewards', async () => {
+        const beforeBalance = await Collateral.balanceOf(await user1.getAddress());
+
         await systems()
           .Core.connect(user1)
           .claimRewards(accountId, poolId, collateralAddress(), RewardDistributor.address);
+
+        // make sure some rewards were actually distributed from the over time distribution
+        const afterBalance = await Collateral.balanceOf(await user1.getAddress());
+        assertBn.gt(afterBalance, beforeBalance);
+
+        await fastForward(1000, provider());
+
+        await systems()
+          .Core.connect(user1)
+          .claimRewards(accountId, poolId, collateralAddress(), RewardDistributor.address);
+
+        // after first claim there should be no more additional rewards to claim
+        assertBn.equal(await Collateral.balanceOf(await user1.getAddress()), afterBalance);
       });
 
       it('cannot be re-registered', async () => {
