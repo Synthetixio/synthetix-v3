24581c941616b1f894ce7648b0bf127a7f36730c
Daniel Beal <git@danb.email>
2023-02-21 21:07:32 -0800

    add comments to proxies
    verification will show this helpful explanation to sleuths

--- protocol/synthetix/contracts/Proxy.sol
+++ protocol/synthetix/contracts/Proxy.sol
@@ -3,6 +3,13 @@ pragma solidity >=0.8.11 <0.9.0;
 
 import {UUPSProxyWithOwner} from "@synthetixio/core-contracts/contracts/proxy/UUPSProxyWithOwner.sol";
 
+/**
+ * Synthetix V3 Official Proxy Contract
+ * 
+ * If you are wondering how to interact with this contract or what functions are available, you can
+ * view and download the full, up-to-date ABI implementation, list of implementation contracts, 
+ * and a UI for reading/writing the proxy at https://usecannon.com/packages/synthetix/interact
+ */
 contract Proxy is UUPSProxyWithOwner {
     // solhint-disable-next-line no-empty-blocks
     constructor(
