9090c4cef23c5105a5ece9c498412a6e83ffba1a
joey <joey@cc.snxdao.io>
2023-03-09 10:27:55 +1100

    Add totalAccountOpenInterest to account module

--- markets/perps-market/contracts/interfaces/IAccountModule.sol
+++ markets/perps-market/contracts/interfaces/IAccountModule.sol
@@ -5,7 +5,8 @@ pragma solidity >=0.8.11 <0.9.0;
  * @title Account module
  */
 interface IAccountModule {
-    // function collaterals( uint128 accountId) external view returns (Collateral[] memory); 
+
     function totalCollateralValue (uint128 accountId) external view returns (uint);
+    function totalAccountOpenInterest (uint128 accountId) external view returns (int);
 
 }
--- markets/perps-market/contracts/modules/AccountModule.sol
+++ markets/perps-market/contracts/modules/AccountModule.sol
@@ -8,8 +8,13 @@ import "../storage/PerpsAccount.sol";
 contract AccountModule is IAccountModule {
     using PerpsAccount for PerpsAccount.Data;
     
-    function totalCollateralValue (uint128 accountId) external view returns (uint){
+    function totalCollateralValue (uint128 accountId) override external view returns (uint){
         PerpsAccount.Data storage perpsAccount =  PerpsAccount.load(accountId);
         return perpsAccount.getTotalCollateralValue();
+    }
+
+    function totalAccountOpenInterest (uint128 accountId) override external view returns (int){
+        PerpsAccount.Data storage perpsAccount =  PerpsAccount.load(accountId);
+        return perpsAccount.getTotalAccountOpenInterest(accountId);
     }
 }
