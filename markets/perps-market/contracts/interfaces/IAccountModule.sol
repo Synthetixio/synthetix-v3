c1080f9b00bc7c1bc7f2ed725a3ffe5ca3ad46bd
joey <joey@cc.snxdao.io>
2023-03-10 11:28:25 +1100

    Prettier

--- markets/perps-market/contracts/interfaces/IAccountModule.sol
+++ markets/perps-market/contracts/interfaces/IAccountModule.sol
@@ -5,8 +5,7 @@ pragma solidity >=0.8.11 <0.9.0;
  * @title Account module
  */
 interface IAccountModule {
+    function totalCollateralValue(uint128 accountId) external view returns (uint);
 
-    function totalCollateralValue (uint128 accountId) external view returns (uint);
-    function totalAccountOpenInterest (uint128 accountId) external view returns (int);
-
+    function totalAccountOpenInterest(uint128 accountId) external view returns (int);
 }
--- markets/perps-market/contracts/modules/AccountModule.sol
+++ markets/perps-market/contracts/modules/AccountModule.sol
@@ -4,17 +4,16 @@ pragma solidity >=0.8.11 <0.9.0;
 import "../interfaces/IAccountModule.sol";
 import "../storage/PerpsAccount.sol";
 
-
 contract AccountModule is IAccountModule {
     using PerpsAccount for PerpsAccount.Data;
-    
-    function totalCollateralValue (uint128 accountId) override external view returns (uint){
-        PerpsAccount.Data storage perpsAccount =  PerpsAccount.load(accountId);
+
+    function totalCollateralValue(uint128 accountId) external view override returns (uint) {
+        PerpsAccount.Data storage perpsAccount = PerpsAccount.load(accountId);
         return perpsAccount.getTotalCollateralValue();
     }
 
-    function totalAccountOpenInterest (uint128 accountId) override external view returns (int){
-        PerpsAccount.Data storage perpsAccount =  PerpsAccount.load(accountId);
+    function totalAccountOpenInterest(uint128 accountId) external view override returns (int) {
+        PerpsAccount.Data storage perpsAccount = PerpsAccount.load(accountId);
         return perpsAccount.getTotalAccountOpenInterest(accountId);
     }
 }
