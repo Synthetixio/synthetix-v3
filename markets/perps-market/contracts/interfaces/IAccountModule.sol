76d57c5d69601d078f8598c946427b77a193e06b
Joey <5688912+bachstatter@users.noreply.github.com>
2023-03-13 11:58:56 +1100

    Add openPosition (#1497)

--- markets/perps-market/contracts/interfaces/IAccountModule.sol
+++ markets/perps-market/contracts/interfaces/IAccountModule.sol
@@ -8,4 +8,9 @@ interface IAccountModule {
     function totalCollateralValue(uint128 accountId) external view returns (uint);
 
     function totalAccountOpenInterest(uint128 accountId) external view returns (int);
+
+    function openPosition(
+        uint128 accountId,
+        uint128 marketId
+    ) external view returns (int, int, int);
 }
