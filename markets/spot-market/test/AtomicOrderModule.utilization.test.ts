8462ad7edc65a94a38067368bd9b257838760c95
Sunny Vempati <sunnyvempati@gmail.com>
2023-03-21 23:43:18 -0500

    remove unused vars

--- markets/spot-market/test/AtomicOrderModule.utilization.test.ts
+++ markets/spot-market/test/AtomicOrderModule.utilization.test.ts
@@ -1,10 +1,8 @@
 import { ethers as Ethers } from 'ethers';
 import { bn, bootstrapTraders, bootstrapWithSynth } from './bootstrap';
-import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
 import { SynthRouter } from '../generated/typechain';
 import { snapshotCheckpoint } from '@synthetixio/main/test/utils/snapshot';
 import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber';
-import assertEvent from '@synthetixio/core-utils/utils/assertions/assert-event';
 
 describe('Atomic Orders Utilization Rate tests', () => {
   const { systems, signers, marketId, provider } = bootstrapTraders(
