a9279ad23baa17e39bb1b33337658e10d86157f5
Sunny Vempati <sunnyvempati@gmail.com>
2023-04-02 19:10:40 -0500

    storage dump

--- markets/spot-market/storage.dump.sol
+++ markets/spot-market/storage.dump.sol
@@ -345,8 +345,3 @@ library Transaction {
         UNWRAP
     }
 }
-
-// @custom:artifact hardhat/console.sol:console
-library console {
-    address internal constant CONSOLE_ADDRESS = address(0x000000000000000000636F6e736F6c652e6c6f67);
-}
