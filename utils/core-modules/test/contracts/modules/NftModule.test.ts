0699e7c18a3ddb99ebc9ffd7cb89ae4d78e7b29b
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2023-04-07 13:29:17 -0400

    n-5
    Co-authored-by: dbeal <git@danb.email>

--- utils/core-modules/test/contracts/modules/NftModule.test.ts
+++ utils/core-modules/test/contracts/modules/NftModule.test.ts
@@ -57,4 +57,21 @@ describe('NftModule', function () {
       await assertRevert(NftModule.connect(user).burn(12341234), 'Unauthorized(', NftModule);
     });
   });
+
+  describe('setBaseTokenURI()', () => {
+    it('successfully persists', async () => {
+      const newBaseUri = 'ipfs://mynewbaseuri';
+      await NftModule.connect(owner).setBaseTokenURI(newBaseUri);
+      await NftModule.connect(owner).mint(await user.getAddress(), 1);
+      assert.equal(await NftModule.tokenURI(1), newBaseUri + '1');
+    });
+
+    it('only allows the owner to call', async () => {
+      await assertRevert(
+        NftModule.connect(user).setBaseTokenURI('ipfs://icantsetit'),
+        'Unauthorized(',
+        NftModule
+      );
+    });
+  });
 });
