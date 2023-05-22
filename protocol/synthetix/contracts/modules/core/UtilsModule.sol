3f34657e4e6f71cd1896be39e6583172d6445194
Noisekit <nikita@cc.snxdao.io>
2023-05-22 11:20:24 +0800

    Getting config values needs to be allowed to any user

--- protocol/synthetix/contracts/modules/core/UtilsModule.sol
+++ protocol/synthetix/contracts/modules/core/UtilsModule.sol
@@ -57,7 +57,6 @@ contract UtilsModule is IUtilsModule {
     }
 
     function getConfig(bytes32 k) external view override returns (bytes32 v) {
-        OwnableStorage.onlyOwner();
         return Config.read(k, 0);
     }
 }
