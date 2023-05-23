85494ac0d5867c6cfd1788d6eef24e96d0c57869
Daniel Beal <git@dbeal.dev>
2023-05-23 11:51:01 -0700

    lint fix

--- protocol/synthetix/test/integration/modules/core/MulticallModule.test.ts
+++ protocol/synthetix/test/integration/modules/core/MulticallModule.test.ts
@@ -6,10 +6,10 @@ import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert
 describe('MulticallModule', function () {
   const { systems, signers } = bootstrap();
 
-  let owner: ethers.Signer, user1: ethers.Signer;
+  let user1: ethers.Signer;
 
   before('identify signers', async () => {
-    [owner, user1] = signers();
+    [, user1] = signers();
   });
 
   describe('multicall()', () => {
@@ -17,11 +17,11 @@ describe('MulticallModule', function () {
       await assertRevert(
         systems()
           .Core.connect(user1)
-          .multicall(
-            [
-              systems().Core.interface.encodeFunctionData('createAccount(uint128)', ['170141183460469231731687303715884105727'])
-            ]
-          ),
+          .multicall([
+            systems().Core.interface.encodeFunctionData('createAccount(uint128)', [
+              '170141183460469231731687303715884105727',
+            ]),
+          ]),
         `InvalidAccountId("170141183460469231731687303715884105727")`,
         systems().Core
       );
@@ -31,14 +31,12 @@ describe('MulticallModule', function () {
       before('call', async () => {
         await systems()
           .Core.connect(user1)
-          .multicall(
-            [
-              systems().Core.interface.encodeFunctionData('createAccount(uint128)', [1234421]),
-              systems().Core.interface.encodeFunctionData('createAccount(uint128)', [1234422]),
-              systems().Core.interface.encodeFunctionData('createAccount(uint128)', [1234423]),
-              systems().Core.interface.encodeFunctionData('createAccount(uint128)', [1234424])
-            ]
-          );
+          .multicall([
+            systems().Core.interface.encodeFunctionData('createAccount(uint128)', [1234421]),
+            systems().Core.interface.encodeFunctionData('createAccount(uint128)', [1234422]),
+            systems().Core.interface.encodeFunctionData('createAccount(uint128)', [1234423]),
+            systems().Core.interface.encodeFunctionData('createAccount(uint128)', [1234424]),
+          ]);
       });
 
       it('creates all the accounts', async () => {
@@ -58,7 +56,9 @@ describe('MulticallModule', function () {
           .multicallThrough(
             [systems().Core.address],
             [
-              systems().Core.interface.encodeFunctionData('createAccount(uint128)', ['170141183460469231731687303715884105727'])
+              systems().Core.interface.encodeFunctionData('createAccount(uint128)', [
+                '170141183460469231731687303715884105727',
+              ]),
             ]
           ),
         `InvalidAccountId("170141183460469231731687303715884105727")`,
@@ -74,14 +74,21 @@ describe('MulticallModule', function () {
             [systems().Core.address, systems().OracleManager.address],
             [
               systems().Core.interface.encodeFunctionData('createAccount(uint128)', [1234434]),
-              systems().OracleManager.interface.encodeFunctionData('registerNode', [8, ethers.utils.defaultAbiCoder.encode(['uint256'], [429]), []])
+              systems().OracleManager.interface.encodeFunctionData('registerNode', [
+                8,
+                ethers.utils.defaultAbiCoder.encode(['uint256'], [429]),
+                [],
+              ]),
             ]
           );
       });
 
       it('calls through on both contracts', async () => {
-
-        const nodeId = systems().OracleManager.getNodeId(8, ethers.utils.defaultAbiCoder.encode(['uint256'], [429]), []);
+        const nodeId = systems().OracleManager.getNodeId(
+          8,
+          ethers.utils.defaultAbiCoder.encode(['uint256'], [429]),
+          []
+        );
 
         assert.equal(await systems().Account.ownerOf(1234434), await user1.getAddress());
         assert.equal((await systems().OracleManager.getNode(nodeId)).nodeType, 8);
