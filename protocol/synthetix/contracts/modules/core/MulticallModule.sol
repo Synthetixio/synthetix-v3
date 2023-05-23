93c2e12852d8f88e805d3533e92afe9b0c112815
Daniel Beal <git@dbeal.dev>
2023-05-23 11:43:28 -0700

    fixes and features for multicall module
    * pass through error on failure (needs assembly)
    * add `multicallThrough` which adds the capability for multicall to work with ecosystem contracts, so long as they are willing to use provided `getMessageSender`

--- protocol/synthetix/contracts/modules/core/MulticallModule.sol
+++ protocol/synthetix/contracts/modules/core/MulticallModule.sol
@@ -3,12 +3,20 @@ pragma solidity >=0.8.11 <0.9.0;
 
 import "../../interfaces/IMulticallModule.sol";
 
+import "../../storage/Config.sol";
+
+import "hardhat/console.sol";
+
 /**
  * @title Module that enables calling multiple methods of the system in a single transaction.
  * @dev See IMulticallModule.
  * @dev Implementation adapted from https://github.com/Uniswap/v3-periphery/blob/main/contracts/base/Multicall.sol
  */
 contract MulticallModule is IMulticallModule {
+    bytes32 internal constant _CONFIG_MESSAGE_SENDER = "_messageSender";
+
+    error RecursiveMulticall(address);
+
     /**
      * @inheritdoc IMulticallModule
      */
@@ -20,16 +28,51 @@ contract MulticallModule is IMulticallModule {
             (bool success, bytes memory result) = address(this).delegatecall(data[i]);
 
             if (!success) {
-                // Next 6 lines from https://ethereum.stackexchange.com/a/83577
-                // solhint-disable-next-line reason-string
-                if (result.length < 68) revert();
+                uint len = result.length;
+                console.log("THELEN", len);
                 assembly {
-                    result := add(result, 0x04)
+                    revert(add(result, 0x20), len)
                 }
-                revert(abi.decode(result, (string)));
             }
 
             results[i] = result;
         }
+    }
+
+    function multicallThrough(
+        address[] calldata to,
+        bytes[] calldata data
+    ) public payable override returns (bytes[] memory results) {
+        if (Config.read(_CONFIG_MESSAGE_SENDER, 0) != 0) {
+            revert RecursiveMulticall(msg.sender);
+        }
+
+        Config.put(_CONFIG_MESSAGE_SENDER, bytes32(uint256(uint160(msg.sender))));
+
+        results = new bytes[](data.length);
+        for (uint256 i = 0; i < data.length; i++) {
+            bool success;
+            bytes memory result;
+            if (to[i] == address(this)) {
+                (success, result) = address(this).delegatecall(data[i]);
+            } else {
+                (success, result) = address(to[i]).call(data[i]);
+            }
+
+            if (!success) {
+                uint len = result.length;
+                assembly {
+                    revert(add(result, 0x20), len)
+                }
+            }
+
+            results[i] = result;
+        }
+
+        Config.put(_CONFIG_MESSAGE_SENDER, 0);
+    }
+
+    function getMessageSender() external view override returns (address) {
+        return Config.readAddress(_CONFIG_MESSAGE_SENDER, address(0));
     }
 }
