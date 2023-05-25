8c3f3e5fa421d536841a7acda7daa1ae949123af
meb <4982406+barrasso@users.noreply.github.com>
2023-05-25 18:35:28 -0400

    Add access check

--- protocol/synthetix/contracts/modules/usd/USDTokenModule.sol
+++ protocol/synthetix/contracts/modules/usd/USDTokenModule.sol
@@ -81,7 +81,14 @@ contract USDTokenModule is ERC20, InitializableMixin, IUSDTokenModule {
      * @inheritdoc IUSDTokenModule
      */
     function burn(uint256 amount) external {
-        burn(msg.sender, amount);
+        if (
+            msg.sender != OwnableStorage.getOwner() &&
+            msg.sender != AssociatedSystem.load(_CCIP_CHAINLINK_TOKEN_POOL).proxy
+        ) {
+            revert AccessError.Unauthorized(msg.sender);
+        }
+
+        _burn(msg.sender, amount);
     }
 
     /**
