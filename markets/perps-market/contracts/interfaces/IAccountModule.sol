fef47109d37c87b2d7893afc8187a4d626316593
meb <4982406+barrasso@users.noreply.github.com>
2023-05-25 10:56:20 -0400

    Add modifyCollateral tests to perps-market (#1619)

--- markets/perps-market/contracts/interfaces/IAccountModule.sol
+++ markets/perps-market/contracts/interfaces/IAccountModule.sol
@@ -5,6 +5,15 @@ pragma solidity >=0.8.11 <0.9.0;
  * @title Account module
  */
 interface IAccountModule {
+    error InvalidAmountDelta(int amountDelta);
+
+    event CollateralModified(
+        uint128 indexed accountId,
+        uint128 indexed synthMarketId,
+        int indexed amountDelta,
+        address sender
+    );
+
     function modifyCollateral(uint128 accountId, uint128 synthMarketId, int amountDelta) external;
 
     function totalCollateralValue(uint128 accountId) external view returns (uint);
