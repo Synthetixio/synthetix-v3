29d8136358846e14e31b416d65891c84c8248b57
Leonardo Massazza <lmassazza@gmail.com>
2023-06-01 17:13:22 -0300

    test parsePriceFeed in Mock Pyth

--- protocol/oracle-manager/test/integration/nodes/PythNode.test.ts
+++ protocol/oracle-manager/test/integration/nodes/PythNode.test.ts
@@ -19,7 +19,7 @@ describe('PythNode', function () {
   const emaPrice = parseUnits('420', decimals).toString();
   const timestamp = 4000;
 
-  before('prepare environment', async () => {
+  before('deploy mock contract', async () => {
     NodeModule = getContract('NodeModule');
 
     const [owner] = getSigners();
@@ -27,48 +27,102 @@ describe('PythNode', function () {
     // Deploy the mock
     const factory = await hre.ethers.getContractFactory('MockPyth');
     PythMock = await factory.connect(owner).deploy(100, 100);
-
-    // Set the latest price
-    const resp = await PythMock.createPriceFeedUpdateData(
-      priceFeedId,
-      price,
-      1,
-      -decimals,
-      emaPrice,
-      1,
-      timestamp
-    );
-    const fee = await PythMock.getUpdateFee([resp]);
-    await PythMock.updatePriceFeeds([resp], { value: fee });
   });
 
-  it('retrieves the latest price', async () => {
-    // Register the mock
-    const NodeParameters = abi.encode(
-      ['address', 'bytes32', 'bool'],
-      [PythMock.address, priceFeedId, false]
-    );
-    await NodeModule.registerNode(NodeTypes.PYTH, NodeParameters, []);
-    const nodeId = await NodeModule.getNodeId(NodeTypes.PYTH, NodeParameters, []);
+  describe('use updated price feed', () => {
+    before('update price feed', async () => {
+      // Set the latest price
+      const resp = await PythMock.createPriceFeedUpdateData(
+        priceFeedId,
+        price,
+        1,
+        -decimals,
+        emaPrice,
+        1,
+        timestamp
+      );
+      const fee = await PythMock.getUpdateFee([resp]);
+      await PythMock.updatePriceFeeds([resp], { value: fee });
+    });
 
-    // Verify the node processes output as expected
-    const output = await NodeModule.process(nodeId);
-    assertBn.equal(output.price, parseUnits(price, 18 - decimals).toString());
-    assertBn.equal(output.timestamp, timestamp);
+    it('retrieves the latest price', async () => {
+      // Register the mock
+      const NodeParameters = abi.encode(
+        ['address', 'bytes32', 'bool'],
+        [PythMock.address, priceFeedId, false]
+      );
+      await NodeModule.registerNode(NodeTypes.PYTH, NodeParameters, []);
+      const nodeId = await NodeModule.getNodeId(NodeTypes.PYTH, NodeParameters, []);
+
+      // Verify the node processes output as expected
+      const output = await NodeModule.process(nodeId);
+      assertBn.equal(output.price, parseUnits(price, 18 - decimals).toString());
+      assertBn.equal(output.timestamp, timestamp);
+    });
+
+    it('retrieves the ema price', async () => {
+      // Register the mock
+      const NodeParameters = abi.encode(
+        ['address', 'bytes32', 'bool'],
+        [PythMock.address, priceFeedId, true]
+      );
+      await NodeModule.registerNode(NodeTypes.PYTH, NodeParameters, []);
+      const nodeId = await NodeModule.getNodeId(NodeTypes.PYTH, NodeParameters, []);
+
+      // Verify the node processes output as expected
+      const output = await NodeModule.process(nodeId);
+      assertBn.equal(output.price, parseUnits(emaPrice, 18 - decimals).toString());
+      assertBn.equal(output.timestamp, timestamp);
+    });
   });
 
-  it('retrieves the ema price', async () => {
-    // Register the mock
-    const NodeParameters = abi.encode(
-      ['address', 'bytes32', 'bool'],
-      [PythMock.address, priceFeedId, true]
-    );
-    await NodeModule.registerNode(NodeTypes.PYTH, NodeParameters, []);
-    const nodeId = await NodeModule.getNodeId(NodeTypes.PYTH, NodeParameters, []);
+  describe('use parsed price feed', () => {
+    let updateFee: ethers.BigNumber;
+    let priceFeedData: string;
+    before('update price feed', async () => {
+      // Set the latest price
+      priceFeedData = await PythMock.createPriceFeedUpdateData(
+        priceFeedId,
+        price,
+        1,
+        -decimals,
+        emaPrice,
+        1,
+        timestamp
+      );
+      updateFee = await PythMock.getUpdateFee([priceFeedData]);
+    });
 
-    // Verify the node processes output as expected
-    const output = await NodeModule.process(nodeId);
-    assertBn.equal(output.price, parseUnits(emaPrice, 18 - decimals).toString());
-    assertBn.equal(output.timestamp, timestamp);
+    before('parse price feed data', async () => {});
+
+    it('retrieves latest price', async () => {
+      const parsedPrice = await PythMock.callStatic.parsePriceFeedUpdates(
+        [priceFeedData],
+        [priceFeedId],
+        timestamp - 10,
+        timestamp + 10,
+        { value: updateFee }
+      );
+
+      assertBn.equal(parsedPrice[0].price.price, price);
+      assertBn.equal(parsedPrice[0].price.conf, 1);
+      assertBn.equal(parsedPrice[0].price.expo, -decimals);
+      assertBn.equal(parsedPrice[0].price.publishTime, timestamp);
+    });
+
+    it('retrieves the ema price', async () => {
+      const parsedPrice = await PythMock.callStatic.parsePriceFeedUpdates(
+        [priceFeedData],
+        [priceFeedId],
+        timestamp - 10,
+        timestamp + 10,
+        { value: updateFee }
+      );
+
+      assertBn.equal(parsedPrice[0].emaPrice.price, emaPrice);
+      assertBn.equal(parsedPrice[0].emaPrice.conf, 1);
+      assertBn.equal(parsedPrice[0].emaPrice.expo, -decimals);
+      assertBn.equal(parsedPrice[0].emaPrice.publishTime, timestamp);
+    });
   });
 });
