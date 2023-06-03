7403ed1133678181d67321219f7dc32aca25f5be
Sunny Vempati <sunnyvempati@gmail.com>
2023-06-03 13:38:59 -0500

    fix uint

--- markets/spot-market/contracts/storage/AsyncOrderClaim.sol
+++ markets/spot-market/contracts/storage/AsyncOrderClaim.sol
@@ -53,7 +53,7 @@ library AsyncOrderClaim {
         address referrer;
     }
 
-    function load(uint128 marketId, uint256 claimId) internal pure returns (Data storage store) {
+    function load(uint128 marketId, uint128 claimId) internal pure returns (Data storage store) {
         bytes32 s = keccak256(
             abi.encode("io.synthetix.spot-market.AsyncOrderClaim", marketId, claimId)
         );
