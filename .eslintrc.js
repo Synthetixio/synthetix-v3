88ff87c834d65bb65761527341cb9dc17216ec9f
joey <joey@cc.snxdao.io>
2023-06-05 10:05:54 +1000

    Use tsconfig.eslint.json for ts files

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,9 +33,7 @@ module.exports = {
     {
       files: ['**/*.ts'],
       extends: ['plugin:@typescript-eslint/recommended'],
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
+
       rules: {
         '@typescript-eslint/no-floating-promises': 'error',
         '@typescript-eslint/no-var-requires': 0,
