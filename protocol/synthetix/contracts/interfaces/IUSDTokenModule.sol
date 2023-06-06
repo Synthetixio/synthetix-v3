3e9793dd5df5180fd52d58af488b3bdebe0350a8
meb <4982406+barrasso@users.noreply.github.com>
2023-06-06 10:22:59 -0400

    Add transfer cross chain USD tests (#1634)
    * add transferCrossChain tests
    
    * refund leftover gas and test cleanup
    
    * create CrossChainUSDModule

--- protocol/synthetix/contracts/interfaces/IUSDTokenModule.sol
+++ protocol/synthetix/contracts/interfaces/IUSDTokenModule.sol
@@ -20,17 +20,4 @@ interface IUSDTokenModule is ITokenModule {
      * @param amount The amount of snxUSD to be burned, denominated with 18 decimals of precision.
      */
     function burn(uint256 amount) external;
-
-    /**
-     * @notice Allows users to transfer tokens cross-chain using CCIP. This is disabled until _CCIP_CHAINLINK_SEND is set in UtilsModule. This is currently included for testing purposes. Functionality will change, including fee collection, as CCIP continues development.
-     * @param destChainId The id of the chain where tokens are to be transferred to.
-     * @param to The destination address in the target chain.
-     * @param amount The amount of tokens to be transferred, denominated with 18 decimals of precision.
-     * @return gasTokenUsed The amount of fees paid in the cross-chain transfer, denominated with 18 decimals of precision.
-     */
-    function transferCrossChain(
-        uint64 destChainId,
-        address to,
-        uint256 amount
-    ) external payable returns (uint256 gasTokenUsed);
 }
