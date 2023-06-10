10ded4b9a0f3d74caee072746e1b9e49415c5712
Sunny Vempati <sunnyvempati@gmail.com>
2023-06-10 11:25:21 -0500

    storage

--- markets/perps-market/storage.dump.sol
+++ markets/perps-market/storage.dump.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.4.22<0.9.0;
+pragma solidity >=0.8.11<0.9.0;
 
 // @custom:artifact @synthetixio/core-contracts/contracts/ownership/OwnableStorage.sol:OwnableStorage
 library OwnableStorage {
@@ -479,7 +479,7 @@ library AsyncOrder {
         uint currentLiquidationMargin;
         int128 newPositionSize;
         uint newNotionalValue;
-        uint currentAvailableMargin;
+        int currentAvailableMargin;
         uint requiredMaintenanceMargin;
         uint initialRequiredMargin;
         uint totalRequiredMargin;
@@ -659,9 +659,4 @@ library SettlementStrategy {
         uint256 priceDeviationTolerance;
         bool disabled;
     }
-}
-
-// @custom:artifact hardhat/console.sol:console
-library console {
-    address internal constant CONSOLE_ADDRESS = address(0x000000000000000000636F6e736F6c652e6c6f67);
 }
