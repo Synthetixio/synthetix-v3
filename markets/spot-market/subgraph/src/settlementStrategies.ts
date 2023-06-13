76ba0e153cde85bfdeedb977c8f52d72fe204bc9
Noah Litvin <noah.litvin@gmail.com>
2023-06-13 09:39:51 -0400

    Spot market subgraph

---
+++ markets/spot-market/subgraph/src/settlementStrategies.ts
@@ -0,0 +1,51 @@
+import {
+  SpotMarketProxy,
+  SettlementStrategyUpdated,
+  SettlementStrategyAdded,
+} from '../generated/SpotMarket/SpotMarketProxy';
+
+import { SettlementStrategy } from '../generated/schema';
+
+export function handleSettlementStrategyAdded(event: SettlementStrategyAdded): void {
+  let id = event.params.strategyId.toString();
+  let strategy = new SettlementStrategy(id);
+
+  let strategyInfo = SpotMarketProxy.bind(event.address).getSettlementStrategy(
+    event.params.synthMarketId,
+    event.params.strategyId
+  );
+
+  strategy.marketId = event.params.synthMarketId;
+  strategy.settlementStrategyId = event.params.strategyId;
+
+  strategy.strategyType = strategyInfo.strategyType;
+  strategy.settlementDelay = strategyInfo.settlementDelay;
+  strategy.settlementWindowDuration = strategyInfo.settlementWindowDuration;
+  strategy.priceVerificationContract = strategyInfo.priceVerificationContract.toHexString();
+  strategy.feedId = strategyInfo.feedId;
+  strategy.url = strategyInfo.url;
+  strategy.settlementReward = strategyInfo.settlementReward;
+  strategy.priceDeviationTolerance = strategyInfo.priceDeviationTolerance;
+  strategy.minimumUsdExchangeAmount = strategyInfo.minimumUsdExchangeAmount;
+  strategy.maxRoundingLoss = strategyInfo.maxRoundingLoss;
+  strategy.disabled = strategyInfo.disabled;
+
+  strategy.save();
+}
+
+export function handleSettlementStrategyUpdated(event: SettlementStrategyUpdated): void {
+  let id = event.params.strategyId.toString();
+  let strategy = SettlementStrategy.load(id);
+
+  if (!strategy) {
+    return;
+  }
+
+  let strategyInfo = SpotMarketProxy.bind(event.address).getSettlementStrategy(
+    event.params.synthMarketId,
+    event.params.strategyId
+  );
+
+  strategy.disabled = strategyInfo.disabled;
+  strategy.save();
+}
