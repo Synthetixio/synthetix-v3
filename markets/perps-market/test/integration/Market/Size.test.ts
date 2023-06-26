d435394a841598b74075fe93d61d1b5175b712b6
max <fritzschoff@icloud.com>
2023-06-26 12:08:13 +0100

    removed resetMarket function

--- markets/perps-market/test/integration/Market/Size.test.ts
+++ markets/perps-market/test/integration/Market/Size.test.ts
@@ -2,8 +2,6 @@ import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert
 import { PerpsMarket, bn, bootstrapMarkets } from '../bootstrap';
 import { depositCollateral, openPosition } from '../helpers';
 import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber';
-import { BigNumber, ethers } from 'ethers';
-import { wei } from '@synthetixio/wei';
 
 describe.only('Market - size test', () => {
   const { systems, perpsMarkets, provider, trader1, trader2, keeper, restore } = bootstrapMarkets({
@@ -25,61 +23,61 @@ describe.only('Market - size test', () => {
     ethMarket = perpsMarkets()[0];
   });
 
-  // before('add collateral to margin', async () => {
-  //   await systems().PerpsMarket.connect(trader1()).modifyCollateral(2, 0, bn(50_000));
-  //   await systems().PerpsMarket.connect(trader2()).modifyCollateral(3, 0, bn(1_000_000));
-  // });
+  before('add collateral to margin', async () => {
+    await systems().PerpsMarket.connect(trader1()).modifyCollateral(2, 0, bn(50_000));
+    await systems().PerpsMarket.connect(trader2()).modifyCollateral(3, 0, bn(1_000_000));
+  });
 
-  // before('open 20 eth position', async () => {
-  //   await openPosition({
-  //     systems,
-  //     provider,
-  //     trader: trader1(),
-  //     accountId: 2,
-  //     keeper: keeper(),
-  //     marketId: ethMarket.marketId(),
-  //     sizeDelta: bn(20),
-  //     settlementStrategyId: ethMarket.strategyId(),
-  //     price: bn(2000),
-  //   });
-  // });
+  before('open 20 eth position', async () => {
+    await openPosition({
+      systems,
+      provider,
+      trader: trader1(),
+      accountId: 2,
+      keeper: keeper(),
+      marketId: ethMarket.marketId(),
+      sizeDelta: bn(20),
+      settlementStrategyId: ethMarket.strategyId(),
+      price: bn(2000),
+    });
+  });
 
-  // // starting skew = 20 from above order
-  // // below test:
-  // // [newOrderSize, expectedSize, expectedSkew]
-  // [
-  //   [bn(20), bn(40), bn(40)],
-  //   [bn(-40), bn(40), bn(0)],
-  //   [bn(-25), bn(65), bn(-25)],
-  //   [bn(12), bn(53), bn(-13)],
-  //   [bn(-5), bn(58), bn(-18)],
-  //   [bn(-70), bn(128), bn(-88)],
-  //   [bn(180), bn(92), bn(92)],
-  //   [bn(-15), bn(77), bn(77)],
-  //   [bn(-19), bn(58), bn(58)],
-  // ].forEach(([newOrderSize, expectedSize, expectedSkew]) => {
-  //   describe('skew and size test', () => {
-  //     before('open position', async () => {
-  //       await openPosition({
-  //         systems,
-  //         provider,
-  //         trader: trader2(),
-  //         accountId: 3,
-  //         keeper: keeper(),
-  //         marketId: ethMarket.marketId(),
-  //         sizeDelta: newOrderSize,
-  //         settlementStrategyId: ethMarket.strategyId(),
-  //         price: bn(2000),
-  //       });
-  //     });
+  // starting skew = 20 from above order
+  // below test:
+  // [newOrderSize, expectedSize, expectedSkew]
+  [
+    [bn(20), bn(40), bn(40)],
+    [bn(-40), bn(40), bn(0)],
+    [bn(-25), bn(65), bn(-25)],
+    [bn(12), bn(53), bn(-13)],
+    [bn(-5), bn(58), bn(-18)],
+    [bn(-70), bn(128), bn(-88)],
+    [bn(180), bn(92), bn(92)],
+    [bn(-15), bn(77), bn(77)],
+    [bn(-19), bn(58), bn(58)],
+  ].forEach(([newOrderSize, expectedSize, expectedSkew]) => {
+    describe('skew and size test', () => {
+      before('open position', async () => {
+        await openPosition({
+          systems,
+          provider,
+          trader: trader2(),
+          accountId: 3,
+          keeper: keeper(),
+          marketId: ethMarket.marketId(),
+          sizeDelta: newOrderSize,
+          settlementStrategyId: ethMarket.strategyId(),
+          price: bn(2000),
+        });
+      });
 
-  //     it(`should have market size ${expectedSize} and skew ${expectedSkew} after newOrderSize ${newOrderSize}`, async () => {
-  //       const market = await systems().PerpsMarket.getMarketSummary(ethMarket.marketId());
-  //       assertBn.equal(market.size, expectedSize);
-  //       assertBn.equal(market.skew, expectedSkew);
-  //     });
-  //   });
-  // });
+      it(`should have market size ${expectedSize} and skew ${expectedSkew} after newOrderSize ${newOrderSize}`, async () => {
+        const market = await systems().PerpsMarket.getMarketSummary(ethMarket.marketId());
+        assertBn.equal(market.size, expectedSize);
+        assertBn.equal(market.skew, expectedSkew);
+      });
+    });
+  });
 
   describe('max market value', () => {
     describe('success', () => {
@@ -123,7 +121,6 @@ describe.only('Market - size test', () => {
           price: bn(2000),
         });
       });
-
       it('if user reduces size of his own trade', async () => {
         await openPosition({
           systems,
@@ -142,7 +139,7 @@ describe.only('Market - size test', () => {
         );
       });
 
-      it('max market size: 10_000, current size: 10_000, opening short: -9_000, results in 19_000 oi', async () => {
+      it('if max market size: 10_000, current size: 10_000, opening short: -9_000, results in 19_000 oi', async () => {
         await openPosition({
           systems,
           provider,
@@ -160,7 +157,7 @@ describe.only('Market - size test', () => {
         );
       });
 
-      it('max market size: 10_000, current size: 10_000, opening short: 10_000, results in 20_000 oi ', async () => {
+      it('if max market size: 10_000, current size: 10_000, opening short: 10_000, results in 20_000 oi ', async () => {
         await openPosition({
           systems,
           provider,
