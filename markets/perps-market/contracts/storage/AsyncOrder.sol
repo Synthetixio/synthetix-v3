004909c29e1df4e9c041a3017150768f81f97699
joey <joey@cc.snxdao.io>
2023-06-28 15:53:31 +1000

    Keep the calcs in the method. - not sure how to refactor it if I'm not suppose to break it out to methods 🤔

--- markets/perps-market/contracts/storage/AsyncOrder.sol
+++ markets/perps-market/contracts/storage/AsyncOrder.sol
@@ -284,30 +284,46 @@ library AsyncOrder {
         int size,
         uint price
     ) internal pure returns (uint) {
+        // How is the p/d-adjusted price calculated using an example:
+        //
+        // price      = $1200 USD (oracle)
+        // size       = 100
+        // skew       = 0
+        // skew_scale = 1,000,000 (1M)
+        //
+        // Then,
+        //
+        // pd_before = 0 / 1,000,000
+        //           = 0
+        // pd_after  = (0 + 100) / 1,000,000
+        //           = 100 / 1,000,000
+        //           = 0.0001
+        //
+        // price_before = 1200 * (1 + pd_before)
+        //              = 1200 * (1 + 0)
+        //              = 1200
+        // price_after  = 1200 * (1 + pd_after)
+        //              = 1200 * (1 + 0.0001)
+        //              = 1200 * (1.0001)
+        //              = 1200.12
+        // Finally,
+        //
+        // fill_price = (price_before + price_after) / 2
+        //            = (1200 + 1200.12) / 2
+        //            = 1200.06
         if (skewScale == 0) {
             return price;
         }
-        int pdBefore = calculatePD(skew, skewScale);
-        int pdAfter = calculatePD(skew + size, skewScale);
+        // calculate pd (premium/discount) before and after trade
+        int pdBefore = skew.divDecimal(skewScale.toInt());
+        int newSkew = skew + size;
+        int pdAfter = newSkew.divDecimal(skewScale.toInt());
 
-        int priceBefore = calculateAdjustedPrice(price, pdBefore);
-        int priceAfter = calculateAdjustedPrice(price, pdAfter);
+        // calculate price before and after trade with pd applied
+        int priceBefore = price.toInt() + (price.toInt().mulDecimal(pdBefore));
+        int priceAfter = price.toInt() + (price.toInt().mulDecimal(pdAfter));
 
-        return calculateAveragePrice(priceBefore, priceAfter);
-    }
-
-    // Calculates PD
-    function calculatePD(int skew, uint skewScale) internal pure returns (int) {
-        return skew.divDecimal(skewScale.toInt());
-    }
-
-    // Calculates the price with pd applied
-    function calculateAdjustedPrice(uint price, int pd) internal pure returns (int) {
-        return price.toInt() + (price.toInt().mulDecimal(pd));
-    }
-
-    // Calculates the average of two prices
-    function calculateAveragePrice(int priceBefore, int priceAfter) internal pure returns (uint) {
+        // the fill price is the average of those prices
         return (priceBefore + priceAfter).toUint().divDecimal(DecimalMath.UNIT * 2);
     }
 }
