3e1a6b39980cf123501391846e7c2281c5d946e9
max <fritzschoff@icloud.com>
2023-06-28 17:27:12 +0100

    fixing typo

--- markets/perps-market/test/integration/Market/MarketConfiguration.test.ts
+++ markets/perps-market/test/integration/Market/MarketConfiguration.test.ts
@@ -6,7 +6,7 @@ import assertBn from '@synthetixio/core-utils/src/utils/assertions/assert-bignum
 import assertEvent from '@synthetixio/core-utils/src/utils/assertions/assert-event';
 import assert from 'assert';
 
-describe('MarketConfiguration', async () => {
+describe.only('MarketConfiguration', async () => {
   const fixture = {
     token: 'snxETH',
     marketName: 'TestPerpsMarket',
