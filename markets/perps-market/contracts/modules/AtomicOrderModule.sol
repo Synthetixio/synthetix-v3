7a672792d0b52f43e1743834f8126e5e18cfe09f
Leonardo Massazza <lmassazza@gmail.com>
2023-07-03 16:51:00 -0300

    some misc docs

--- markets/perps-market/contracts/modules/AtomicOrderModule.sol
+++ markets/perps-market/contracts/modules/AtomicOrderModule.sol
@@ -1,4 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.11 <0.9.0;
 
-contract AtomicOrderModule {}
+/**
+ * @title Module for atomic orders. Not used.
+ */
+// solhint-disable-next-line no-empty-blocks
+contract AtomicOrderModule {
+
+}
--- markets/perps-market/contracts/modules/LimitOrderModule.sol
+++ markets/perps-market/contracts/modules/LimitOrderModule.sol
@@ -3,4 +3,10 @@ pragma solidity >=0.8.11 <0.9.0;
 
 import "../interfaces/ILimitOrderModule.sol";
 
-contract LimitOrderModule is ILimitOrderModule {}
+/**
+ * @title Module for limit orders. Not used.
+ */
+// solhint-disable-next-line no-empty-blocks
+contract LimitOrderModule is ILimitOrderModule {
+
+}
