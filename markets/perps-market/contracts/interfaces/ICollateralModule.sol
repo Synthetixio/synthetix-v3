9d1287ee0d7fcb4d2d7f8a4b00aebc20605128b7
Leonardo Massazza <lmassazza@gmail.com>
2023-07-03 16:51:16 -0300

    ICollateralModule docs

--- markets/perps-market/contracts/interfaces/ICollateralModule.sol
+++ markets/perps-market/contracts/interfaces/ICollateralModule.sol
@@ -6,16 +6,16 @@ pragma solidity >=0.8.11 <0.9.0;
  */
 interface ICollateralModule {
     /**
-     * @notice Gets fired when max collateral amount for synth is set by owner.
-     * @param synthId Synth market id, 0 for snxUSD.
-     * @param maxCollateralAmount max amount that was set for the synth
+     * @notice Gets fired when max collateral amount for synth collateral for the system is set by owner.
+     * @param synthMarketId Synth market id, 0 for snxUSD.
+     * @param collateralAmount max amount that was set for the synth
      */
-    event MaxCollateralSet(uint128 indexed synthId, uint256 maxCollateralAmount);
+    event MaxCollateralSet(uint128 indexed synthMarketId, uint256 collateralAmount);
 
     /**
      * @notice Set the max collateral amoutn via this function
-     * @param synthId Synth market id, 0 for snxUSD.
-     * @param maxCollateralAmount max amount that for the synth
+     * @param synthMarketId Synth market id, 0 for snxUSD.
+     * @param collateralAmount max amount that for the synth
      */
-    function setMaxCollateralAmount(uint128 synthId, uint maxCollateralAmount) external;
+    function setMaxCollateralAmount(uint128 synthMarketId, uint collateralAmount) external;
 }
