b83952e73d0ed60078bb39f0c2ff6cb35a17c254
Leonardo Massazza <lmassazza@gmail.com>
2023-07-03 13:07:09 -0300

    Docs partial

--- markets/perps-market/contracts/interfaces/IAsyncOrderModule.sol
+++ markets/perps-market/contracts/interfaces/IAsyncOrderModule.sol
@@ -8,6 +8,18 @@ import {SettlementStrategy} from "../storage/SettlementStrategy.sol";
  * @title Module for committing and settling async orders.
  */
 interface IAsyncOrderModule {
+    /**
+     * @notice Gets fired when a new order is committed.
+     * @param marketId Id of the market used for the trade.
+     * @param accountId Id of the account used for the trade.
+     * @param orderType Should send 0 (at time of writing) that correlates to the transaction type enum defined in SettlementStrategy.Type.
+     * @param sizeDelta requested change in size of the order sent by the user.
+     * @param acceptablePrice maximum or minimum, depending on the sizeDelta direction, accepted price to settle the order, set by the user.
+     * @param settlementTime Time at which the order can be settled.
+     * @param expirationTime Time at which the order expired.
+     * @param trackingCode Optional code for integrator tracking purposes.
+     * @param sender address of the sender of the order. Authorized to commit by account owner.
+     */
     event OrderCommitted(
         uint128 indexed marketId,
         uint128 indexed accountId,
@@ -20,6 +32,18 @@ interface IAsyncOrderModule {
         address sender
     );
 
+    /**
+     * @notice Gets fired when a new order is settled.
+     * @param marketId Id of the market used for the trade.
+     * @param accountId Id of the account used for the trade.
+     * @param fillPrice Price at which the order was settled.
+     * @param accountPnlRealized Realized PnL of the position at the time of settlement.
+     * @param newSize New size of the position after settlement.
+     * @param collectedFees Amount of fees collected by the protocol.
+     * @param settelementReward Amount of fees collected by the settler.
+     * @param trackingCode Optional code for integrator tracking purposes.
+     * @param settler address of the settler of the order.
+     */
     event OrderSettled(
         uint128 indexed marketId,
         uint128 indexed accountId,
@@ -32,6 +56,13 @@ interface IAsyncOrderModule {
         address settler
     );
 
+    /**
+     * @notice Gets fired when a new order is canceled.
+     * @param marketId Id of the market used for the trade.
+     * @param accountId Id of the account used for the trade.
+     * @param acceptablePrice maximum or minimum, depending on the sizeDelta direction, accepted price to settle the order, set by the user.
+     * @param settlementTime Time at which the order can be settled.
+     */
     event OrderCanceled(
         uint128 indexed marketId,
         uint128 indexed accountId,
@@ -39,8 +70,19 @@ interface IAsyncOrderModule {
         uint256 acceptablePrice
     );
 
+    /**
+     * @notice Gets thrown when commit order is called when a pending order already exists.
+     */
     error OrderAlreadyCommitted(uint128 marketId, uint128 accountId);
+
+    /**
+     * @notice Gets thrown when settle order is called with invalid settlement strategy.
+     */
     error SettlementStrategyNotFound(SettlementStrategy.Type strategyType);
+
+    /**
+     * @notice Gets thrown when settle order is called as a signal to the client to perform offchain lookup.
+     */
     error OffchainLookup(
         address sender,
         string[] urls,
@@ -49,11 +91,33 @@ interface IAsyncOrderModule {
         bytes extraData
     );
 
+    /**
+     * @notice Commit an async order via this function
+     * @param commitment Order commitment data (see AsyncOrder.OrderCommitmentRequest struct).
+     * @return retOrder order details (see AsyncOrder.Data struct).
+     * @return fees order fees (protocol + settler)
+     */
     function commitOrder(
         AsyncOrder.OrderCommitmentRequest memory commitment
     ) external returns (AsyncOrder.Data memory retOrder, uint fees);
 
+    /**
+     * @notice Cancel an expired order via this function
+     * @param marketId Id of the market used for the trade.
+     * @param accountId Id of the account used for the trade.
+     */
     function cancelOrder(uint128 marketId, uint128 accountId) external;
+
+    /**
+     * @notice Get an order details via this function
+     * @param marketId Id of the market used for the trade.
+     * @param accountId Id of the account used for the trade.
+     * @return order order details (see AsyncOrder.Data struct).
+     */
+    function getOrder(
+        uint128 marketId,
+        uint128 accountId
+    ) external returns (AsyncOrder.Data memory order);
 
     // only used due to stack too deep during settlement
     struct SettleOrderRuntime {
@@ -66,9 +130,4 @@ interface IAsyncOrderModule {
         uint256 settlementReward;
         bytes32 trackingCode;
     }
-
-    function getOrder(
-        uint128 marketId,
-        uint128 accountId
-    ) external returns (AsyncOrder.Data memory);
 }
--- markets/perps-market/contracts/interfaces/ICollateralModule.sol
+++ markets/perps-market/contracts/interfaces/ICollateralModule.sol
@@ -1,6 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.11 <0.9.0;
 
+/**
+ * @title Module for setting max collateral distribution.
+ */
 interface ICollateralModule {
     /**
      * @notice Gets fired when max collateral amount for synth is set by owner.
@@ -9,5 +12,10 @@ interface ICollateralModule {
      */
     event MaxCollateralSet(uint128 indexed synthId, uint256 maxCollateralAmount);
 
+    /**
+     * @notice Set the max collateral amoutn via this function
+     * @param synthId Synth market id, 0 for snxUSD.
+     * @param maxCollateralAmount max amount that for the synth
+     */
     function setMaxCollateralAmount(uint128 synthId, uint maxCollateralAmount) external;
 }
