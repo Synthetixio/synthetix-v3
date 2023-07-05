df26ebd071f7298ba415397244014d5b9ce6635b
Joey <5688912+bachstatter@users.noreply.github.com>
2023-07-05 12:42:57 +1000

    MarketUpdated event (#1697)
    * Add tracking code to runtime
    
    * Add market size and market skew to
    
    * Update test asserting the event
    
    * Add MarketUpdated event.
    
    Note that for an event to be emitted from a library, it needs to be defined in two places:
    - The library itself, and
    - The interface of the contract that is using the library.
    
    * We now hit a size limit AsyncOrderModule
    - Creating a AsyncOrderViewModule
    
    * Dont forget to update cannon files
    
    * Add currentFundingRate and currentFundingVelocity to MarketUpdated event
    
    * Revert IAsyncOrderViewModule
    
    * Use AsyncOrderSettlementModule like spot market
    
    * Let call site emit MarketUpdated event to avoid emitting events from library

--- markets/perps-market/contracts/interfaces/IAsyncOrderModule.sol
+++ markets/perps-market/contracts/interfaces/IAsyncOrderModule.sol
@@ -20,18 +20,6 @@ interface IAsyncOrderModule {
         address sender
     );
 
-    event OrderSettled(
-        uint128 indexed marketId,
-        uint128 indexed accountId,
-        uint256 fillPrice,
-        int256 accountPnlRealized,
-        int128 newSize,
-        uint256 collectedFees,
-        uint256 settelementReward,
-        bytes32 indexed trackingCode,
-        address settler
-    );
-
     event OrderCanceled(
         uint128 indexed marketId,
         uint128 indexed accountId,
@@ -40,35 +28,15 @@ interface IAsyncOrderModule {
     );
 
     error OrderAlreadyCommitted(uint128 marketId, uint128 accountId);
-    error SettlementStrategyNotFound(SettlementStrategy.Type strategyType);
-    error OffchainLookup(
-        address sender,
-        string[] urls,
-        bytes callData,
-        bytes4 callbackFunction,
-        bytes extraData
-    );
 
     function commitOrder(
         AsyncOrder.OrderCommitmentRequest memory commitment
     ) external returns (AsyncOrder.Data memory retOrder, uint fees);
 
-    function cancelOrder(uint128 marketId, uint128 accountId) external;
-
-    // only used due to stack too deep during settlement
-    struct SettleOrderRuntime {
-        uint128 marketId;
-        uint128 accountId;
-        int128 newPositionSize;
-        int256 pnl;
-        uint256 pnlUint;
-        uint256 amountToDeposit;
-        uint256 settlementReward;
-        bytes32 trackingCode;
-    }
-
     function getOrder(
         uint128 marketId,
         uint128 accountId
     ) external returns (AsyncOrder.Data memory);
+
+    function cancelOrder(uint128 marketId, uint128 accountId) external;
 }
