2f9c8e2299fd93c56fb25eeb7f9d64808c00211b
David Vuong <david.vuong256@gmail.com>
2023-07-09 14:16:27 +1000

    refactor+docs: Rename IPositionModule to ILiquidationModule
    Creating a separate module for position views. Since a position can only
    be modified via orders, it will mostly just be views. Liquidations is
    the only collection of mutatives that can adjust a position but
    liquidations alone is quite complicated so makes sense to separate this
    into another module.

--- contracts/interfaces/ILiquidationModule.sol
+++ contracts/interfaces/ILiquidationModule.sol
@@ -1,18 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.11 <0.9.0;
 
-interface IPositionModule {
+interface ILiquidationModule {
     // --- Mutative --- //
 
+    /**
+     * @dev Flags position belonging to `accountId` for liquidation. A flagged position is frozen from all operations.
+     */
     function flagPosition(uint128 accountId, uint128 marketId) external;
 
+    /**
+     * @dev Liquidates a flagged position.
+     */
     function liquidatePosition(uint128 accountId, uint128 marketId) external;
-
-    function forceLiquidatePosition(uint128 accountId, uint128 marketId) external;
 
     // --- Views --- //
 
+    /**
+     * @dev Returns whether a position owned by `accountId` can be flagged for liquidated.
+     */
     function canLiquidate(uint128 accountId, uint128 marketId) external view returns (bool);
-
-    function isFlagged(uint128 accountId, uint128 marketId) external view returns (bool);
 }
