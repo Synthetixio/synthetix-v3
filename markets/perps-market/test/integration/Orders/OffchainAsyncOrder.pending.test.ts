2ed8c052e77ebe8a976d1e5a2b417ee78129b902
Leonardo Massazza <lmassazza@gmail.com>
2023-07-10 22:20:26 -0300

    checkpoint

--- markets/perps-market/test/integration/Orders/OffchainAsyncOrder.pending.test.ts
+++ markets/perps-market/test/integration/Orders/OffchainAsyncOrder.pending.test.ts
@@ -29,19 +29,29 @@ describe('Offchain Async Order - Prevent updates with pending order test', () =>
         price: bn(1000),
         fundingParams: { skewScale: bn(100_000), maxFundingVelocity: bn(10) },
       },
+      {
+        name: 'Bitcoin',
+        token: 'snxBTC',
+        price: bn(10_000),
+        fundingParams: { skewScale: bn(100_000), maxFundingVelocity: bn(10) },
+      },
     ],
     traderAccountIds: [2, 3],
   });
   let ethMarketId: ethers.BigNumber;
+  let btcMarketId: ethers.BigNumber;
   let btcSynth: SynthMarkets[number];
 
   before('identify actors', async () => {
     ethMarketId = perpsMarkets()[0].marketId();
+    btcMarketId = perpsMarkets()[1].marketId();
     btcSynth = synthMarkets()[0];
   });
 
-
   describe('With a pending order', () => {
+    let startTime: number;
+    let extraData: string;
+    let pythCallData: string;
     before('add collateral', async () => {
       await depositCollateral({
         systems,
@@ -56,7 +66,7 @@ describe('Offchain Async Order - Prevent updates with pending order test', () =>
     });
 
     before('commit the order', async () => {
-      tx = await systems()
+      const tx = await systems()
         .PerpsMarket.connect(trader1())
         .commitOrder({
           marketId: ethMarketId,
@@ -83,28 +93,74 @@ describe('Offchain Async Order - Prevent updates with pending order test', () =>
     const restoreToCommit = snapshotCheckpoint(provider);
 
     describe('failures', () => {
-      before(restoreToCommit)
-      
-      it('reverts if attempt to update collateral', async () => {})
+      before(restoreToCommit);
 
-      it('reverts if attempt to commit another order', async () => {})
-    })
-    
+      it('reverts if attempt to update collateral', async () => {
+        await assertRevert(
+          systems().PerpsMarket.connect(trader1()).modifyCollateral(2, 0, bn(10)),
+          'PendingOrdersExist()',
+          systems().PerpsMarket
+        );
+      });
+
+      it('reverts if attempt to commit another order', async () => {
+        await assertRevert(
+          systems()
+            .PerpsMarket.connect(trader1())
+            .commitOrder({
+              marketId: btcMarketId,
+              accountId: 2,
+              sizeDelta: bn(0.01),
+              settlementStrategyId: 0,
+              acceptablePrice: bn(10050), // 5% slippage
+              trackingCode: ethers.constants.HashZero,
+            }),
+          'PendingOrdersExist()',
+          systems().PerpsMarket
+        );
+      });
+    });
+
     describe('after settle the pending order', () => {
-      before(restoreToCommit)
-      before('settle the order', async () => {})
-      it('can update the collateral', async () => {})
+      before(restoreToCommit);
+      before('settle the order', async () => {});
+      it('can update the collateral', async () => {
+        systems().PerpsMarket.connect(trader1()).modifyCollateral(2, 0, bn(10));
+      });
 
-      it('can commit another order', async () => {})
-    })
+      it('can commit another order', async () => {
+        systems()
+          .PerpsMarket.connect(trader1())
+          .commitOrder({
+            marketId: btcMarketId,
+            accountId: 2,
+            sizeDelta: bn(0.01),
+            settlementStrategyId: 0,
+            acceptablePrice: bn(10050), // 5% slippage
+            trackingCode: ethers.constants.HashZero,
+          });
+      });
+    });
 
     describe('after cancel the pending order', () => {
-      before(restoreToCommit)
-      before('cancel the order', async () => {})
-      it('can update the collateral', async () => {})
+      before(restoreToCommit);
+      before('cancel the order', async () => {});
+      it('can update the collateral', async () => {
+        systems().PerpsMarket.connect(trader1()).modifyCollateral(2, 0, bn(10));
+      });
 
-      it('can commit another order', async () => {})
-    })
-  })
-
+      it('can commit another order', async () => {
+        systems()
+          .PerpsMarket.connect(trader1())
+          .commitOrder({
+            marketId: btcMarketId,
+            accountId: 2,
+            sizeDelta: bn(0.01),
+            settlementStrategyId: 0,
+            acceptablePrice: bn(10050), // 5% slippage
+            trackingCode: ethers.constants.HashZero,
+          });
+      });
+    });
+  });
 });
