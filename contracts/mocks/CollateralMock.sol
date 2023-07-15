e439e8fbc7b749f21356827a8224fb01e45b4131
David Vuong <david.vuong256@gmail.com>
2023-07-15 22:03:59 +0800

    test: Add a few mocks for test dev

---
+++ contracts/mocks/CollateralMock.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.11 <0.9.0;
+
+import {CollateralMock as BaseCollateralMock} from "@synthetixio/main/contracts/mocks/CollateralMock.sol";
+
+// solhint-disable-next-line no-empty-blocks
+contract CollateralMock is BaseCollateralMock {
+
+}
