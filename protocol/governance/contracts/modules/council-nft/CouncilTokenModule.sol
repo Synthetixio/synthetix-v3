72b5a6acc9a8ceeff497f1d551f2908c0b97d52d
Matías <mjlescano@protonmail.com>
2023-07-18 12:58:27 +0200

    fix lint

--- protocol/governance/contracts/modules/council-nft/CouncilTokenModule.sol
+++ protocol/governance/contracts/modules/council-nft/CouncilTokenModule.sol
@@ -2,10 +2,10 @@
 pragma solidity >=0.8.11 <0.9.0;
 
 import "@synthetixio/core-modules/contracts/modules/NftModule.sol";
+import "@synthetixio/core-contracts/contracts/utils/SafeCast.sol";
 import "../../interfaces/ICouncilTokenModule.sol";
 
-import "@synthetixio/core-contracts/contracts/utils/SafeCast.sol";
-
+/* solhint-disable no-empty-blocks */
 /**
  * @title Module with custom NFT logic for the account token.
  * @dev See IAccountTokenModule.
@@ -13,3 +13,4 @@ import "@synthetixio/core-contracts/contracts/utils/SafeCast.sol";
 contract CouncilTokenModule is ICouncilTokenModule, NftModule {
 
 }
+/* solhint-enable no-empty-blocks */
