6b09c9b3f279db2f62eb638afcdd30228c9ff090
Noisekit <nikita@cc.snxdao.io>
2023-07-19 14:05:58 +0200

    Generate full v3 Contracts page

---
+++ utils/docgen/docgen:contracts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ROOT=$(yarn workspace synthetix-v3 exec pwd)
+OUT="$ROOT/docs/Contracts.md"
+mkdir -p $ROOT/docs
+touch $OUT
+
+echo "# Smart Contracts" > $OUT
+echo "" >> $OUT
+
+echo "# Synthetix Core" >> $OUT
+echo "" >> $OUT
+cat $ROOT/protocol/synthetix/docs/index.md >> $OUT
+
+echo "# Spot Market" >> $OUT
+echo "" >> $OUT
+cat $ROOT/markets/spot-market/docs/index.md >> $OUT
+
+echo "# Perps Market" >> $OUT
+echo "" >> $OUT
+cat $ROOT/markets/perps-market/docs/index.md >> $OUT
+
+echo "# Governance" >> $OUT
+echo "" >> $OUT
+cat $ROOT/protocol/governance/docs/index.md >> $OUT
+
+echo "# Oracle Manager" >> $OUT
+echo "" >> $OUT
+cat $ROOT/protocol/oracle-manager/docs/index.md >> $OUT
