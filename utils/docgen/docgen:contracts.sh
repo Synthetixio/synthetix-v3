0297ff4349b5e28c42789192e11d898152a5a90e
Noisekit <nikita@cc.snxdao.io>
2023-07-19 16:28:31 +0200

    Add TOC

--- utils/docgen/docgen:contracts.sh
+++ utils/docgen/docgen:contracts.sh
@@ -8,22 +8,30 @@ touch $OUT
 echo "# Smart Contracts" > $OUT
 echo "" >> $OUT
 
-echo "# Synthetix Core" >> $OUT
+echo "- [Synthetix Core](#synthetix-core)" >> $OUT
+echo "- [Spot Market](#spot-market)" >> $OUT
+echo "- [Perps Market](#perps-market)" >> $OUT
+echo "- [Governance](#governance)" >> $OUT
+echo "- [Oracle Manager](#oracle-manager)" >> $OUT
+echo "" >> $OUT
+
+
+echo "## Synthetix Core" >> $OUT
 echo "" >> $OUT
 cat $ROOT/protocol/synthetix/docs/index.md >> $OUT
 
-echo "# Spot Market" >> $OUT
+echo "## Spot Market" >> $OUT
 echo "" >> $OUT
 cat $ROOT/markets/spot-market/docs/index.md >> $OUT
 
-echo "# Perps Market" >> $OUT
+echo "## Perps Market" >> $OUT
 echo "" >> $OUT
 cat $ROOT/markets/perps-market/docs/index.md >> $OUT
 
-echo "# Governance" >> $OUT
+echo "## Governance" >> $OUT
 echo "" >> $OUT
 cat $ROOT/protocol/governance/docs/index.md >> $OUT
 
-echo "# Oracle Manager" >> $OUT
+echo "## Oracle Manager" >> $OUT
 echo "" >> $OUT
 cat $ROOT/protocol/oracle-manager/docs/index.md >> $OUT
