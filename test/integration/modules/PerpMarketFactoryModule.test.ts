6e7885e59136c9de612fa7b5306568102f6531b2
David Vuong <david.vuong256@gmail.com>
2023-07-20 11:37:06 +0200

    test: Add PerpMarketFactoryModule tests

--- test/integration/modules/PerpMarketFactoryModule.test.ts
+++ test/integration/modules/PerpMarketFactoryModule.test.ts
@@ -1,11 +1,53 @@
+import assert from 'assert';
+import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
+import { bootstrap } from '../../bootstrap';
+import { genAddress, genBootstrap } from '../../generators';
+
 describe('PerpMarketFactoryModule', () => {
+  const { traders, owner, systems, restore } = bootstrap(genBootstrap());
+
+  beforeEach(restore);
+
   describe('setSynthetix', () => {
-    it('should set successfully');
-    it('should revert when not owner');
+    it('should revert when invalid synthetix addr (due to needing USD token)', async () => {
+      const { PerpMarketProxy } = systems();
+      const from = owner();
+
+      const address = genAddress();
+      await assertRevert(
+        PerpMarketProxy.connect(from).setSynthetix(address),
+        'Error: transaction reverted in contract unknown'
+      );
+    });
+
+    it('should revert when not owner', async () => {
+      const { PerpMarketProxy } = systems();
+      const from = traders()[0].signer;
+      const address = genAddress();
+      await assertRevert(
+        PerpMarketProxy.connect(from).setSynthetix(address),
+        `Unauthorized("${await from.getAddress()}")`
+      );
+    });
   });
 
-  describe('.setPyth', () => {
-    it('should set successfully');
-    it('should revert when not owner');
+  describe('setPyth', () => {
+    it('should set successfully', async () => {
+      const { PerpMarketProxy } = systems();
+      const from = owner();
+
+      const address = genAddress();
+      await PerpMarketProxy.connect(from).setPyth(address);
+      const config = await PerpMarketProxy.getMarketParameters();
+
+      assert(config.pyth, address);
+    });
+
+    it('should revert when not owner', async () => {
+      const { PerpMarketProxy } = systems();
+      const from = traders()[0].signer;
+      const address = genAddress();
+      await assertRevert(PerpMarketProxy.connect(from).setPyth(address), `Unauthorized("${await from.getAddress()}")`);
+    });
   });
 });
