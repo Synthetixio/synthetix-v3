45bb2bd2f6155a11139af88e88d72e060931ba6a
Peiman <25097709+Rickk137@users.noreply.github.com>
2023-08-04 22:32:38 +0330

    Perps subgraph update (#1747)
    * fix: perps graph issue
    
    * feat: update core subgraph core proxy
    
    * feat: update perps subgraph
    
    * Update markets/perps-market/subgraph/package.json
    
    Co-authored-by: troy (troyb.eth) <troy.burmeister@gmail.com>
    
    ---------
    
    Co-authored-by: Sunny Vempati <sunnyvempati@gmail.com>
    Co-authored-by: troy (troyb.eth) <troy.burmeister@gmail.com>

--- markets/perps-market/subgraph/src/account.ts
+++ markets/perps-market/subgraph/src/account.ts
@@ -1,6 +1,10 @@
-import { AccountCreated } from '../generated/PerpsMarketProxy/PerpsMarketProxy';
+import {
+  AccountCreated,
+  AccountLiquidated,
+  PositionLiquidated,
+} from '../generated/PerpsMarketProxy/PerpsMarketProxy';
 
-import { Account } from '../generated/schema';
+import { Account, LiquidatedAccount, LiquidatedPosition } from '../generated/schema';
 
 export function handleAccountCreated(event: AccountCreated): void {
   const id = event.params.accountId.toString();
@@ -9,5 +13,29 @@ export function handleAccountCreated(event: AccountCreated): void {
 
   account.accountId = event.params.accountId;
   account.owner = event.params.owner.toHexString();
+  account.save();
+}
+
+export function handleAccountLiquidated(event: AccountLiquidated): void {
+  const id = event.transaction.hash.toHexString();
+
+  const account = new LiquidatedAccount(id);
+
+  account.accountId = event.params.accountId;
+  account.accountFullyLiquidated = event.params.fullLiquidation;
+  account.keeperLiquidationReward = event.params.reward;
+  account.save();
+}
+
+export function handlePositionLiquidated(event: PositionLiquidated): void {
+  const id = event.transaction.hash.toHexString();
+
+  const account = new LiquidatedPosition(id);
+
+  account.accountId = event.params.accountId;
+  account.marketId = event.params.marketId;
+  account.amountLiquidated = event.params.amountLiquidated;
+  account.currentPositionSize = event.params.currentPositionSize;
+
   account.save();
 }
