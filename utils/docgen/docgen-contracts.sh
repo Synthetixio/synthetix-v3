b6d9d0c10e873484ebaf0c1141cb98158659a499
Noisekit <nikita@cc.snxdao.io>
2023-08-18 10:09:37 +0800

    Remove TOC as gitbook auto-generates it

--- utils/docgen/docgen-contracts.sh
+++ utils/docgen/docgen-contracts.sh
@@ -11,45 +11,27 @@ touch $OUT
 echo "# Smart Contracts" > $OUT
 echo "" >> $OUT
 
-echo "- [Synthetix Core](#synthetix-core)" >> $OUT
-echo "- [Spot Market](#spot-market)" >> $OUT
-echo "- [Perps Market](#perps-market)" >> $OUT
-echo "- [Legacy Market](#legacy-market)" >> $OUT
-echo "- [Governance](#governance)" >> $OUT
-echo "- [Oracle Manager](#oracle-manager)" >> $OUT
-echo "" >> $OUT
-
 echo "## Synthetix Core" >> $OUT
 echo "" >> $OUT
 cat $ROOT/protocol/synthetix/docs/index.md >> $OUT
 
 echo "## Spot Market" >> $OUT
 echo "" >> $OUT
-echo "- [Back to TOC](#synthetix-core)" >> $OUT
-echo "" >> $OUT
 cat $ROOT/markets/spot-market/docs/index.md >> $OUT
 
 echo "## Perps Market" >> $OUT
-echo "" >> $OUT
-echo "- [Back to TOC](#synthetix-core)" >> $OUT
 echo "" >> $OUT
 cat $ROOT/markets/perps-market/docs/index.md >> $OUT
 
 echo "## Legacy Market" >> $OUT
 echo "" >> $OUT
-echo "- [Back to TOC](#synthetix-core)" >> $OUT
-echo "" >> $OUT
 cat $ROOT/markets/legacy-market/docs/index.md >> $OUT
 
 echo "## Governance" >> $OUT
 echo "" >> $OUT
-echo "- [Back to TOC](#synthetix-core)" >> $OUT
-echo "" >> $OUT
 cat $ROOT/protocol/governance/docs/index.md >> $OUT
 
 echo "## Oracle Manager" >> $OUT
-echo "" >> $OUT
-echo "- [Back to TOC](#synthetix-core)" >> $OUT
 echo "" >> $OUT
 cat $ROOT/protocol/oracle-manager/docs/index.md >> $OUT
 
