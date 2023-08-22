5bc16cec2198b9b3f88ff7be40b38c3c16b47ae8
Noisekit <nikita@cc.snxdao.io>
2023-08-22 16:08:52 +0800

    Refactor synthetix subgraph splitting all handlers and tests (#1783)
    This is a prep work to get reusable handlers and tests across multiple network deployments

--- protocol/synthetix/subgraph/src/marketSnapshotByWeek.ts
+++ protocol/synthetix/subgraph/src/marketSnapshotByWeek.ts
@@ -1,26 +1,7 @@
 /* eslint-disable no-undef */
 import { Market, MarketSnapshotByWeek } from '../generated/schema';
+import { getISOWeekNumber } from './getISOWeekNumber';
 import { BigInt } from '@graphprotocol/graph-ts';
-
-// exported for test
-export function getISOWeekNumber(timestamp: i64): i64 {
-  const dateOfTimeStamp = new Date(<i64>timestamp).toISOString().slice(0, 4);
-  // Calculate the timestamp for the first day of the year
-  const firstDayDate = Date.parse(dateOfTimeStamp + '-01-01T00:00:00.000Z');
-
-  const firstDayTimestamp = firstDayDate.getTime() as f64;
-  // Calculate the day of the week for the first day of the year (0 = Sunday, 6 = Saturday)
-  const firstDayOfWeek = firstDayDate.getUTCDay() as f64;
-
-  // Calculate the number of milliseconds in one day
-  const oneDay = (1000 * 60 * 60 * 24) as f64;
-  // Calculate the number of days between the first day of the year and the date in question
-  const msSinceFirstDayOfYear = ((timestamp as f64) - firstDayTimestamp) as f64;
-  const days = Math.round(msSinceFirstDayOfYear / oneDay);
-
-  // Calculate the week number (weeks start on Monday)
-  return Math.floor((days + firstDayOfWeek) / 7) as i64;
-}
 
 export function createMarketSnapshotByWeek(marketWithLatestValues: Market): void {
   const date = new Date(<i64>parseInt(marketWithLatestValues.updated_at.toString()) * 1000);
---
+++ protocol/synthetix/subgraph/tests/handleUSDMinted.ts
@@ -0,0 +1,51 @@
+import { assert, createMockedFunction } from 'matchstick-as';
+import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts';
+import { address, defaultGraphContractAddress } from './constants';
+import { handleDelegationUpdated, handleUSDMinted } from '../src';
+import { createDelegationUpdateEvent, createUSDMintedEvent } from './event-factories';
+
+export default function test(): void {
+  // Needs to be here because of Closures
+  const now = new Date(1668448739566).getTime();
+  const newDelegationUpdatedEvent = createDelegationUpdateEvent(
+    BigInt.fromI32(1),
+    BigInt.fromI32(1),
+    Address.fromString(address),
+    BigInt.fromI32(2323),
+    BigInt.fromI32(10),
+    now,
+    now - 1000
+  );
+  createMockedFunction(
+    Address.fromString(defaultGraphContractAddress),
+    'getPositionCollateralizationRatio',
+    'getPositionCollateralizationRatio(uint128,uint128,address):(uint256)'
+  )
+    .withArgs([
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(1)),
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(1)),
+      ethereum.Value.fromAddress(Address.fromString(address)),
+    ])
+    .returns([ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(200))]);
+  handleDelegationUpdated(newDelegationUpdatedEvent);
+  const newUSDMintedEvent = createUSDMintedEvent(
+    BigInt.fromI32(1),
+    BigInt.fromI32(1),
+    Address.fromString(address),
+    BigInt.fromI32(2000),
+    now + 1000,
+    now
+  );
+  handleUSDMinted(newUSDMintedEvent);
+  assert.fieldEquals('Position', `1-1-${address}`, 'id', `1-1-${address}`);
+  assert.fieldEquals('Position', `1-1-${address}`, 'created_at', now.toString());
+  assert.fieldEquals('Position', `1-1-${address}`, 'created_at_block', (now - 1000).toString());
+  assert.fieldEquals('Position', `1-1-${address}`, 'collateral_amount', '2323');
+  assert.fieldEquals('Position', `1-1-${address}`, 'updated_at', (now + 1000).toString());
+  assert.fieldEquals('Position', `1-1-${address}`, 'updated_at_block', now.toString());
+  // assert.fieldEquals('Position', `1-1-${address}`, 'c_ratio', '200');
+  assert.fieldEquals('Position', `1-1-${address}`, 'leverage', '10');
+  assert.fieldEquals('Position', `1-1-${address}`, 'total_minted', '2000');
+  handleUSDMinted(newUSDMintedEvent);
+  assert.fieldEquals('Position', `1-1-${address}`, 'total_minted', '4000');
+}
