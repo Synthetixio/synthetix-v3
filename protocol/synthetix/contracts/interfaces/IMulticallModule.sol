aadd24145b08fd10576925d0040033a48902a047
dbeal <git@danb.email>
2023-09-01 22:13:48 +0000

    SIP-325: add multicall through (#1750)
    * add multicall through
    
    implements [SIP-325](https://sips.synthetix.io/sips/sip-325/).
    
    most of the code was already there and ready to go, just needed to be
    commented out for its inevitable inclusion in the protocol.
    
    * add ability to specify allowlist of multicall targets for throuugh
    
    this was hte main feedback we got from the auditors on the previous
    generation. It is important for EIP-7412 to have this impl
    
    * add tests
    
    * add multicall module to regualr perps market
    
    * fix audit feedback
    
    * fix multicall tests

--- protocol/synthetix/contracts/interfaces/IMulticallModule.sol
+++ protocol/synthetix/contracts/interfaces/IMulticallModule.sol
@@ -5,6 +5,9 @@ pragma solidity >=0.8.11 <0.9.0;
  * @title Module that enables calling multiple methods of the system in a single transaction.
  */
 interface IMulticallModule {
+    error RecursiveMulticall(address);
+    error DeniedMulticallTarget(address);
+
     /**
      * @notice Executes multiple transaction payloads in a single transaction.
      * @dev Each transaction is executed using `delegatecall`, and targets the system address.
@@ -12,4 +15,31 @@ interface IMulticallModule {
      * @return results Array of each `delegatecall`'s response corresponding to the incoming calldata array.
      */
     function multicall(bytes[] calldata data) external returns (bytes[] memory results);
+
+    /**
+     * @notice Similar to `multicall`, but allows for transactions to be executed
+     * @dev If the address specified in `to` iteration is not the core system, it will call the contract with a regular "call". If it is the core system, it will be delegatecall.
+     * @dev Target `to` contracts will need to support calling the below `getMessageSender` rather than regular `msg.sender` in order to allow for usage of permissioned calls with this function
+     * @dev It is not possible to call this function recursively.
+     * @dev Fails immediately on revert of any call.
+     * @return results Array of each call's response corresponding
+     */
+    function multicallThrough(
+        address[] calldata to,
+        bytes[] calldata data,
+        uint256[] calldata values
+    ) external payable returns (bytes[] memory results);
+
+    /**
+     * @notice Permit the given target to be called through `multicallThrough`.
+     * @dev This function can only be called by the system owner.
+     * @param target The address of the contract to alter permissions
+     * @param allowlisted Whether or not the target is allowlisted
+     */
+    function setAllowlistedMulticallTarget(address target, bool allowlisted) external;
+
+    /**
+     * @notice When receiving a call from this contract through `multicallThrough`, the receiver can use this function to get the original caller.
+     */
+    function getMessageSender() external view returns (address);
 }
