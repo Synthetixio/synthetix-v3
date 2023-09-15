a47d363ef744603278ac5616fb6a60995e9d08ef
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2023-09-15 09:09:56 -0500

    update context and change linter

--- protocol/synthetix/contracts/utils/ERC2771Context.sol
+++ protocol/synthetix/contracts/utils/ERC2771Context.sol
@@ -1,34 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "../storage/Config.sol";
+contract ERC2771Context {
+    // This is the trusted-multicall-forwarder. The address is constant due to CREATE2.
+    address private constant TRUSTED_FORWARDER = 0xAE788aaf52780741E12BF79Ad684B91Bb0EF4D92;
 
-library ERC2771Context {
     function _msgSender() internal view returns (address sender) {
-        if (_isTrustedForwarder(msg.sender) && msg.data.length >= 20) {
+        // solhint-disable-next-line erc2771/no-msg-sender erc2771/no-msg-data
+        if (isTrustedForwarder(msg.sender) && msg.data.length >= 20) {
             // The assembly code is more direct than the Solidity version using `abi.decode`.
             /// @solidity memory-safe-assembly
             assembly {
                 sender := shr(96, calldataload(sub(calldatasize(), 20)))
             }
         } else {
+            // solhint-disable-next-line erc2771/no-msg-sender
             return msg.sender;
         }
     }
 
     function _msgData() internal view returns (bytes calldata) {
-        if (_isTrustedForwarder(msg.sender) && msg.data.length >= 20) {
+        // solhint-disable-next-line erc2771/no-msg-sender erc2771/no-msg-data
+        if (isTrustedForwarder(msg.sender) && msg.data.length >= 20) {
+            // solhint-disable-next-line erc2771/no-msg-data
             return msg.data[:msg.data.length - 20];
         } else {
+            // solhint-disable-next-line erc2771/no-msg-data
             return msg.data;
         }
     }
 
-    function _isTrustedForwarder(address forwarder) internal pure returns (bool) {
-        return forwarder == trustedForwarder();
+    function isTrustedForwarder(address forwarder) internal pure returns (bool) {
+        return forwarder == TRUSTED_FORWARDER;
     }
 
     function trustedForwarder() internal pure returns (address) {
-        return 0x04A11fC66B8eFC746F395E446d69e9bfAC5B83Bb;
+        return TRUSTED_FORWARDER;
     }
 }
