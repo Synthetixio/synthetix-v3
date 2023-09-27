129cf5f9d670e45ef9889634fc6bbba488352192
Leonardo Massazza <lmassazza@gmail.com>
2023-09-27 14:12:05 -0300

    add missing tests to GlobalPerpsMarketModule

--- markets/perps-market/test/integration/Markets/GlobalPerpsMarket.test.ts
+++ markets/perps-market/test/integration/Markets/GlobalPerpsMarket.test.ts
@@ -1,11 +1,12 @@
-import { BigNumber } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
 import { bn, bootstrapMarkets } from '../bootstrap';
 import assertBn from '@synthetixio/core-utils/src/utils/assertions/assert-bignumber';
 import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
 import assert from 'assert';
+import { wei } from '@synthetixio/wei';
 
 describe('GlobalPerpsMarket', () => {
-  const { systems, perpsMarkets, trader1, superMarketId, owner } = bootstrapMarkets({
+  const { systems, perpsMarkets, signers, trader1, superMarketId, owner } = bootstrapMarkets({
     synthMarkets: [{ name: 'Ether', token: 'snxETH', buyPrice: bn(1000), sellPrice: bn(1000) }],
     perpsMarkets: [
       { requestedMarketId: 25, name: 'Ether', token: 'snxETH', price: bn(1000) },
@@ -78,6 +79,56 @@ describe('GlobalPerpsMarket', () => {
       assertBn.equal(markets.length, 2);
       assertBn.equal(markets[0], perpsMarkets()[0].marketId());
       assertBn.equal(markets[1], perpsMarkets()[1].marketId());
+    });
+  });
+
+  describe('global settings are correct', () => {
+    const referrerRatio = wei(0.1); // 10%
+    let referrer: ethers.Signer;
+
+    before('identify referrer', () => {
+      referrer = signers()[8];
+    });
+
+    before('set fee collector and referral', async () => {
+      await systems()
+        .PerpsMarket.connect(owner())
+        .setFeeCollector(systems().FeeCollectorMock.address);
+      await systems()
+        .PerpsMarket.connect(owner())
+        .updateReferrerShare(await referrer.getAddress(), referrerRatio.toBN()); // 10%
+    });
+
+    it('reverts attempting to set an invalid fee collector', async () => {
+      await assertRevert(
+        systems()
+          .PerpsMarket.connect(owner())
+          .setFeeCollector(await referrer.getAddress()),
+        `InvalidFeeCollectorInterface("${await referrer.getAddress()}")`
+      );
+    });
+
+    it('fee collector is set correctly', async () => {
+      assert.equal(
+        await systems().PerpsMarket.getFeeCollector(),
+        systems().FeeCollectorMock.address
+      );
+    });
+
+    it('referrer share is set correctly', async () => {
+      assertBn.equal(
+        await systems().PerpsMarket.getReferrerShare(await referrer.getAddress()),
+        referrerRatio.toBN()
+      );
+      assertBn.equal(await systems().PerpsMarket.getReferrerShare(ethers.constants.AddressZero), 0);
+    });
+
+    it('per account caps are set correctly', async () => {
+      const { maxPositionsPerAccount, maxCollateralsPerAccount } =
+        await systems().PerpsMarket.getPerAccountCaps();
+      // check defaults set in bootstrap
+      assertBn.equal(maxPositionsPerAccount, 100_000);
+      assertBn.equal(maxCollateralsPerAccount, 100_000);
     });
   });
 });
