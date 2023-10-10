648b9ebe2922a8334919be4b9556f0aed0ab61ea
Peiman <peymannaderi3@yahoo.com>
2023-10-10 16:43:41 +0330

    feat: account view functions

--- markets/perps-market/contracts/interfaces/IPerpsAccountModule.sol
+++ markets/perps-market/contracts/interfaces/IPerpsAccountModule.sol
@@ -44,6 +44,20 @@ interface IPerpsAccountModule {
     ) external view returns (uint256);
 
     /**
+     * @notice Gets the account's active synth ids
+     * @param accountId Id of the account.
+     */
+    function getAccountActiveCollaterals(
+        uint128 accountId
+    ) external view returns (uint256[] memory);
+
+    /**
+     * @notice Gets all markets that a given account id has a position in
+     * @param accountId Id of the account.
+     */
+    function getOpenPositionMarketIds(uint128 accountId) external view returns (uint256[] memory);
+
+    /**
      * @notice Gets the account's total collateral value.
      * @param accountId Id of the account.
      * @return collateralValue total collateral value of the account. USD denominated.
--- markets/perps-market/contracts/modules/PerpsAccountModule.sol
+++ markets/perps-market/contracts/modules/PerpsAccountModule.sol
@@ -178,6 +178,24 @@ contract PerpsAccountModule is IPerpsAccountModule {
         return PerpsAccount.load(accountId).collateralAmounts[synthMarketId];
     }
 
+    /**
+     * @inheritdoc IPerpsAccountModule
+     */
+    function getAccountActiveCollaterals(
+        uint128 accountId
+    ) external view override returns (uint256[] memory) {
+        return PerpsAccount.load(accountId).activeCollateralTypes.values();
+    }
+
+    /**
+     * @inheritdoc IPerpsAccountModule
+     */
+    function getOpenPositionMarketIds(
+        uint128 accountId
+    ) external view override returns (uint256[] memory) {
+        return PerpsAccount.load(accountId).openPositionMarketIds.values();
+    }
+
     function _depositMargin(
         PerpsMarketFactory.Data storage perpsMarketFactory,
         uint128 perpsMarketId,
