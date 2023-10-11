ce85cc28190dc760efd9a795cd4d2c7d1d1a0f0e
Matías <mjlescano@protonmail.com>
2023-10-11 15:31:55 -0300

    add ccip router

--- utils/core-modules/test/integration/CrossChain.test.ts
+++ utils/core-modules/test/integration/CrossChain.test.ts
@@ -1,10 +1,13 @@
+import { AnvilServer } from '@foundry-rs/hardhat-anvil/dist/src/anvil-server';
 import { ethers } from 'ethers';
-import { CcipRouterMock } from '../../typechain-types/contracts/mocks/CcipRouterMock';
 import { CcipRouterMock__factory } from '../../typechain-types/factories/contracts/mocks/CcipRouterMock__factory';
 import { launchAnvil } from './helpers/anvil';
 
+import type { CcipRouterMock } from '../../typechain-types/contracts/mocks/CcipRouterMock';
+
 interface Chain {
   chainId: number;
+  server: AnvilServer;
   provider: ethers.providers.JsonRpcProvider;
   ccipRouter: CcipRouterMock;
 }
@@ -20,21 +23,33 @@ describe('CrossChain', function () {
 
     for (const chainId of [1, 10]) {
       const anvil = await launchAnvil({ chainId });
-      const { provider } = anvil;
-
+      const { server, provider } = anvil;
       const signer = provider.getSigner(0);
-      const router = await factory.connect(signer).deploy();
-      await router.deployTransaction.wait();
+
+      // Deploy CcipRouterMock
+      const ccipRouter = (await factory.connect(signer).deploy()) as CcipRouterMock;
+      await ccipRouter.deployTransaction.wait();
 
       chains[chainId] = {
         chainId,
+        server,
         provider,
-        ccipRouter: router as CcipRouterMock,
+        ccipRouter,
       };
     }
   });
 
+  after('stop chain servers', async function () {
+    await Promise.all(
+      Object.values(chains).map(async (chain) => {
+        await chain.server.kill();
+        await chain.server.waitUntilClosed();
+      })
+    );
+  });
+
   it('do', async function () {
     console.log(chains);
+    throw 1;
   });
 });
