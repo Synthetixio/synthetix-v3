185fb18fa284412d1365bd9d9992b70222a95c8d
jmzwar <james@jmzwar.com>
2023-10-24 11:42:16 +0200

    add rd

---
+++ src/RewardsDistributor.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {IRewardsManagerModule} from "lib/synthetix-v3/protocol/synthetix/contracts/interfaces/IRewardsManagerModule.sol"; 
+import {IRewardDistributor} from "lib/synthetix-v3/protocol/synthetix/contracts/interfaces/external/IRewardDistributor.sol";
+import {AccessError} from "lib/synthetix-v3/utils/core-contracts/contracts/errors/AccessError.sol";
+import {IERC20} from "lib/synthetix-v3/utils/core-contracts/contracts/interfaces/IERC20.sol";
+
+contract RewardsDistributor is IRewardDistributor {
+    address private _rewardManager;
+    address private _token;
+    string private _name;
+
+    bool public shouldFailPayout;
+
+    constructor(address rewardManager, address token_, string memory name_) {
+        _rewardManager = rewardManager;
+        _token = token_;
+        _name = name_;
+    }
+
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    function token() public view override returns (address) {
+        return _token;
+    }
+
+    function setShouldFailPayout(bool fail) external {
+        shouldFailPayout = fail;
+    }
+
+    function payout(
+        uint128,
+        uint128,
+        address,
+        address sender,
+        uint256 amount
+    ) external returns (bool) {
+        // IMPORTANT: In production, this function should revert if msg.sender is not the Synthetix CoreProxy address.
+        if (msg.sender != _rewardManager) {
+            revert AccessError.Unauthorized(msg.sender);
+        }
+        IERC20(_token).transfer(sender, amount);
+        return !shouldFailPayout;
+    }
+
+    function distributeRewards(
+        uint128 poolId,
+        address collateralType,
+        uint256 amount,
+        uint64 start,
+        uint32 duration
+    ) public {
+        IRewardsManagerModule(_rewardManager).distributeRewards(
+            poolId,
+            collateralType,
+            amount,
+            start,
+            duration
+        );
+    }
+
+    function onPositionUpdated(uint128, uint128, address, uint256) external pure {
+        return;
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(IERC165) returns (bool) {
+        return
+            interfaceId == type(IRewardDistributor).interfaceId ||
+            interfaceId == this.supportsInterface.selector;
+    }
+}
