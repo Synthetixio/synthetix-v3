19b9976aebbcd3ee2b98e84604892566f7057063
Daniel Beal <git@dbeal.dev>
2023-10-27 18:45:26 +0900

    optimize contracts
    wont affect proxy contracts becuase they are exclusively locked by the common config

--- utils/common-config/hardhat.config.ts
+++ utils/common-config/hardhat.config.ts
@@ -18,11 +18,25 @@ dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') });
 
 const config = {
   solidity: {
-    version: '0.8.17',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    compilers: [
+      {
+        version: '0.8.22',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+    overrides: {
+      'contracts/Proxy.sol': {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: false,
+          },
+        },
       },
     },
   },
