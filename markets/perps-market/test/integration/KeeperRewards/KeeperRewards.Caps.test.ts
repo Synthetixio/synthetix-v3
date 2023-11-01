fd0e7ddd37a92bcc238d7f653a593bc991a722f4
Leonardo Massazza <lmassazza@gmail.com>
2023-11-01 15:00:40 -0300

    checkpoint: caps

--- markets/perps-market/test/integration/KeeperRewards/KeeperRewards.Caps.test.ts
+++ markets/perps-market/test/integration/KeeperRewards/KeeperRewards.Caps.test.ts
@@ -85,54 +85,130 @@ describe.only('Keeper Rewards - Caps', () => {
     assertBn.equal(await keeperCostOracleNode().elegibilityCost(), KeeperCosts.elegibilityCost);
   });
 
-  describe('uncapped', () => {
-    let liquidateTxn: ethers.providers.TransactionResponse;
-    before(restoreToConfiguration);
+  [
+    {
+      name: 'uncapped just cost',
+      withRatio: false,
+      lowerCap: 1,
+      higherCap: bn(10),
+      expected: KeeperCosts.flagCost + KeeperCosts.liquidateCost,
+    },
+    {
+      name: 'lower capped just cost',
+      withRatio: false,
+      lowerCap: 10_000,
+      higherCap: 10_0000,
+      expected: 10_000,
+    },
+    {
+      name: 'higher capped just cost',
+      withRatio: false,
+      lowerCap: 1,
+      higherCap: 100,
+      expected: 100,
+    },
+    {
+      name: 'uncapped plus reward ratio',
+      withRatio: true,
+      lowerCap: 1,
+      higherCap: bn(10),
+      expected: bn(5).add(KeeperCosts.flagCost + KeeperCosts.liquidateCost),
+    },
+    {
+      name: 'lower capped plus reward ratio',
+      withRatio: true,
+      lowerCap: bn(8),
+      higherCap: bn(10),
+      expected: bn(8),
+    },
+    {
+      name: 'higher capped plus reward ratio',
+      withRatio: true,
+      lowerCap: bn(1),
+      higherCap: bn(3),
+      expected: bn(3),
+    },
+  ].forEach((test) => {
+    describe(`${test.name}`, () => {
+      let liquidateTxn: ethers.providers.TransactionResponse;
+      before(restoreToConfiguration);
 
-    before('set minLiquidationRewardUsd, maxLiquidationRewardUsd', async () => {
-      await systems().PerpsMarket.setLiquidationRewardGuards(1, 10_000);
-    });
-
-    before('add collateral', async () => {
-      await depositCollateral(collateralsTestCase[0].collateralData);
-    });
-
-    before('open position', async () => {
-      await openPosition({
-        systems,
-        provider,
-        trader: trader1(),
-        accountId: 2,
-        keeper: keeper(),
-        marketId: ethMarketId,
-        sizeDelta: bn(100),
-        settlementStrategyId: ethSettlementStrategyId,
-        price: bn(1000),
+      before('set minLiquidationRewardUsd, maxLiquidationRewardUsd', async () => {
+        await systems().PerpsMarket.setLiquidationRewardGuards(test.lowerCap, test.higherCap);
       });
-    });
 
-    before('lower price to liquidation', async () => {
-      await perpsMarkets()[0].aggregator().mockSetCurrentPrice(bn(1));
-    });
+      before('set liquidation reward ratio', async () => {
+        if (test.withRatio) {
+          const initialMarginFraction = bn(0);
+          const maintenanceMarginScalar = bn(0);
+          const minimumInitialMarginRatio = bn(0);
+          const liquidationRewardRatio = bn(0.05); // 100 * 0.05 = 5
+          const minimumPositionMargin = bn(0);
+          // const maxLiquidationLimitAccumulationMultiplier = bn(1);
+          // const maxSecondsInLiquidationWindow = ethers.BigNumber.from(10);
+          await systems()
+            .PerpsMarket.connect(owner())
+            .setLiquidationParameters(
+              ethMarketId,
+              initialMarginFraction,
+              maintenanceMarginScalar,
+              minimumInitialMarginRatio,
+              liquidationRewardRatio,
+              minimumPositionMargin
+            );
+          // await systems()
+          //   .PerpsMarket.connect(owner())
+          //   .setMaxLiquidationParameters(
+          //     ethMarketId,
+          //     maxLiquidationLimitAccumulationMultiplier,
+          //     maxSecondsInLiquidationWindow,
+          //     0,
+          //     ethers.constants.AddressZero
+          //   );
+        }
+      });
 
-    before('liquidate account', async () => {
-      liquidateTxn = await systems().PerpsMarket.connect(keeper()).liquidate(2);
-    });
+      before('add collateral', async () => {
+        await depositCollateral(collateralsTestCase[0].collateralData);
+      });
 
-    it('emits position liquidated event', async () => {
-      await assertEvent(
-        liquidateTxn,
-        `PositionLiquidated(2, 25, ${bn(100)}, 0)`,
-        systems().PerpsMarket
-      );
-    });
+      before('open position', async () => {
+        await openPosition({
+          systems,
+          provider,
+          trader: trader1(),
+          accountId: 2,
+          keeper: keeper(),
+          marketId: ethMarketId,
+          sizeDelta: bn(100),
+          settlementStrategyId: ethSettlementStrategyId,
+          price: bn(1000),
+        });
+      });
 
-    it('emits account liquidated event', async () => {
-      await assertEvent(
-        liquidateTxn,
-        `AccountLiquidated(2, ${KeeperCosts.flagCost + KeeperCosts.liquidateCost}, true)`, // not capped
-        systems().PerpsMarket
-      );
+      before('lower price to liquidation', async () => {
+        await perpsMarkets()[0].aggregator().mockSetCurrentPrice(bn(1));
+      });
+
+      before('liquidate account', async () => {
+        liquidateTxn = await systems().PerpsMarket.connect(keeper()).liquidate(2);
+      });
+
+      it('emits position liquidated event', async () => {
+        await assertEvent(
+          liquidateTxn,
+          `PositionLiquidated(2, 25, ${bn(100)}, 0)`,
+          systems().PerpsMarket
+        );
+      });
+
+      it('emits account liquidated event', async () => {
+        await assertEvent(
+          liquidateTxn,
+          `AccountLiquidated(2, ${test.expected}, true)`, // not capped
+          systems().PerpsMarket
+        );
+      });
     });
   });
 });
