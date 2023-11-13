7b40e5f5a390b22803f855af2fa61ce5efaf957f
joey <joey@cc.snxdao.io>
2023-11-13 13:59:58 +1100

    Add method to get total collateral amount given a id across all markets

--- contracts/storage/GlobalPerpMarket.sol
+++ contracts/storage/GlobalPerpMarket.sol
@@ -1,4 +1,7 @@
+//SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
+
+import {PerpMarket} from "./PerpMarket.sol";
 
 library GlobalPerpMarket {
     struct Data {
@@ -13,5 +16,23 @@ library GlobalPerpMarket {
         assembly {
             d.slot := s
         }
+    }
+
+    function getTotalCollateralAmounts(
+        GlobalPerpMarket.Data storage self,
+        uint128 synthMarketId
+    ) internal view returns (uint256) {
+        uint128[] memory activeMarketIds = self.activeMarketIds;
+
+        uint256 activeMarketIdsLength = activeMarketIds.length;
+        uint256 totalCollateralAmount;
+        // Accumulate collateral amounts for active markets
+        for (uint256 i = 0; i < activeMarketIdsLength; i++) {
+            uint128 marketId = activeMarketIds[i];
+            PerpMarket.Data storage market = PerpMarket.load(marketId);
+
+            totalCollateralAmount += market.depositedCollateral[synthMarketId];
+        }
+        return totalCollateralAmount;
     }
 }
