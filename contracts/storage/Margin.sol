f235f8593fe66f22edf5544a9069769e37b2f6c7
David Vuong <david.vuong256@gmail.com>
2023-11-20 09:38:57 +1100

    chore: updated Margin.sol pragma

--- contracts/storage/Margin.sol
+++ contracts/storage/Margin.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >=0.8.11 <0.9.0;
+pragma solidity 0.8.19;
 
 import {DecimalMath} from "@synthetixio/core-contracts/contracts/utils/DecimalMath.sol";
 import {ITokenModule} from "@synthetixio/core-modules/contracts/interfaces/ITokenModule.sol";
