f44fefef7010b13e6b3c7f9a6f151678755c6134
Daniel Beal <git@dbeal.dev>
2023-11-20 22:44:41 +0900

    format everything

--- src/RewardsDistributor.sol
+++ src/RewardsDistributor.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IRewardsManagerModule} from "@synthetixio/main/contracts/interfaces/IRewardsManagerModule.sol"; 
+import {IRewardsManagerModule} from "@synthetixio/main/contracts/interfaces/IRewardsManagerModule.sol";
 import {IRewardDistributor} from "@synthetixio/main/contracts/interfaces/external/IRewardDistributor.sol";
 import {AccessError} from "@synthetixio/core-contracts/contracts/errors/AccessError.sol";
 import {IERC20} from "@synthetixio/core-contracts/contracts/interfaces/IERC20.sol";
@@ -32,13 +32,7 @@ contract RewardsDistributor is IRewardDistributor {
         shouldFailPayout = fail;
     }
 
-    function payout(
-        uint128,
-        uint128,
-        address,
-        address sender,
-        uint256 amount
-    ) external returns (bool) {
+    function payout(uint128, uint128, address, address sender, uint256 amount) external returns (bool) {
         // IMPORTANT: In production, this function should revert if msg.sender is not the Synthetix CoreProxy address.
         if (msg.sender != _rewardManager) {
             revert AccessError.Unauthorized(msg.sender);
@@ -47,20 +41,10 @@ contract RewardsDistributor is IRewardDistributor {
         return !shouldFailPayout;
     }
 
-    function distributeRewards(
-        uint128 poolId,
-        address collateralType,
-        uint256 amount,
-        uint64 start,
-        uint32 duration
-    ) public {
-        IRewardsManagerModule(_rewardManager).distributeRewards(
-            poolId,
-            collateralType,
-            amount,
-            start,
-            duration
-        );
+    function distributeRewards(uint128 poolId, address collateralType, uint256 amount, uint64 start, uint32 duration)
+        public
+    {
+        IRewardsManagerModule(_rewardManager).distributeRewards(poolId, collateralType, amount, start, duration);
     }
 
     function onPositionUpdated(uint128, uint128, address, uint256) external pure {
@@ -70,11 +54,7 @@ contract RewardsDistributor is IRewardDistributor {
     /**
      * @dev See {IERC165-supportsInterface}.
      */
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view virtual override(IERC165) returns (bool) {
-        return
-            interfaceId == type(IRewardDistributor).interfaceId ||
-            interfaceId == this.supportsInterface.selector;
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165) returns (bool) {
+        return interfaceId == type(IRewardDistributor).interfaceId || interfaceId == this.supportsInterface.selector;
     }
 }
