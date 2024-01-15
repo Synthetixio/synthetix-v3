21e5945ef698c34c5018e94ef3737a3748c32026
David Vuong <david.vuong256@gmail.com>
2024-01-15 16:00:45 +1100

    test: Add more PythWstEthNode tests

--- test/integration/oracles/PythWstEthNode.test.ts
+++ test/integration/oracles/PythWstEthNode.test.ts
@@ -1,13 +1,7 @@
-import assert from 'assert';
 import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber';
-import assertEvent from '@synthetixio/core-utils/utils/assertions/assert-event';
-import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
-import Wei, { wei } from '@synthetixio/wei';
-import { BigNumber, Signer, ethers, utils } from 'ethers';
-import forEach from 'mocha-each';
+import { wei } from '@synthetixio/wei';
 import { bootstrap } from '../../bootstrap';
-import { bn, genBootstrap, genOneOf } from '../../generators';
-import { assertEvents } from '../../assert';
+import { bn, genBootstrap, genNumber } from '../../generators';
 import { getPythPriceData } from '../../helpers';
 
 describe.only('PythWstEthNode', () => {
@@ -17,6 +11,28 @@ describe.only('PythWstEthNode', () => {
   beforeEach(restore);
 
   describe('process', () => {
+    const configureExchangeRatios = async (desiredWstEthToEth?: number, desiredStEthToEth?: number) => {
+      const { WstETHMock, StEthToEthMock } = systems();
+
+      const wstEthToEth = bn(desiredWstEthToEth ?? genNumber(1.1, 1.2));
+      const stEthToETh = bn(desiredStEthToEth ?? genNumber(0.98, 1.02));
+
+      await WstETHMock.mockSetWstEthToStEthRatio(wstEthToEth);
+      await StEthToEthMock.mockSetCurrentPrice(stEthToETh);
+
+      return { wstEthToEth, stEthToETh };
+    };
+
+    const getMarketMetadata = async () => {
+      const { PerpMarketProxy } = systems();
+
+      const market = markets()[1]; // ETHPERP.
+      const marketId = market.marketId();
+      const priceFeedId = (await PerpMarketProxy.getMarketConfigurationById(marketId)).pythPriceFeedId;
+
+      return { market, marketId, priceFeedId };
+    };
+
     it('should compute zero price when no prices available', async () => {
       const { OracleManager } = systems();
       const { pythWstEthNodeId } = extras();
@@ -24,34 +40,33 @@ describe.only('PythWstEthNode', () => {
       assertBn.isZero(price);
     });
 
-    it.only('should compute an accurate wstETH price', async () => {
-      const { PerpMarketProxy, OracleManager, WstETHMock, StEthToEthMock, PythMock } = systems();
+    it('should compute a wstETH price (concrete)', async () => {
+      const { OracleManager, PythMock } = systems();
       const { pythWstEthNodeId } = extras();
 
-      const market = markets()[1]; // ETHPERP.
-      const marketId = market.marketId();
-
-      const wstEthToEth = bn(1.15326952);
-      const stEthToETh = bn(0.99);
-
-      await WstETHMock.mockSetWstEthToStEthRatio(wstEthToEth);
-      await StEthToEthMock.mockSetCurrentPrice(stEthToETh);
-
-      // Perform a ETH price update (Pyth)
-      // Perform a stETH<>wstETH exchange rate update (Lido)
-      // Perform a stETH<>ETH exchange rate update (CL)
-
-      // Ensure the math works out correctly e.g.
-      //
-      // ETH price is 2000
-      // stETH<>wstETH is 0.8
-      // stETH<>ETH is 0.99
-      //
-      // Then, 2000 / 0.99 / 0.8 = 2525.25252525 (per wstETH)
+      const { wstEthToEth, stEthToETh } = await configureExchangeRatios(1.153, 0.99);
+      const { priceFeedId } = await getMarketMetadata();
 
       const ethPrice = 2586;
-      const priceFeedId = (await PerpMarketProxy.getMarketConfigurationById(marketId)).pythPriceFeedId;
+      const { updateData, updateFee } = await getPythPriceData(bs, ethPrice, priceFeedId);
+      await PythMock.updatePriceFeeds([updateData], { value: updateFee });
 
+      // 2586 * 1.153 * 0.99 = 2951.84142 wstETH
+      const { price } = await OracleManager.process(pythWstEthNodeId);
+      const expectedPrice = wei(ethPrice).mul(stEthToETh).mul(wstEthToEth).toBN();
+
+      assertBn.near(price, expectedPrice, bn(0.000001));
+    });
+
+    // Same as the compute test above but with random data.
+    it('should compute a wstETH price', async () => {
+      const { OracleManager, PythMock } = systems();
+      const { pythWstEthNodeId } = extras();
+
+      const { wstEthToEth, stEthToETh } = await configureExchangeRatios();
+      const { priceFeedId } = await getMarketMetadata();
+
+      const ethPrice = genNumber(1800, 10_000);
       const { updateData, updateFee } = await getPythPriceData(bs, ethPrice, priceFeedId);
       await PythMock.updatePriceFeeds([updateData], { value: updateFee });
 
@@ -61,15 +76,54 @@ describe.only('PythWstEthNode', () => {
       assertBn.near(price, expectedPrice, bn(0.000001));
     });
 
-    it('should be zero if the Pyth price is zero');
+    it('should be zero if the Pyth price is zero', async () => {
+      const { OracleManager, PythMock } = systems();
+      const { pythWstEthNodeId } = extras();
+
+      await configureExchangeRatios();
+      const { priceFeedId } = await getMarketMetadata();
+
+      const ethPrice = 0;
+      const { updateData, updateFee } = await getPythPriceData(bs, ethPrice, priceFeedId);
+      await PythMock.updatePriceFeeds([updateData], { value: updateFee });
+
+      const { price } = await OracleManager.process(pythWstEthNodeId);
+      assertBn.isZero(price);
+    });
+
+    it('should revert if wstETH<>stETH exchange is zero', async () => {
+      const { OracleManager, PythMock } = systems();
+      const { pythWstEthNodeId } = extras();
+
+      await configureExchangeRatios(0, undefined);
+      const { priceFeedId } = await getMarketMetadata();
+
+      const ethPrice = genNumber(1800, 10_000);
+      const { updateData, updateFee } = await getPythPriceData(bs, ethPrice, priceFeedId);
+      await PythMock.updatePriceFeeds([updateData], { value: updateFee });
+
+      const { price } = await OracleManager.process(pythWstEthNodeId);
+      assertBn.isZero(price);
+    });
+
+    it('should revert if stETH<>ETH exchange rate is zero', async () => {
+      const { OracleManager, PythMock } = systems();
+      const { pythWstEthNodeId } = extras();
+
+      await configureExchangeRatios(undefined, 0);
+      const { priceFeedId } = await getMarketMetadata();
+
+      const ethPrice = genNumber(1800, 10_000);
+      const { updateData, updateFee } = await getPythPriceData(bs, ethPrice, priceFeedId);
+      await PythMock.updatePriceFeeds([updateData], { value: updateFee });
+
+      const { price } = await OracleManager.process(pythWstEthNodeId);
+      assertBn.isZero(price);
+    });
 
     it('should sell collateral on a losing pnl settlement with Pyth PythWstEthNode');
 
     // Not sure if this can ever occur if Spot market Synth oracle is the same as the Perp wstETH collateral...
     it('should revert on sale of collateral when Pyth oracle price is stale');
-
-    it('should revert if stETH<>wstETH exchange is zero');
-
-    it('should revert if stETH<>ETH exchange rate is zero');
   });
 });
