c001af0a235acb3f187739f644972624b918c9bd
joey <joey@cc.snxdao.io>
2024-01-18 10:29:34 +1100

    Emit UtilizationRecomputed and dont call oraclePrice() twice

--- contracts/modules/MarginModule.sol
+++ contracts/modules/MarginModule.sol
@@ -166,9 +166,10 @@ contract MarginModule is IMarginModule {
         if (position.size != 0) {
             revert ErrorUtil.PositionFound(accountId, marketId);
         }
-        uint oraclePrice = market.getOraclePrice();
+        uint256 oraclePrice = market.getOraclePrice();
         (int256 fundingRate, ) = market.recomputeFunding(oraclePrice);
         emit FundingRecomputed(marketId, market.skew, fundingRate, market.getCurrentFundingVelocity());
+
         (uint256 utilizationRate, ) = market.recomputeUtilization(oraclePrice);
         emit UtilizationRecomputed(marketId, market.skew, utilizationRate);
 
@@ -239,9 +240,12 @@ contract MarginModule is IMarginModule {
         Margin.Data storage accountMargin = Margin.load(accountId, marketId);
         uint256 absAmountDelta = MathUtil.abs(amountDelta);
 
-        (int256 fundingRate, ) = market.recomputeFunding(market.getOraclePrice());
+        uint256 oraclePrice = market.getOraclePrice();
+        (int256 fundingRate, ) = market.recomputeFunding(oraclePrice);
         emit FundingRecomputed(marketId, market.skew, fundingRate, market.getCurrentFundingVelocity());
-        market.recomputeUtilization(market.getOraclePrice());
+
+        (uint256 utilizationRate, ) = market.recomputeUtilization(oraclePrice);
+        emit UtilizationRecomputed(marketId, market.skew, utilizationRate);
 
         // > 0 is a deposit whilst < 0 is a withdrawal.
         if (amountDelta > 0) {
