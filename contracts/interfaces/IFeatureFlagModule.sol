533dc883c5d258c0f4ea3fd50251e7b7054fb716
joey <joey@cc.snxdao.io>
2024-02-02 15:45:41 +1100

    Refactor enable/suspend all and add docs

--- contracts/interfaces/IFeatureFlagModule.sol
+++ contracts/interfaces/IFeatureFlagModule.sol
@@ -3,7 +3,12 @@ pragma solidity 0.8.19;
 import {IFeatureFlagModule as IBaseFeatureFlagModule} from "@synthetixio/core-modules/contracts/interfaces/IFeatureFlagModule.sol";
 
 interface IFeatureFlagModule is IBaseFeatureFlagModule {
+    // @notice Emitted when all features get suspended or enabled.
+    event PerpMarketSuspended(bool suspended);
+
+    // @notice Suspends all features. Can be called by owner or a configured "denier"
     function suspendAllFeatures() external;
 
+    // @notice Enable all features. Can be called by owner
     function enableAllFeatures() external;
 }
--- contracts/modules/FeatureFlagModule.sol
+++ contracts/modules/FeatureFlagModule.sol
@@ -3,61 +3,68 @@ pragma solidity 0.8.19;
 
 import {FeatureFlagModule as BaseFeatureFlagModule} from "@synthetixio/core-modules/contracts/modules/FeatureFlagModule.sol";
 import {IFeatureFlagModule} from "../interfaces/IFeatureFlagModule.sol";
-import {IFeatureFlagModule as IBaseFeatureFlagModule} from "@synthetixio/core-modules/contracts/interfaces/IFeatureFlagModule.sol";
 
 import {FeatureFlag} from "@synthetixio/core-modules/contracts/storage/FeatureFlag.sol";
 import {OwnableStorage} from "@synthetixio/core-contracts/contracts/ownership/OwnableStorage.sol";
 import {Flags} from "../utils/Flags.sol";
 
-contract FeatureFlagModule is BaseFeatureFlagModule {
+contract FeatureFlagModule is IFeatureFlagModule, BaseFeatureFlagModule {
     using FeatureFlag for FeatureFlag.Data;
 
-    // TODO remove this when this method is implemented in @synthetixio/core-modules/contracts/storage/FeatureFlag.sol
-    function _setFeatureFlagAllowAll(bytes32 feature, bool allowAll) internal {
-        OwnableStorage.onlyOwner();
-        FeatureFlag.load(feature).allowAll = allowAll;
-
-        if (allowAll) {
-            FeatureFlag.load(feature).denyAll = false;
-        }
-
-        emit IBaseFeatureFlagModule.FeatureFlagAllowAllSet(feature, allowAll);
-    }
-
-    // TODO remove this when this method is implemented in @synthetixio/core-modules/contracts/storage/FeatureFlag.sol
-    function _setFeatureFlagDenyAll(bytes32 feature, bool denyAll) internal {
+    /**
+     * @dev Allow all addresses to use a feature. This can be called by the owner.
+     */
+    function enableFeature(bytes32 feature) internal {
         FeatureFlag.Data storage flag = FeatureFlag.load(feature);
 
-        if (!denyAll || !flag.isDenier(msg.sender)) {
+        flag.allowAll = true;
+        flag.denyAll = false;
+    }
+
+    /**
+     * @dev Deny all addresses to use a feature. This can be called by a "denier" or the owner.
+     */
+    function suspendFeature(bytes32 feature) internal {
+        FeatureFlag.Data storage flag = FeatureFlag.load(feature);
+
+        if (!flag.isDenier(msg.sender)) {
             OwnableStorage.onlyOwner();
         }
 
-        flag.denyAll = denyAll;
-
-        emit FeatureFlagDenyAllSet(feature, denyAll);
+        flag.denyAll = true;
     }
 
+    /**
+     * @inheritdoc IFeatureFlagModule
+     */
     function suspendAllFeatures() external {
-        OwnableStorage.onlyOwner();
-        _setFeatureFlagDenyAll(Flags.CREATE_ACCOUNT, true);
-        _setFeatureFlagDenyAll(Flags.DEPOSIT, true);
-        _setFeatureFlagDenyAll(Flags.WITHDRAW, true);
-        _setFeatureFlagDenyAll(Flags.COMMIT_ORDER, true);
-        _setFeatureFlagDenyAll(Flags.SETTLE_ORDER, true);
-        _setFeatureFlagDenyAll(Flags.CANCEL_ORDER, true);
-        _setFeatureFlagDenyAll(Flags.FLAG_POSITION, true);
-        _setFeatureFlagDenyAll(Flags.LIQUIDATE_POSITION, true);
+        suspendFeature(Flags.CREATE_ACCOUNT);
+        suspendFeature(Flags.DEPOSIT);
+        suspendFeature(Flags.WITHDRAW);
+        suspendFeature(Flags.COMMIT_ORDER);
+        suspendFeature(Flags.SETTLE_ORDER);
+        suspendFeature(Flags.CANCEL_ORDER);
+        suspendFeature(Flags.FLAG_POSITION);
+        suspendFeature(Flags.LIQUIDATE_POSITION);
+
+        emit PerpMarketSuspended(true);
     }
 
+    /**
+     * @inheritdoc IFeatureFlagModule
+     */
     function enableAllFeatures() external {
         OwnableStorage.onlyOwner();
-        _setFeatureFlagAllowAll(Flags.CREATE_ACCOUNT, true);
-        _setFeatureFlagAllowAll(Flags.DEPOSIT, true);
-        _setFeatureFlagAllowAll(Flags.WITHDRAW, true);
-        _setFeatureFlagAllowAll(Flags.COMMIT_ORDER, true);
-        _setFeatureFlagAllowAll(Flags.SETTLE_ORDER, true);
-        _setFeatureFlagAllowAll(Flags.CANCEL_ORDER, true);
-        _setFeatureFlagAllowAll(Flags.FLAG_POSITION, true);
-        _setFeatureFlagAllowAll(Flags.LIQUIDATE_POSITION, true);
+
+        enableFeature(Flags.CREATE_ACCOUNT);
+        enableFeature(Flags.DEPOSIT);
+        enableFeature(Flags.WITHDRAW);
+        enableFeature(Flags.COMMIT_ORDER);
+        enableFeature(Flags.SETTLE_ORDER);
+        enableFeature(Flags.CANCEL_ORDER);
+        enableFeature(Flags.FLAG_POSITION);
+        enableFeature(Flags.LIQUIDATE_POSITION);
+
+        emit PerpMarketSuspended(false);
     }
 }
