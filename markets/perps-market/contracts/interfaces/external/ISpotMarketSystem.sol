54d245e90772a661f2b69b4842c83c0eea42f87e
Sunny Vempati <5723490+sunnyvempati@users.noreply.github.com>
2024-02-15 09:20:45 -0600

    wip

--- markets/perps-market/contracts/interfaces/external/ISpotMarketSystem.sol
+++ markets/perps-market/contracts/interfaces/external/ISpotMarketSystem.sol
@@ -3,6 +3,11 @@ pragma solidity >=0.8.11 <0.9.0;
 
 import {IAtomicOrderModule} from "@synthetixio/spot-market/contracts/interfaces/IAtomicOrderModule.sol";
 import {ISpotMarketFactoryModule} from "@synthetixio/spot-market/contracts/interfaces/ISpotMarketFactoryModule.sol";
+import {IMarketConfigurationModule} from "@synthetixio/spot-market/contracts/interfaces/IMarketConfigurationModule.sol";
 
 // solhint-disable-next-line no-empty-blocks
-interface ISpotMarketSystem is IAtomicOrderModule, ISpotMarketFactoryModule {}
+interface ISpotMarketSystem is
+    IAtomicOrderModule,
+    ISpotMarketFactoryModule,
+    IMarketConfigurationModule
+{}
