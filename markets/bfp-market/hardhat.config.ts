e57d291533ed1b8f5dda0f91849db7e700525501
David Vuong <david.vuong256@gmail.com>
2024-03-02 17:14:37 -0700

    chore: add more deps and incl docgen

--- markets/bfp-market/hardhat.config.ts
+++ markets/bfp-market/hardhat.config.ts
@@ -1,8 +1,24 @@
 import commonConfig from '@synthetixio/common-config/hardhat.config';
 
+import 'solidity-docgen';
+import { templates } from '@synthetixio/docgen';
+
 const config = {
   ...commonConfig,
   allowUnlimitedContractSize: true,
+  docgen: {
+    exclude: [
+      './interfaces/external',
+      './modules',
+      './mixins',
+      './mocks',
+      './utils',
+      './storage',
+      './Proxy.sol',
+      './Router.sol',
+    ],
+    templates,
+  },
   mocha: {
     timeout: 30_000,
   },
