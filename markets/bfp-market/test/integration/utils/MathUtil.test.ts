a33b47a29d9a5b3d793150141b4b527960849463
David Vuong <david.vuong256@gmail.com>
2024-03-06 11:08:37 -0700

    rm: delete empty MathUtil test

--- markets/bfp-market/test/integration/utils/MathUtil.test.ts
+++
@@ -1,8 +0,0 @@
-// TODO: Need a mock + setup to test the MathUtil library.
-describe('MathUtil', () => {
-  describe('abs', () => {
-    it('should return positive when given negative');
-
-    it('should return positive when given positive');
-  });
-});
