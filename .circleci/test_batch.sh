b61ebbbc6a7e23f7b784772e747440903cedea48
Noisekit <nikita@cc.snxdao.io>
2024-03-14 13:14:27 +0800

    Make some retries

--- .circleci/test_batch.sh
+++ .circleci/test_batch.sh
@@ -1,3 +1,15 @@
+padding () {
+  echo
+  echo
+  echo
+  echo
+  echo '---------------------------------------------------------------------------------------'
+  echo
+  echo
+  echo
+  echo
+}
+
 execute_batch () {
   BATCH_INDEX=$1
   shift
@@ -5,25 +17,20 @@ execute_batch () {
   echo "Running batch $BATCH_INDEX..."
   for ATTEMPT in {1..3}; do
     echo "Running attempt $ATTEMPT..."
-    echo mocha --jobs 1 --retries 0 --timeout 10000 --require hardhat/register --exit "${BATCH[@]}"
-    mocha --jobs 1 --retries 0 --timeout 10000 --require hardhat/register --exit "${BATCH[@]}" && {
+    echo mocha --jobs 1 --retries 2 --timeout 20000 --require hardhat/register --exit "${BATCH[@]}"
+    mocha --jobs 1 --retries 2 --timeout 20000 --require hardhat/register --exit "${BATCH[@]}" && {
       echo "Batch $BATCH_INDEX passed."
+      padding
       return 0
     } || {
       echo "Batch $BATCH_INDEX failed... Retrying attempt $ATTEMPT..."
       sleep 1
       if [ $ATTEMPT -eq 3 ]; then
+        padding
         return 1
       fi
     }
-    echo
-    echo
-    echo
-    echo
-    echo
-    echo
-    echo
-    echo
+    padding
   done
 }
 
