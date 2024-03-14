c8e1a555cbbcb24f6d1a6badcba7058c8baa3b0f
Noisekit <nikita@cc.snxdao.io>
2024-03-14 14:09:37 +0800

    Run more tests with parallelism

--- .circleci/test_batch.sh
+++ .circleci/test_batch.sh
@@ -15,17 +15,17 @@ execute_batch () {
   shift
   BATCH=("${@}")
   echo "Running batch $BATCH_INDEX..."
-  for ATTEMPT in {1..3}; do
+  for ATTEMPT in $(seq 1 ${BATCH_RETRIES:-3}); do
     echo "Running attempt $ATTEMPT..."
-    echo mocha --jobs 1 --retries 2 --timeout 20000 --require hardhat/register --exit "${BATCH[@]}"
-    mocha --jobs 1 --retries 2 --timeout 20000 --require hardhat/register --exit "${BATCH[@]}" && {
+    echo mocha --jobs 1 --retries ${MOCHA_RETRIES:-2} --timeout 20000 --require hardhat/register --reporter mocha-junit-reporter --reporter-options mochaFile=/tmp/junit/batch-$BATCH_INDEX.xml --exit "${BATCH[@]}"
+    mocha --jobs 1 --retries ${MOCHA_RETRIES:-2} --timeout 20000 --require hardhat/register --reporter mocha-junit-reporter --reporter-options mochaFile=/tmp/junit/batch-$BATCH_INDEX.xml --exit "${BATCH[@]}" && {
       echo "Batch $BATCH_INDEX passed."
       padding
       return 0
     } || {
       echo "Batch $BATCH_INDEX failed... Retrying attempt $ATTEMPT..."
       sleep 1
-      if [ $ATTEMPT -eq 3 ]; then
+      if [ $ATTEMPT -eq ${BATCH_RETRIES:-3} ]; then
         padding
         return 1
       fi
@@ -34,11 +34,17 @@ execute_batch () {
   done
 }
 
+echo "TEST_FILES:"
+echo "$TEST_FILES" | tr ' ' '\n' | while read -r TEST; do
+  echo "- $TEST"
+done
+
 BATCH_INDEX=0
 BATCH=()
+
 echo "$TEST_FILES" | tr ' ' '\n' | while read -r TEST; do
   BATCH+=("$TEST")
-  if [ ${#BATCH[@]} -eq 1 ]; then
+  if [ ${#BATCH[@]} -eq ${BATCH_SIZE:-1} ]; then
     execute_batch $BATCH_INDEX "${BATCH[@]}"
     BATCH=()
     ((BATCH_INDEX++))
