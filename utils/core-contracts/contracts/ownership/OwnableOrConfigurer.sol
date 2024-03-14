1a28d3e4da62236f5b456aa33c4618c733f179d2
dd0sxx <contacttheotelonis@gmail.com>
2024-03-14 15:52:00 -0400

    saving work so far

---
+++ utils/core-contracts/contracts/ownership/OwnableOrConfigurer.sol
@@ -0,0 +1,89 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.11 <0.9.0;
+
+import "./OwnableOrConfigurerStorage.sol";
+import "./OwnableOrConfigurerStorage.sol";
+import "../interfaces/IOwnableOrConfigurer.sol";
+import "../errors/AddressError.sol";
+import "../errors/ChangeError.sol";
+
+/**
+ * @title Contract for facilitating ownership by a the Configurer address.
+ * See IOwnable.
+ */
+contract OwnableOrConfigurer is IOwnableOrConfigurer {
+    constructor(address initialOwner) {
+        OwnableStorage.load().owner = initialOwner;
+    }
+
+    /**
+     * @inheritdoc IOwnable
+     */
+    function acceptOwnership() public override {
+        OwnableStorage.Data storage store = OwnableStorage.load();
+
+        address currentNominatedOwner = store.nominatedOwner;
+        if (msg.sender != currentNominatedOwner) {
+            revert NotNominated(msg.sender);
+        }
+
+        emit OwnerChanged(store.owner, currentNominatedOwner);
+        store.owner = currentNominatedOwner;
+
+        store.nominatedOwner = address(0);
+    }
+
+    /**
+     * @inheritdoc IOwnable
+     */
+    function nominateNewOwner(address newNominatedOwner) public override onlyOwner {
+        OwnableStorage.Data storage store = OwnableStorage.load();
+
+        if (newNominatedOwner == address(0)) {
+            revert AddressError.ZeroAddress();
+        }
+
+        if (newNominatedOwner == store.nominatedOwner) {
+            revert ChangeError.NoChange();
+        }
+
+        store.nominatedOwner = newNominatedOwner;
+        emit OwnerNominated(newNominatedOwner);
+    }
+
+    /**
+     * @inheritdoc IOwnable
+     */
+    function renounceNomination() external override {
+        OwnableStorage.Data storage store = OwnableStorage.load();
+
+        if (store.nominatedOwner != msg.sender) {
+            revert NotNominated(msg.sender);
+        }
+
+        store.nominatedOwner = address(0);
+    }
+
+    /**
+     * @inheritdoc IOwnable
+     */
+    function owner() external view override returns (address) {
+        return OwnableStorage.load().owner;
+    }
+
+    /**
+     * @inheritdoc IOwnable
+     */
+    function nominatedOwner() external view override returns (address) {
+        return OwnableStorage.load().nominatedOwner;
+    }
+
+    /**
+     * @dev Reverts if the caller is not the owner.
+     */
+    modifier onlyOwner() {
+        OwnableStorage.onlyOwner();
+
+        _;
+    }
+}
---
+++ utils/core-contracts/contracts/ownership/OwnableOrConfigurerStorage.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.11 <0.9.0;
+
+import "./OwnableOrConfigurerStorage.sol";
+import "../errors/AccessError.sol";
+
+library OwnableOrConfigurerStorage {
+    bytes32 private constant _SLOT_OWNABLE_STORAGE =
+        keccak256(abi.encode("io.synthetix.core-contracts.Ownable"));
+
+    struct Data {
+        address owner;
+        address nominatedOwner;
+    }
+
+    function load() internal pure returns (Data storage store) {
+        bytes32 s = _SLOT_OWNABLE_STORAGE;
+        assembly {
+            store.slot := s
+        }
+    }
+
+    function onlyOwner() internal view {
+        if (msg.sender != getOwner()) {
+            revert AccessError.Unauthorized(msg.sender);
+        }
+    }
+
+    function getOwner() internal view returns (address) {
+        return OwnableStorage.load().owner;
+    }
+}
