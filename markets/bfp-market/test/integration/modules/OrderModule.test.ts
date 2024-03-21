89954aa990a352eb19b5b0806cba87a0554f9bde
Joey <joey@cc.snxdao.io>
2024-03-22 10:33:40 +1100

    Fix lint

--- markets/bfp-market/test/integration/modules/OrderModule.test.ts
+++ markets/bfp-market/test/integration/modules/OrderModule.test.ts
@@ -31,7 +31,6 @@ import {
   getPythPriceData,
   getSusdCollateral,
   getPythPriceDataByMarketId,
-  setMarketConfiguration,
   setMarketConfigurationById,
   withExplicitEvmMine,
 } from '../../helpers';
