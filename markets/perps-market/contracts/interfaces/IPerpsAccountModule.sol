900d6009bfcae92e2562d05040187a1bb6fc5c64
Sunny Vempati <5723490+sunnyvempati@users.noreply.github.com>
2024-03-22 14:41:04 -0500

    debt tests

--- markets/perps-market/contracts/interfaces/IPerpsAccountModule.sol
+++ markets/perps-market/contracts/interfaces/IPerpsAccountModule.sol
@@ -144,4 +144,11 @@ interface IPerpsAccountModule {
      * @param amount debt amount to pay off
      */
     function payDebt(uint128 accountId, uint256 amount) external;
+
+    /**
+     * @notice Returns account's debt
+     * @param accountId Id of the account.
+     * @return accountDebt specified account id's debt
+     */
+    function debt(uint128 accountId) external view returns (uint256 accountDebt);
 }
