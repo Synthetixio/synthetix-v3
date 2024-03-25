62f7b7157bbb0759622be50ed67c4385fb43f47f
Leonardo Massazza <lmassazza@gmail.com>
2024-03-25 12:40:11 -0300

    Replace Rewards Distributor (#2059)
    * Replace Rewards Distributor
    
    * remove old unused stuff
    
    * Use new RD
    
    * rename package (32 chars limit)
    
    * bump rd version since it was updated

--- markets/perps-market/test/integration/bootstrap/index.ts
+++ markets/perps-market/test/integration/bootstrap/index.ts
@@ -2,3 +2,4 @@ export * from './bootstrap';
 export * from './bootstrapPerpsMarkets';
 export * from './bootstrapTraders';
 export * from './createKeeperCostNode';
+export * from './createRewardsDistributor';
