73132c370a0574f0cc8bc0bd71510369667bbf8c
Leonardo Massazza <lmassazza@gmail.com>
2024-03-26 15:10:22 -0300

    test modifyCollateral  with unregistered rd

--- markets/perps-market/test/integration/Market/Market.RewardDistributor.test.ts
+++ markets/perps-market/test/integration/Market/Market.RewardDistributor.test.ts
@@ -5,9 +5,11 @@ import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert
 import assertEvent from '@synthetixio/core-utils/utils/assertions/assert-event';
 import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber';
 import { createRewardsDistributor } from '../bootstrap';
+import { snapshotCheckpoint } from '@synthetixio/core-utils/utils/mocha/snapshot';
 
 describe('PerpsMarket: Reward Distributor configuration test', () => {
-  const { systems, signers, owner, synthMarkets } = bootstrapMarkets({
+  const accountId = 4;
+  const { systems, signers, trader1, owner, synthMarkets, provider } = bootstrapMarkets({
     synthMarkets: [
       {
         name: 'Bitcoin',
@@ -17,7 +19,7 @@ describe('PerpsMarket: Reward Distributor configuration test', () => {
       },
     ],
     perpsMarkets: [], // don't create a market in bootstrap
-    traderAccountIds: [2, 3],
+    traderAccountIds: [accountId],
     collateralLiquidateRewardRatio: bn(0.42),
     skipRegisterDistributors: true,
   });
@@ -63,6 +65,8 @@ describe('PerpsMarket: Reward Distributor configuration test', () => {
       poolDelegatedCollateralTypes: poolDelegatedCollateralTypes,
     };
   });
+
+  const restoreToSetup = snapshotCheckpoint(provider);
 
   describe('initial configuration', () => {
     it('collateral liquidate reward ratio', async () => {
@@ -249,6 +253,111 @@ describe('PerpsMarket: Reward Distributor configuration test', () => {
         assert.equal(
           registeredDistributorData.poolDelegatedCollateralTypes[0],
           poolDelegatedCollateralTypes[0]
+        );
+      });
+    });
+  });
+
+  describe('delegate collateral', () => {
+    let tx: ethers.ContractTransaction;
+    const delegatedCollateral = bn(1);
+
+    describe('can delegate snxUSD (collateralId = 0)', () => {
+      before('delegate collateral', async () => {
+        tx = await systems()
+          .PerpsMarket.connect(trader1())
+          .modifyCollateral(accountId, 0, delegatedCollateral);
+      });
+
+      it('emits event', async () => {
+        await assertEvent(
+          tx,
+          `CollateralModified(${accountId.toString()}, ${bn(0).toString()}, ${delegatedCollateral.toString()}, "${await trader1().getAddress()}")`,
+          systems().PerpsMarket
+        );
+      });
+
+      it('has correct available margin', async () => {
+        assertBn.equal(
+          await systems().PerpsMarket.getAvailableMargin(accountId),
+          delegatedCollateral
+        );
+      });
+
+      it('has correct withdrawable margin', async () => {
+        assertBn.equal(
+          await systems().PerpsMarket.getWithdrawableMargin(accountId),
+          delegatedCollateral
+        );
+      });
+    });
+
+    describe('fails to delegate other collateral without a registered distributor', () => {
+      before(restoreToSetup);
+
+      it('reverts delegate snxBTC (collateralId = 2)', async () => {
+        await assertRevert(
+          systems()
+            .PerpsMarket.connect(trader1())
+            .modifyCollateral(accountId, synthBTCMarketId, delegatedCollateral),
+          `InvalidId("${synthBTCMarketId}")`
+        );
+      });
+    });
+
+    describe('can delegate other collateral when the distributor is registered', () => {
+      const delegatedCollateralValue = delegatedCollateral.mul(10_000);
+      before(restoreToSetup);
+
+      before('register distributor', async () => {
+        await systems()
+          .PerpsMarket.connect(owner())
+          .registerDistributor(
+            distributorData.tokenAddress,
+            distributorData.distributorAddress,
+            synthBTCMarketId,
+            distributorData.poolDelegatedCollateralTypes
+          );
+      });
+
+      before('get and approve synth collateral', async () => {
+        // trade snxUSD for synth
+        await systems()
+          .SpotMarket.connect(trader1())
+          .buy(synthBTCMarketId, bn(10_000 * 1000), 0, ethers.constants.AddressZero);
+
+        // approve amount of collateral to be transfered to the market
+        await synthMarkets()[0]
+          .synth()
+          .connect(trader1())
+          .approve(systems().PerpsMarket.address, delegatedCollateral);
+      });
+
+      before('delegate collateral', async () => {
+        tx = await systems()
+          .PerpsMarket.connect(trader1())
+          .modifyCollateral(accountId, synthBTCMarketId, delegatedCollateral);
+      });
+
+      it('emits event', async () => {
+        await assertEvent(
+          tx,
+          `CollateralModified(${accountId.toString()}, ${synthBTCMarketId.toString()}, ${delegatedCollateral.toString()}, "${await trader1().getAddress()}")`,
+          systems().PerpsMarket
+        );
+      });
+
+      it('has correct available margin', async () => {
+        assertBn.equal(
+          await systems().PerpsMarket.getAvailableMargin(accountId),
+          delegatedCollateralValue
+        );
+      });
+
+      it('has correct withdrawable margin', async () => {
+        assertBn.equal(
+          await systems().PerpsMarket.getWithdrawableMargin(accountId),
+          delegatedCollateralValue
         );
       });
     });
