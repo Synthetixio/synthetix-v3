cabcebcd44c3cdd5d088d9628d636e63a546031f
Sunny Vempati <5723490+sunnyvempati@users.noreply.github.com>
2024-03-28 12:21:58 -0500

    add fix for accounts (#2077)

--- markets/perps-market/storage.dump.sol
+++ markets/perps-market/storage.dump.sol
@@ -890,8 +890,8 @@ library Position {
         uint128 marketId;
         int128 size;
         uint128 latestInteractionPrice;
-        uint256 latestInterestAccrued;
         int128 latestInteractionFunding;
+        uint256 latestInterestAccrued;
     }
 }
 
