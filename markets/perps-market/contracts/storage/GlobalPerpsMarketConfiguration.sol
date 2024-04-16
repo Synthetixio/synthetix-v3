2a35ff5ca5e9eab10e3fbd3f50d83561ae0486be
Leonardo Massazza <lmassazza@gmail.com>
2024-04-15 21:54:48 -0300

    wip: fix

--- markets/perps-market/contracts/storage/GlobalPerpsMarketConfiguration.sol
+++ markets/perps-market/contracts/storage/GlobalPerpsMarketConfiguration.sol
@@ -207,7 +207,7 @@ library GlobalPerpsMarketConfiguration {
         address referrer,
         PerpsMarketFactory.Data storage factory
     ) private returns (uint256 referralFeesSent) {
-        if (referrer == address(0)) {
+        if (fees == 0 || referrer == address(0)) {
             return 0;
         }
 
