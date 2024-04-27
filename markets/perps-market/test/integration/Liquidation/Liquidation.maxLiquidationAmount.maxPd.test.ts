18456846ba87a5fad7fbfb49bebba9b19a259553
Noisekit <28145325+noisekit@users.noreply.github.com>
2024-04-27 13:47:43 +1000

    Refactor test Liquidation - max premium discount (#2125)
    * Refactor test Liquidation - max premium discount
    
    * Await liquidations
    
    * Check test initial state
    
    * Add snapshot and restore
    
    * Refactor test for multiple liquidations within the same block
    
    * Run a single test
    
    * Enable all tests

--- markets/perps-market/test/integration/Liquidation/Liquidation.maxLiquidationAmount.maxPd.test.ts
+++ markets/perps-market/test/integration/Liquidation/Liquidation.maxLiquidationAmount.maxPd.test.ts
@@ -2,8 +2,9 @@ import { BigNumber, ethers } from 'ethers';
 import { PerpsMarket, bn, bootstrapMarkets } from '../bootstrap';
 import { openPosition } from '../helpers';
 import assertBn from '@synthetixio/core-utils/src/utils/assertions/assert-bignumber';
+import { snapshotCheckpoint } from '@synthetixio/core-utils/utils/mocha/snapshot';
 
-describe('Liquidation - max pd', () => {
+describe('Liquidation - max premium discount', () => {
   const { systems, provider, owner, trader1, trader2, keeper, perpsMarkets } = bootstrapMarkets({
     synthMarkets: [],
     perpsMarkets: [
@@ -35,17 +36,19 @@ describe('Liquidation - max pd', () => {
     traderAccountIds: [2, 3],
   });
 
-  let perpsMarket: PerpsMarket;
-  before('identify actors', () => {
-    perpsMarket = perpsMarkets()[0];
-  });
+  const restore = snapshotCheckpoint(provider);
 
-  before('add collateral to margin', async () => {
+  let perpsMarket: PerpsMarket;
+  before(async () => {
+    await restore();
+
+    perpsMarket = perpsMarkets()[0];
+
+    // add collateral to margin
     await systems().PerpsMarket.connect(trader1()).modifyCollateral(2, 0, bn(500));
     await systems().PerpsMarket.connect(trader2()).modifyCollateral(3, 0, bn(500));
-  });
 
-  before('open position', async () => {
+    // open position
     await openPosition({
       systems,
       provider,
@@ -57,125 +60,115 @@ describe('Liquidation - max pd', () => {
       settlementStrategyId: perpsMarket.strategyId(),
       price: bn(10),
     });
-  });
 
-  before('lower price to liquidation', async () => {
+    // lower price to liquidation
     await perpsMarket.aggregator().mockSetCurrentPrice(bn(1));
   });
+
+  const getTrader1Position = () => systems().PerpsMarket.getOpenPosition(2, perpsMarket.marketId());
+  const getTrader2Position = () => systems().PerpsMarket.getOpenPosition(3, perpsMarket.marketId());
 
   /**
    * Based on the above configuration, the max liquidation amount for window == 25
    * * (maker + taker) * skewScale * secondsInWindow * multiplier
    */
-  describe('without max pd set', () => {
-    before('call liquidate', async () => {
-      await systems().PerpsMarket.connect(keeper()).liquidate(2);
-    });
+  it('without max premium discount set', async () => {
+    const [, , initialSize] = await getTrader1Position();
+    assertBn.equal(initialSize, bn(90));
 
-    it('liquidated 25 OP', async () => {
-      const [, , size] = await systems().PerpsMarket.getOpenPosition(2, perpsMarket.marketId());
-      assertBn.equal(size, bn(65));
-    });
+    // liquidate
+    await (await systems().PerpsMarket.connect(keeper()).liquidate(2)).wait();
 
-    describe('call liquidate again', () => {
-      before('call liquidate', async () => {
-        await systems().PerpsMarket.connect(keeper()).liquidate(2);
-      });
-      it('liquidates no more OP', async () => {
-        const [, , size] = await systems().PerpsMarket.getOpenPosition(2, perpsMarket.marketId());
-        assertBn.equal(size, bn(65));
-      });
-    });
+    // liquidated 25 OP
+    const [, , sizeAfterLiquidation] = await getTrader1Position();
+    assertBn.equal(sizeAfterLiquidation, bn(65));
+
+    // call liquidate again
+    await (await systems().PerpsMarket.connect(keeper()).liquidate(2)).wait();
+
+    // liquidates no more OP
+    const [, , sizeAfterSecondLiquidation] = await getTrader1Position();
+    assertBn.equal(sizeAfterSecondLiquidation, bn(65));
   });
 
   /**
    * Scenario
-   * Trader 1 position left to be liquidated = 75
+   * Trader 1 position left to be liquidated = 65
    * maxPD set to 0.06 so under 60 OP skew is required for more liquidation otherwise trader has to wait for window to be liquidated
    * Trader 2 opens position which moves skew under 60 OP
    * Trader 1 can now be liquidated again by 25 OP
    */
-  describe('with max pd', () => {
-    before('set max pd', async () => {
-      await systems().PerpsMarket.connect(owner()).setMaxLiquidationParameters(
-        perpsMarket.marketId(),
-        bn(0.25),
-        BigNumber.from(10),
-        bn(0.06), // 60 OP maxPD
-        ethers.constants.AddressZero
-      );
+  it('with max premium discount', async () => {
+    const [, , initialSize] = await getTrader1Position();
+    assertBn.equal(initialSize, bn(65));
+
+    // set max premium discount
+    await systems().PerpsMarket.connect(owner()).setMaxLiquidationParameters(
+      perpsMarket.marketId(),
+      bn(0.25),
+      BigNumber.from(10),
+      bn(0.06), // 60 OP maxPD
+      ethers.constants.AddressZero
+    );
+
+    // trader 2 arbs
+    await openPosition({
+      systems,
+      provider,
+      trader: trader2(),
+      accountId: 3,
+      keeper: keeper(),
+      marketId: perpsMarket.marketId(),
+      sizeDelta: bn(-25),
+      settlementStrategyId: perpsMarket.strategyId(),
+      price: bn(1),
     });
 
-    before('trader 2 arbs', async () => {
-      await openPosition({
-        systems,
-        provider,
-        trader: trader2(),
-        accountId: 3,
-        keeper: keeper(),
-        marketId: perpsMarket.marketId(),
-        sizeDelta: bn(-25),
-        settlementStrategyId: perpsMarket.strategyId(),
-        price: bn(1),
-      });
-    });
+    await systems().PerpsMarket.connect(keeper()).liquidate(2);
 
-    before('call liquidate', async () => {
-      await systems().PerpsMarket.connect(keeper()).liquidate(2);
-    });
-
-    it('liquidated 25 OP more', async () => {
-      const [, , size] = await systems().PerpsMarket.getOpenPosition(2, perpsMarket.marketId());
-      assertBn.equal(size, bn(40));
-    });
+    // liquidated 25 OP more
+    const [, , sizeAfterArbLiquidation] = await getTrader1Position();
+    assertBn.equal(sizeAfterArbLiquidation, bn(40));
   });
 
-  describe('more liquidation of trader 1 since under max pd', () => {
-    describe('same block', () => {
-      before('call liquidate twice more since under max pd', async () => {
-        await systems().TrustedMulticallForwarder.aggregate([
-          {
-            target: systems().PerpsMarket.address,
-            callData: systems().PerpsMarket.interface.encodeFunctionData('liquidate', [2]),
-          },
-          {
-            target: systems().PerpsMarket.address,
-            callData: systems().PerpsMarket.interface.encodeFunctionData('liquidate', [2]),
-          },
-        ]);
-      });
+  it('should liquidate more of trader 1 since under max premium discount', async () => {
+    // call liquidate twice more since under max premium discount
+    await provider().send('evm_setAutomine', [false]);
+    // Same block multiple liquidations are ignored and only one is effective
+    const tx1 = await systems().PerpsMarket.connect(keeper()).liquidate(2);
+    const tx2 = await systems().PerpsMarket.connect(keeper()).liquidate(2);
+    const tx3 = await systems().PerpsMarket.connect(keeper()).liquidate(2);
+    const tx4 = await systems().PerpsMarket.connect(keeper()).liquidate(2);
+    await provider().send('evm_setAutomine', [true]);
+    await provider().send('evm_mine', []);
+    await provider().send('evm_mine', []);
+    await provider().send('evm_mine', []);
+    await provider().send('evm_mine', []);
 
-      it('liquidated 25 OP more', async () => {
-        const [, , size] = await systems().PerpsMarket.getOpenPosition(2, perpsMarket.marketId());
-        assertBn.equal(size, bn(15));
-      });
-    });
+    // Make sure all the liquidation txns are finalised
+    await Promise.all([tx1.wait(), tx2.wait(), tx3.wait(), tx4.wait()]);
 
-    describe('next block', () => {
-      before('call liquidate again', async () => {
-        await systems().PerpsMarket.connect(keeper()).liquidate(2);
-      });
+    // liquidated 25 OP more in the same block (only one liquidation on the same block was actually effective)
+    const [, , sizeOnSameBlock] = await getTrader1Position();
+    assertBn.equal(sizeOnSameBlock, bn(15));
 
-      it('liquidated 25 OP more', async () => {
-        const [, , size] = await systems().PerpsMarket.getOpenPosition(2, perpsMarket.marketId());
-        assertBn.equal(size, bn(0));
-      });
-    });
+    // const [, , sizeOnNextBlock] = await getTrader1Position();
+    // assertBn.equal(sizeOnNextBlock, bn(15));
+
+    // liquidated 25 OP more in the next block
+    await (await systems().PerpsMarket.connect(keeper()).liquidate(2)).wait();
+
+    // await systems().PerpsMarket.connect(keeper()).liquidate(2);
+    const [, , sizeOnNextBlock] = await getTrader1Position();
+    assertBn.equal(sizeOnNextBlock, bn(0));
   });
 
-  describe('liquidate trader 2', () => {
-    before('change price of OP', async () => {
-      await perpsMarket.aggregator().mockSetCurrentPrice(bn(30));
-    });
-
-    before('call liquidate', async () => {
-      await systems().PerpsMarket.connect(keeper()).liquidate(3);
-    });
-
+  it('should liquidate trader 2', async () => {
+    // change price of OP
+    await perpsMarket.aggregator().mockSetCurrentPrice(bn(30));
+    await systems().PerpsMarket.connect(keeper()).liquidate(3);
     // because the previous liquidation of trader 1 was of 15 OP, the remaining amount that can be liquidated is 10 OP
-    it('liquidated all 10 OP', async () => {
-      const [, , size] = await systems().PerpsMarket.getOpenPosition(3, perpsMarket.marketId());
-      assertBn.equal(size, 0);
-    });
+    const [, , size] = await getTrader2Position();
+    assertBn.equal(size, 0);
   });
 });
