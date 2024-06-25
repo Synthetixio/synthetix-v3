9e78307443705a748203a303415bbc5d86028bdf
Matías <mjlescano@protonmail.com>
2024-06-25 22:00:27 +0900

    remove last ccip references on governance

--- protocol/governance/contracts/mocks/CcipRouterMock.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import {CcipRouterMock as CcipRouterMockBase} from "@synthetixio/core-modules/contracts/mocks/CcipRouterMock.sol";
-
-// solhint-disable-next-line no-empty-blocks
-contract CcipRouterMock is CcipRouterMockBase {}
