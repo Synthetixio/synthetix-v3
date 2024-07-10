6da53dc9f0e55429355eee5aa82c84ef706b39e1
Matías <mjlescano@protonmail.com>
2024-07-10 15:13:35 +0200

    add dismisal members tests

--- protocol/governance/contracts/mocks/CouncilMemberMock.sol
+++ protocol/governance/contracts/mocks/CouncilMemberMock.sol
@@ -5,11 +5,11 @@ import {ElectionCredentials} from "../submodules/election/ElectionCredentials.so
 
 /// @dev Helper functions to test council members functionality
 contract CouncilMemberMock is ElectionCredentials {
-    function addCouncilMembersMock(address[] memory membersToAdd, uint256 epochIndex) public {
+    function addCouncilMembersMock(address[] memory membersToAdd, uint256 epochIndex) external {
         _addCouncilMembers(membersToAdd, epochIndex);
     }
 
-    function removeAllCouncilMembersMock(uint256 epochIndex) public {
+    function removeAllCouncilMembersMock(uint256 epochIndex) external {
         _removeAllCouncilMembers(epochIndex);
     }
 }
--- protocol/governance/test/contracts/ElectionModule/CouncilMembers.test.ts
+++ protocol/governance/test/contracts/ElectionModule/CouncilMembers.test.ts
@@ -1,40 +1,113 @@
-import { deepEqual, equal } from 'node:assert/strict';
-import assertRevert from '@synthetixio/core-utils/utils/assertions/assert-revert';
-import { fastForwardTo } from '@synthetixio/core-utils/utils/hardhat/rpc';
+import { deepEqual } from 'node:assert/strict';
+import assertBn from '@synthetixio/core-utils/utils/assertions/assert-bignumber';
+import assertEvent from '@synthetixio/core-utils/utils/assertions/assert-event';
 import { ethers } from 'ethers';
 import { bootstrap } from '../../bootstrap';
+import { ElectionPeriod } from '../../constants';
+
+function _sortedDeepEqual(actual: string[], expected: string[]) {
+  deepEqual([...actual].sort(), [...expected].sort());
+}
 
 describe('ElectionModule - council members', function () {
   const { c, getSigners, snapshotCheckpoint } = bootstrap();
 
   let owner: ethers.Signer;
-  let members: ethers.Signer[];
+  let members: string[];
+
+  const MINIMUM_COUNCIL_MEMBERS = 3;
 
   before('identify signers', async function () {
-    owner = getSigners()[0];
-    members = getSigners().slice(1, 4);
-  });
-
-  before('register emitters', async function () {
-    await c.GovernanceProxy.connect(owner).setRegisteredEmitters(
-      [10002],
-      [c.GovernanceProxy.address]
+    [owner] = getSigners();
+    members = await Promise.all(
+      getSigners()
+        .slice(1, 5)
+        .map((m) => m.getAddress())
     );
   });
 
-  before('prepare council members', async function () {
-    const membersToAdd = await Promise.all(members.map((m) => m.getAddress()));
-    await c.GovernanceProxy.removeAllCouncilMembersMock(0);
-    await c.GovernanceProxy.addCouncilMembersMock(membersToAdd, 0);
+  before('register emitters', async function () {
+    await c.GovernanceProxy.connect(owner)
+      .setRegisteredEmitters([10002], [c.GovernanceProxy.address])
+      .then((tx) => tx.wait());
   });
 
-  it('correctly shows all members', async function () {
-    const expected = await Promise.all(members.map((m) => m.getAddress()));
-    const current = await c.GovernanceProxy.getCouncilMembers();
-    deepEqual(expected, current);
+  before('update settings', async function () {
+    await c.GovernanceProxy.ElectionSettings_set_minimumActiveMembers(
+      0,
+      MINIMUM_COUNCIL_MEMBERS
+    ).then((tx) => tx.wait());
+  });
+
+  before('prepare council members', async function () {
+    await c.GovernanceProxy.removeAllCouncilMembersMock(0).then((tx) => tx.wait());
+    await c.GovernanceProxy.addCouncilMembersMock(members, 0).then((tx) => tx.wait());
+  });
+
+  it('lists all members', async function () {
+    deepEqual(await c.GovernanceProxy.getCouncilMembers(), members);
   });
 
   describe('#dismissMembers', function () {
     snapshotCheckpoint();
+
+    let receipt: ethers.ContractReceipt;
+
+    before('dismiss a single member', async function () {
+      const tx = await c.GovernanceProxy.dismissMembers([members[0]]);
+      receipt = await tx.wait();
+    });
+
+    it('lists remaining members', async function () {
+      _sortedDeepEqual(await c.GovernanceProxy.getCouncilMembers(), members.slice(1));
+
+      await assertEvent(receipt, `CouncilMemberRemoved("${members[0]}", 0)`, c.GovernanceProxy);
+      await assertEvent(
+        receipt,
+        `CouncilMembersDismissed(["${members[0]}"], 0)`,
+        c.GovernanceProxy
+      );
+    });
+
+    it('did not start an emergency election', function () {
+      const emergencyEvents = receipt.events?.filter(
+        (evt) => evt.event === 'EmergencyElectionStarted'
+      );
+      deepEqual(emergencyEvents, []);
+    });
+
+    describe('when dismissing all members', function () {
+      let membersToDissmiss: string[];
+      let receipt: ethers.ContractReceipt;
+
+      before('dismiss remaining members', async function () {
+        [, ...membersToDissmiss] = members;
+        const tx = await c.GovernanceProxy.dismissMembers(membersToDissmiss);
+        receipt = await tx.wait();
+      });
+
+      it('lists remaining members', async function () {
+        _sortedDeepEqual(await c.GovernanceProxy.getCouncilMembers(), []);
+
+        await Promise.all(
+          membersToDissmiss.map((m) =>
+            assertEvent(receipt, `CouncilMemberRemoved("${m}", 0)`, c.GovernanceProxy)
+          )
+        );
+        await assertEvent(
+          receipt,
+          `CouncilMembersDismissed(["${membersToDissmiss.join('", "')}"], 0)`,
+          c.GovernanceProxy
+        );
+      });
+
+      it('triggered emergency election', async function () {
+        await assertEvent(receipt, `EmergencyElectionStarted(0)`, c.GovernanceProxy);
+      });
+
+      it('jump to nomination period', async function () {
+        assertBn.equal(await c.GovernanceProxy.getCurrentPeriod(), ElectionPeriod.Nomination);
+      });
+    });
   });
 });
