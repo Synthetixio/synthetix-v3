0e6e54eac3aa221468f1bb16ea1f8c5030165942
Noisekit <28145325+noisekit@users.noreply.github.com>
2024-08-26 20:37:46 +0800

    Docgen contracts (#2236)
    * Remove perps docs from git
    
    * Add missing docs to the generator and fix generation for new packages
    
    * Deps
    
    * Append docs

--- utils/docgen/docgen-contracts.sh
+++ utils/docgen/docgen-contracts.sh
@@ -17,6 +17,14 @@ echo "## Synthetix Core" >> $OUT
 echo "" >> $OUT
 cat $ROOT/protocol/synthetix/docs/index.md >> $OUT
 
+echo "## Utility Core Contracts" >> $OUT
+echo "" >> $OUT
+cat $ROOT/utils/core-contracts/docs/index.md >> $OUT
+
+echo "## Utility Core Modules" >> $OUT
+echo "" >> $OUT
+cat $ROOT/utils/core-modules/docs/index.md >> $OUT
+
 echo "## Spot Market" >> $OUT
 echo "" >> $OUT
 cat $ROOT/markets/spot-market/docs/index.md >> $OUT
@@ -29,6 +37,10 @@ echo "## Legacy Market" >> $OUT
 echo "" >> $OUT
 cat $ROOT/markets/legacy-market/docs/index.md >> $OUT
 
+echo "## BFP Market" >> $OUT
+echo "" >> $OUT
+cat $ROOT/markets/bfp-market/docs/index.md >> $OUT
+
 echo "## Governance" >> $OUT
 echo "" >> $OUT
 cat $ROOT/protocol/governance/docs/index.md >> $OUT
@@ -36,5 +48,16 @@ cat $ROOT/protocol/governance/docs/index.md >> $OUT
 echo "## Oracle Manager" >> $OUT
 echo "" >> $OUT
 cat $ROOT/protocol/oracle-manager/docs/index.md >> $OUT
+
+
+echo "# Auxiliary Packages" >> $OUT
+echo "" >> $OUT
+cat $ROOT/auxiliary/ArbitrumGasPriceOracle/docs/index.md >> $OUT
+cat $ROOT/auxiliary/BuybackSnx/docs/index.md >> $OUT
+cat $ROOT/auxiliary/ERC4626ToAssetsRatioOracle/docs/index.md >> $OUT
+cat $ROOT/auxiliary/OpGasPriceOracle/docs/index.md >> $OUT
+cat $ROOT/auxiliary/PythERC7412Wrapper/docs/index.md >> $OUT
+cat $ROOT/auxiliary/SpotMarketOracle/docs/index.md >> $OUT
+cat $ROOT/auxiliary/WstEthToStEthRatioOracle/docs/index.md >> $OUT
 
 echo "OK Docgen contracts"
