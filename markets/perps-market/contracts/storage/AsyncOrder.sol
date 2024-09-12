56f1fd5dfb7e85c84c937b32b09bf8385af76815
Leonardo Massazza <lmassazza@gmail.com>
2024-09-12 19:29:24 -0300

    🔴 H-01: Required Credit Check Can DoS Decrease Orders (#2239) (#2287)
    * 🔴 H-01: Required Credit Check Can DoS Decrease Orders (#2239)
    
    * 👷 add size magnitude logic
    
    * 📚 add docs and question
    
    * 📚 add docs (wip)
    
    * 📚 add docs
    
    * 📚 add natspec
    
    * 👷 require validation is side changes
    
    * 🧹 clean up docs
    
    * 📚 remove question and update event comment
    
    * 👷 overcome stack limitations
    
    * 📚 update docs
    
    * 👷 use correct credit value
    
    * ✅ test reducing magnitude logic
    
    * ✅ test magnitude logic with side change
    
    * 🗑️ remove .only
    
    ---------
    
    Co-authored-by: Leonardo Massazza <lmassazza@gmail.com>
    
    * lint fix
    
    * lint fix
    
    * ✅ add restore snapshot
    
    * remove rogue .only
    
    * partially close the last position
    
    ---------
    
    Co-authored-by: wasp in bush <177558793+wasp-in-bush@users.noreply.github.com>
    Co-authored-by: Leo M <leo@cc.sxndao.io>
    Co-authored-by: Fred Snax <fred@cc.snxdao.io>

--- markets/perps-market/contracts/storage/AsyncOrder.sol
+++ markets/perps-market/contracts/storage/AsyncOrder.sol
@@ -264,10 +264,12 @@ library AsyncOrder {
         SettlementStrategy.Data storage strategy,
         uint256 orderPrice
     ) internal returns (Position.Data memory, uint256, uint256, Position.Data storage oldPosition) {
+        /// @dev runtime stores order settlement data and prevents stack too deep
         SimulateDataRuntime memory runtime;
-        runtime.sizeDelta = order.request.sizeDelta;
+
         runtime.accountId = order.request.accountId;
         runtime.marketId = order.request.marketId;
+        runtime.sizeDelta = order.request.sizeDelta;
 
         if (runtime.sizeDelta == 0) {
             revert ZeroSizeOrder();
@@ -348,11 +350,28 @@ library AsyncOrder {
             revert InsufficientMargin(runtime.currentAvailableMargin, runtime.totalRequiredMargin);
         }
 
-        int256 lockedCreditDelta = perpsMarketData.requiredCreditForSize(
-            MathUtil.abs(runtime.newPositionSize).toInt() - MathUtil.abs(oldPosition.size).toInt(),
-            PerpsPrice.Tolerance.DEFAULT
-        );
-        GlobalPerpsMarket.load().validateMarketCapacity(lockedCreditDelta);
+        /// @dev if new position size is not 0, further credit validation required
+        if (runtime.newPositionSize != 0) {
+            /// @custom:magnitude determines if more market credit is required
+            /// when a position's magnitude is increased, more credit is required and risk increases
+            /// when a position's magnitude is decreased, less credit is required and risk decreases
+            uint256 newMagnitude = MathUtil.abs(runtime.newPositionSize);
+            uint256 oldMagnitude = MathUtil.abs(oldPosition.size);
+
+            /// @custom:side reflects if position is long or short; if side changes, further validation required
+            /// given new position size cannot be zero, it is inconsequential if old size is zero;
+            /// magnitude will necessarily be larger
+            bool sameSide = runtime.newPositionSize > 0 == oldPosition.size > 0;
+
+            // require validation if magnitude has increased or side has not remained the same
+            if (newMagnitude > oldMagnitude || !sameSide) {
+                int256 lockedCreditDelta = perpsMarketData.requiredCreditForSize(
+                    newMagnitude.toInt() - oldMagnitude.toInt(),
+                    PerpsPrice.Tolerance.DEFAULT
+                );
+                GlobalPerpsMarket.load().validateMarketCapacity(lockedCreditDelta);
+            }
+        }
 
         runtime.newPosition = Position.Data({
             marketId: runtime.marketId,
