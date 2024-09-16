4aa28e17a8b5612e1de3f8eaf9bafa6e00c96902
Fred Snax <fred@cc.snxdao.io>
2024-09-16 09:12:00 -0700

    change new required margin check to use current price instead of fill price to match liquidations

--- markets/perps-market/contracts/storage/AsyncOrder.sol
+++ markets/perps-market/contracts/storage/AsyncOrder.sol
@@ -571,7 +571,7 @@ library AsyncOrder {
         // get initial margin requirement for the new position
         (, , runtime.newRequiredMargin, ) = marketConfig.calculateRequiredMargins(
             newPositionSize,
-            fillPrice
+            PerpsPrice.getCurrentPrice(marketId, PerpsPrice.Tolerance.DEFAULT)
         );
 
         // get initial margin of old position
