1bb9bddff187e048b96c8475f59350e9ca1a80eb
Moss <moss@cc.snxdao.io>
2024-09-25 17:51:20 -0400

    pushing fonudry test skeleton

---
+++ markets/perps-market/test/integration/Clearinghouse/ClearinghouseModule.t.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.8.11 <0.9.0;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {ClearinghouseModule} from "../../contracts/modules/ClearinghouseModule.sol";
+
+contract ClearinghouseModule {
+ClearinghouseModule clearingHouse;
+}
+
+contract CancelOrders is ClearinghouseModule {
+    function setUp() public {
+        clearingHouse = new ClearinghouseModule();
+    }
+    function test_CancelsSingleOrders(uint256 _nonce) public {
+
+    }
+}
