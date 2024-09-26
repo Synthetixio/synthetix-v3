85664fd3935e3ecf326b1f72edf234887ce6de22
Daniel Beal <git@dbeal.dev>
2024-09-27 03:41:27 +0900

    fixes

--- markets/perps-market/contracts/modules/PerpsCoreModule.sol
+++ markets/perps-market/contracts/modules/PerpsCoreModule.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.11 <0.9.0;
 
-import {CoreModule as BaseCoreModule} from "@synthetixio/core-modules/contracts/modules/CoreModule.sol";
+import {CoreModule} from "@synthetixio/core-modules/contracts/modules/CoreModule.sol";
 
 /**
  * @title Module that defines ownership and upgradability.
  * See core-modules/../CoreModule
  */
 // solhint-disable-next-line no-empty-blocks
-contract CoreModule is BaseCoreModule {
+contract PerpsCoreModule is CoreModule {
 
 }
--- markets/perps-market/contracts/modules/PerpsFeatureFlagModule.sol
+++ markets/perps-market/contracts/modules/PerpsFeatureFlagModule.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.11 <0.9.0;
 
-import {FeatureFlagModule as BaseFeatureFlagModule} from "@synthetixio/core-modules/contracts/modules/FeatureFlagModule.sol";
+import {FeatureFlagModule} from "@synthetixio/core-modules/contracts/modules/FeatureFlagModule.sol";
 
 /**
  * @title Module that allows disabling certain system features.
@@ -9,4 +9,4 @@ import {FeatureFlagModule as BaseFeatureFlagModule} from "@synthetixio/core-modu
  * Users will not be able to interact with certain functions associated to disabled features.
  */
 // solhint-disable-next-line no-empty-blocks
-contract FeatureFlagModule is BaseFeatureFlagModule {}
+contract PerpsFeatureFlagModule is FeatureFlagModule {}
